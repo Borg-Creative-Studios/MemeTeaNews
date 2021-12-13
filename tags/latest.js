f4f11e6f73e2ba0c564a7a7748190cbfe719f131
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-13 13:52:08 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,28 +1,28 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'The Biden Admin wants 50% of car sale shares to be electric by 2030.'
-    document.getElementById("content1").innerHTML = '<p>This plan includes:</p><p>• A joint DOE and DOT office</p><p>• Gather stakeholer opinions</p><p>• Issue guidence and standards</p><p>• Gain info from domestic manufacturers</p><p>and more</p>'
+    document.getElementById("title1").innerHTML = 'The average price of gas across the USA is up $1.184 from 2020.'
+    document.getElementById("content1").innerHTML = '<p>Current ave. (Regular, mid, & premium): $3.666</p><p>2020 ave. (Regular, mid, & premium): $2.482</p>'
     document.getElementById("content2").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'Read the release below'
-    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/13/fact-sheet-the-biden-harris-electric-vehicle-charging-action-plan/'
-    document.getElementById("time1").innerHTML = '12/13/21 1:09pm'
+    document.getElementById("link1").innerHTML = 'Find gas prices for your state here.'
+    document.getElementById("link1").href = 'https://gasprices.aaa.com/state-gas-price-averages/'
+    document.getElementById("time1").innerHTML = '12/13/21 1:51pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Samaritans Purse is responding to tornado stricken communities in Kentucky and Arkansas.'
-    document.getElementById("content3").innerHTML = '<p>#kentuckytornado #disasterrecovery #news #openteanews </p>'
+    document.getElementById("title2").innerHTML = 'The Biden Admin wants 50% of car sale shares to be electric by 2030.'
+    document.getElementById("content3").innerHTML = '<p>This plan includes:</p><p>• A joint DOE and DOT office</p><p>• Gather stakeholer opinions</p><p>• Issue guidence and standards</p><p>• Gain info from domestic manufacturers</p><p>and more</p>'
     document.getElementById("content4").innerHTML = ''
-    document.getElementById("link2").innerHTML = 'Learn more about how to volunteer with Samaritan’s Purse and how they are responding here.'
-    document.getElementById("link2").href = 'https://www.samaritanspurse.org/article/deadly-tornado-outbreak-brings-heartache-across-multiple-states/'
-    document.getElementById("time2").innerHTML = '12/13/21 12:32am'
+    document.getElementById("link2").innerHTML = 'WH RELEASE'
+    document.getElementById("link2").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/13/fact-sheet-the-biden-harris-electric-vehicle-charging-action-plan/'
+    document.getElementById("time2").innerHTML = '12/13/21 1:09pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'Federal financial aid has been approved for Kentucky in the wake of devastating tornadoes.'
-    document.getElementById("content5").innerHTML = '<p>-The aid will be available to impacted individuals in Caldwell, Fulton, Graves, Hopkins, Marshall, Muhlenberg, Taylor, and Warren counties.</p><p>-The funds can be used for temporary housing, repairs, low-cost loans, etc.</p><p>-Funding is also available for the governments of the commonwealth of Kentucky and some private nonprofits.</p>'
+    document.getElementById("title3").innerHTML = 'Samaritans Purse is responding to tornado stricken communities in Kentucky and Arkansas.'
+    document.getElementById("content5").innerHTML = '<p>#kentuckytornado #disasterrecovery #news #openteanews </p>'
     document.getElementById("content6").innerHTML = ''
-    document.getElementById("link3").innerHTML = 'WH RELEASE'
-    document.getElementById("link3").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/12/president-joseph-r-biden-jr-approves-kentucky-disaster-declaration-3/'
-    document.getElementById("time3").innerHTML = '12/12/21 9:38pm'
+    document.getElementById("link3").innerHTML = 'Learn more about how to volunteer with Samaritan’s Purse and how they are responding here.'
+    document.getElementById("link3").href = 'https://www.samaritanspurse.org/article/deadly-tornado-outbreak-brings-heartache-across-multiple-states/'
+    document.getElementById("time3").innerHTML = '12/13/21 12:32am'
 
 }
