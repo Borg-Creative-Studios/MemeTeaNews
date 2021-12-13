9aca7cb6986fb9100af5f3f01e9e466bd37bfff3
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-12 20:27:06 -0700

    update

---
+++ tags/alert.js
@@ -0,0 +1,5 @@
+function alert(){
+    document.getElementById("acontent1").innerHTML = 'FEMA disaster relief resources can be found'
+    document.getElementById("alink").innerHTML = 'here.'
+    document.getElementById("alink").href = 'https://www.fema.gov/assistance/individual'
+}
