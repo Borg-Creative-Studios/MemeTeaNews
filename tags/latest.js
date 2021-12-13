c2b7d5e253cbe7a5c85fc68da8e10d85b8cf8580
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-12 20:50:41 -0700

    update

--- tags/latest.js
+++ tags/latest.js
@@ -1,7 +1,10 @@
 function latest(){
-    document.getElementById("title").innerHTML = '0'
-    document.getElementById("content1").innerHTML = '0'
-    document.getElementById("content2").innerHTML = '0'
-    document.getElementById("link").innerHTML = '0'
-    document.getElementById("link").href = '0'
+    document.getElementById("title").innerHTML = 'Red Cross has requested additional blood donations in the wake of the #kentuckytornado'
+    document.getElementById("content1").innerHTML = '<p>-Red Cross has provided an additional 200 blood products since the tornadoes.</p><p>-Red Cross is on standby to provide more.</p><p>-Prior to the tornadoes there was already a low supply of blood products.</p>'
+    document.getElementById("content2").innerHTML = '#news #redcross #disasterrecovery #blooddonations '
+    document.getElementById("link").innerHTML = 'Find how you can donate blood here.'
+    document.getElementById("link").href = 'https://www.redcross.org/about-us/news-and-events/news/2021/blood-donors-needed-to-replenish-supply-following-tornado-outbre.html'
+
+    document.getElementById("time").innerHTML = '12/12/21 7:29pm'
+
 }
