44f647b7d4d1ce2a6d86a2b666b35d34b0fd03a5
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-13 00:39:24 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,27 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'Federal financial aid has been approved for Kentucky in the wake of devastating tornadoes.'
-    document.getElementById("content1").innerHTML = '<p>-The aid will be available to impacted individuals in Caldwell, Fulton, Graves, Hopkins, Marshall, Muhlenberg, Taylor, and Warren counties.</p><p>-The funds can be used for temporary housing, repairs, low-cost loans, etc.</p><p>-Funding is also available for the governments of the commonwealth of Kentucky and some private nonprofits.</p>'
+    document.getElementById("title1").innerHTML = 'Samaritan’s Purse is responding to tornado stricken communities in Kentucky and Arkansas.'
+    document.getElementById("content1").innerHTML = '<p>#kentuckytornado #disasterrecovery #news #openteanews </p>'
     document.getElementById("content2").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'WH RELEASE'
-    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/12/president-joseph-r-biden-jr-approves-kentucky-disaster-declaration-3/'
-    document.getElementById("time1").innerHTML = '12/12/21 9:38pm'
+    document.getElementById("link1").innerHTML = 'Learn more about how to volunteer with Samaritan’s Purse and how they are responding here.'
+    document.getElementById("link1").href = 'https://www.samaritanspurse.org/article/deadly-tornado-outbreak-brings-heartache-across-multiple-states/'
+    document.getElementById("time1").innerHTML = '12/13/21 12:32am'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Red Cross has requested additional blood donations in the wake of the #kentuckytornado'
-    document.getElementById("content3").innerHTML = '<p>-Red Cross has provided an additional 200 blood products since the tornadoes.</p><p>-Red Cross is on standby to provide more.</p><p>-Prior to the tornadoes there was already a low supply of blood products.</p>'
-    document.getElementById("content4").innerHTML = '#news #redcross #disasterrecovery #blooddonations '
-    document.getElementById("link2").innerHTML = 'Find how you can donate blood here.'
-    document.getElementById("link2").href = 'https://www.redcross.org/about-us/news-and-events/news/2021/blood-donors-needed-to-replenish-supply-following-tornado-outbre.html'
-    document.getElementById("time2").innerHTML = '12/12/21 7:29pm'
+    document.getElementById("title2").innerHTML = 'Federal financial aid has been approved for Kentucky in the wake of devastating tornadoes.'
+    document.getElementById("content3").innerHTML = '<p>-The aid will be available to impacted individuals in Caldwell, Fulton, Graves, Hopkins, Marshall, Muhlenberg, Taylor, and Warren counties.</p><p>-The funds can be used for temporary housing, repairs, low-cost loans, etc.</p><p>-Funding is also available for the governments of the commonwealth of Kentucky and some private nonprofits.</p>'
+    document.getElementById("content4").innerHTML = ''
+    document.getElementById("link2").innerHTML = 'WH RELEASE'
+    document.getElementById("link2").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/12/president-joseph-r-biden-jr-approves-kentucky-disaster-declaration-3/'
+    document.getElementById("time2").innerHTML = '12/12/21 9:38pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'Since approximately December 8th*, several Maxwell trial tracking accounts on Twitter have been suspended.'
-    document.getElementById("content5").innerHTML = '<p>Reporting on the trial from MSM has been limited, but still present.</p>'
-    document.getElementById("content6").innerHTML = '*This is an approximate date based on the information we have found on social accounts being suspended. Information on which accounts and when they were suspended has been difficult to identify and verify authenticity.'
-    document.getElementById("link3").innerHTML = 'View the post'
-    document.getElementById("link3").href = 'https://gettr.com/post/pjint01b2e'
-    document.getElementById("time3").innerHTML = '12/12/21 6:45pm'
-
-
+    document.getElementById("title3").innerHTML = 'Red Cross has requested additional blood donations in the wake of the #kentuckytornado'
+    document.getElementById("content5").innerHTML = '<p>-Red Cross has provided an additional 200 blood products since the tornadoes.</p><p>-Red Cross is on standby to provide more.</p><p>-Prior to the tornadoes there was already a low supply of blood products.</p>'
+    document.getElementById("content6").innerHTML = '#news #redcross #disasterrecovery #blooddonations '
+    document.getElementById("link3").innerHTML = 'Find how you can donate blood here.'
+    document.getElementById("link3").href = 'https://www.redcross.org/about-us/news-and-events/news/2021/blood-donors-needed-to-replenish-supply-following-tornado-outbre.html'
+    document.getElementById("time3").innerHTML = '12/12/21 7:29pm'
 }
