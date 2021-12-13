76270913e863f46212fe271ff127ebaceb6bf6f1
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-12 20:17:19 -0700

    update

---
+++ tags/latest.js
@@ -0,0 +1,7 @@
+function latest(){
+    document.getElementById("title").innerHTML = '0'
+    document.getElementById("content1").innerHTML = '0'
+    document.getElementById("content2").innerHTML = '0'
+    document.getElementById("link").innerHTML = '0'
+    document.getElementById("link").href = '0'
+}
