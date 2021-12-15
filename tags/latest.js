5636e42d94e534cdd58a72ea2a9cc79b100db0a3
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-14 17:29:13 -0700

    update

--- tags/latest.js
+++ tags/latest.js
@@ -1,28 +1,28 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'KY Gov. Beshear gave an update regarding the Dec 11th tornadoes.'
-    document.getElementById("content1").innerHTML = '<p>-64 confirmed lives lost.</p><p>-18 of those lost have yet to be identified.</p><p>-105 individuals are still missing.</p>'
-    document.getElementById("content2").innerHTML = '<p>-Approximately 1000 homes have been destroyed or damaged.</p><p>-Flags in KY will be flown half mast Tuesday through Monday.</p><p>-The KY relief fund has raised $4,009,817.71</p>'
-    document.getElementById("link1").innerHTML = 'FDonate to the relief fund.'
-    document.getElementById("link1").href = 'https://secure.kentucky.gov/formservices/Finance/WKYRelief'
-    document.getElementById("time1").innerHTML = '12/13/21 3:03pm'
+    document.getElementById("title1").innerHTML = 'Biden has announced a “historic release” from the Strategic Petroleum Reserve in an effort to reduce gas prices.'
+    document.getElementById("content1").innerHTML = '<p>-Prices are down $0.10 from last months peak.</p><p>-Prices are down $0.30 from this year’s peak.</p>'
+    document.getElementById("content2").innerHTML = '<p>-As of yesterday, the average price per gallon of fuel in the US was up $1.184 from 2020.</p><p>#gasprices #economy #news #openteanews</p>'
+    document.getElementById("link1").innerHTML = ''
+    document.getElementById("link1").href = ''
+    document.getElementById("time1").innerHTML = '12/14/21 5:23pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'The average price of gas across the USA is up $1.184 from 2020.'
-    document.getElementById("content3").innerHTML = '<p>Current ave. (Regular, mid, & premium): $3.666</p><p>2020 ave. (Regular, mid, & premium): $2.482</p>'
-    document.getElementById("content4").innerHTML = ''
-    document.getElementById("link2").innerHTML = 'Find gas prices for your state here.'
-    document.getElementById("link2").href = 'https://gasprices.aaa.com/state-gas-price-averages/'
-    document.getElementById("time2").innerHTML = '12/13/21 1:51pm'
+    document.getElementById("title2").innerHTML = 'KY Gov. Beshear gave an update regarding the Dec 11th tornadoes.'
+    document.getElementById("content3").innerHTML = '<p>-64 confirmed lives lost.</p><p>-18 of those lost have yet to be identified.</p><p>-105 individuals are still missing.</p>'
+    document.getElementById("content4").innerHTML = '<p>-Approximately 1000 homes have been destroyed or damaged.</p><p>-Flags in KY will be flown half mast Tuesday through Monday.</p><p>-The KY relief fund has raised $4,009,817.71</p>'
+    document.getElementById("link2").innerHTML = 'FDonate to the relief fund.'
+    document.getElementById("link2").href = 'https://secure.kentucky.gov/formservices/Finance/WKYRelief'
+    document.getElementById("time2").innerHTML = '12/13/21 3:03pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'The Biden Admin wants 50% of car sale shares to be electric by 2030.'
-    document.getElementById("content5").innerHTML = '<p>This plan includes:</p><p>• A joint DOE and DOT office</p><p>• Gather stakeholer opinions</p><p>• Issue guidence and standards</p><p>• Gain info from domestic manufacturers</p><p>and more</p>'
+    document.getElementById("title3").innerHTML = 'The average price of gas across the USA is up $1.184 from 2020.'
+    document.getElementById("content5").innerHTML = '<p>Current ave. (Regular, mid, & premium): $3.666</p><p>2020 ave. (Regular, mid, & premium): $2.482</p>'
     document.getElementById("content6").innerHTML = ''
-    document.getElementById("link3").innerHTML = 'WH RELEASE'
-    document.getElementById("link3").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/13/fact-sheet-the-biden-harris-electric-vehicle-charging-action-plan/'
-    document.getElementById("time3").innerHTML = '12/13/21 1:09pm'
+    document.getElementById("link3").innerHTML = 'Find gas prices for your state here.'
+    document.getElementById("link3").href = 'https://gasprices.aaa.com/state-gas-price-averages/'
+    document.getElementById("time3").innerHTML = '12/13/21 1:51pm'
 
 }
