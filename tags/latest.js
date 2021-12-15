7bf6689be9795900d6d9fba35502e2141f4f2ee8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-15 11:11:57 -0700

    updates

--- tags/latest.js
+++ tags/latest.js
@@ -1,28 +1,30 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'NYPD has released the November crime statistics.'
-    document.getElementById("content1").innerHTML = '<p>-Nov 2021 crime was up 21.3% from Nov 2020.</p><p>-Crime in NYC is up 3.4% from 2020.</p><p>-Crime in NYC is down 11% from 2013.</p><p>-Crime in NYC is down 46% from 2000.</p>'
-    document.getElementById("content2").innerHTML = '<p>The NYPD released this information on the 8th of December.</p>'
+
+    document.getElementById("title1").innerHTML = 'Californias newest statewide mask mandate goes into effect today.'
+    document.getElementById("content1").innerHTML = '<p>-Mandated by CDPH (California Department of Public Health)</p><p>-Mandates that masks be worn in all indoor, public spaces</p><p>-Mandate ends January 15th, 2022</p>'
+    document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '12/15/21 10:50am'
+    document.getElementById("time1").innerHTML = '12/15/21 11:10am'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Biden agrees with the Uyghur Forced Labor Prevention Act. The admin has also imposed,'
-    document.getElementById("content3").innerHTML = '<p>-Financial sanctions</p><p>-Visa restrictions</p><p>-Import & export restrictions</p>'
-    document.getElementById("content4").innerHTML = ''
-    document.getElementById("link2").innerHTML = 'WH RELEASE'
-    document.getElementById("link2").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/14/statement-by-press-secretary-jen-psaki-on-the-uyghur-forced-labor-prevention-act/'
-    document.getElementById("time2").innerHTML = '12/14/21 7:54pm'
+    document.getElementById("title2").innerHTML = 'NYPD has released the November crime statistics.'
+    document.getElementById("content3").innerHTML = '<p>-Nov 2021 crime was up 21.3% from Nov 2020.</p><p>-Crime in NYC is up 3.4% from 2020.</p><p>-Crime in NYC is down 11% from 2013.</p><p>-Crime in NYC is down 46% from 2000.</p>'
+    document.getElementById("content4").innerHTML = '<p>The NYPD released this information on the 8th of December.</p>'
+    document.getElementById("link2").innerHTML = ''
+    document.getElementById("link2").href = ''
+    document.getElementById("time2").innerHTML = '12/15/21 10:50am'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'Today Governor Ron DeSantis said:'
-    document.getElementById("content5").innerHTML = '<p>“The devastation left in the wake of the tornado outbreak over the weekend is an unthinkable tragedy, and Casey and I are keeping the victims and all impacted individuals in our prayers as initial recovery efforts begin. The State of Florida has been communicating with our partners in the impacted states since early Saturday morning and we stand ready to provide any assistance needed during this time. We’ll continue to work with them to ensure they have the resources they need to respond.”</p>'
-    document.getElementById("content6").innerHTML = '<p>#kentuckytornado #news #openteanews </p>'
-    document.getElementById("link3").innerHTML = ''
-    document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/14/21 6:13pm'
+
+    document.getElementById("title3").innerHTML = 'Biden agrees with the Uyghur Forced Labor Prevention Act. The admin has also imposed,'
+    document.getElementById("content5").innerHTML = '<p>-Financial sanctions</p><p>-Visa restrictions</p><p>-Import & export restrictions</p>'
+    document.getElementById("content6").innerHTML = ''
+    document.getElementById("link3").innerHTML = 'WH RELEASE'
+    document.getElementById("link3").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/14/statement-by-press-secretary-jen-psaki-on-the-uyghur-forced-labor-prevention-act/'
+    document.getElementById("time3").innerHTML = '12/14/21 7:54pm'
 
 }
--- tags/older.js
+++ tags/older.js
@@ -1,21 +1,21 @@
 function older(){
     /* older 1 */
 
-    document.getElementById("otitle1").innerHTML = 'KY Gov. Beshear gave an update regarding the Dec 11th tornadoes.'
-    document.getElementById("ocontent1").innerHTML = '<p>-64 confirmed lives lost.</p><p>-18 of those lost have yet to be identified.</p><p>-105 individuals are still missing.</p>'
-    document.getElementById("ocontent2").innerHTML = '<p>-Approximately 1000 homes have been destroyed or damaged.</p><p>-Flags in KY will be flown half mast Tuesday through Monday.</p><p>-The KY relief fund has raised $4,009,817.71</p>'
-    document.getElementById("olink1").innerHTML = 'Donate to the relief fund.'
-    document.getElementById("olink1").href = 'https://secure.kentucky.gov/formservices/Finance/WKYRelief'
-    document.getElementById("otime1").innerHTML = '12/13/21 3:03pm'
+    document.getElementById("otitle1").innerHTML = 'Today Governor Ron DeSantis said:'
+    document.getElementById("ocontent1").innerHTML = '<p>“The devastation left in the wake of the tornado outbreak over the weekend is an unthinkable tragedy, and Casey and I are keeping the victims and all impacted individuals in our prayers as initial recovery efforts begin. The State of Florida has been communicating with our partners in the impacted states since early Saturday morning and we stand ready to provide any assistance needed during this time. We’ll continue to work with them to ensure they have the resources they need to respond.”</p>'
+    document.getElementById("ocontent2").innerHTML = '<p>#kentuckytornado #news #openteanews </p>'
+    document.getElementById("olink1").innerHTML = ''
+    document.getElementById("olink1").href = ''
+    document.getElementById("otime1").innerHTML = '12/14/21 6:13pm'
 
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'The average price of gas across the USA is up $1.184 from 2020.'
-    document.getElementById("ocontent3").innerHTML = '<p>Current ave. (Regular, mid, & premium): $3.666</p><p>2020 ave. (Regular, mid, & premium): $2.482</p>'
-    document.getElementById("ocontent4").innerHTML = ''
-    document.getElementById("olink2").innerHTML = 'Find gas prices for your state here.'
-    document.getElementById("olink2").href = 'https://gasprices.aaa.com/state-gas-price-averages/'
-    document.getElementById("otime2").innerHTML = '12/13/21 1:51pm'
+    document.getElementById("otitle2").innerHTML = 'KY Gov. Beshear gave an update regarding the Dec 11th tornadoes.'
+    document.getElementById("ocontent3").innerHTML = '<p>-64 confirmed lives lost.</p><p>-18 of those lost have yet to be identified.</p><p>-105 individuals are still missing.</p>'
+    document.getElementById("ocontent4").innerHTML = '<p>-Approximately 1000 homes have been destroyed or damaged.</p><p>-Flags in KY will be flown half mast Tuesday through Monday.</p><p>-The KY relief fund has raised $4,009,817.71</p>'
+    document.getElementById("olink2").innerHTML = 'Donate to the relief fund.'
+    document.getElementById("olink2").href = 'https://secure.kentucky.gov/formservices/Finance/WKYRelief'
+    document.getElementById("otime2").innerHTML = '12/13/21 3:03pm'
 
 
 }
