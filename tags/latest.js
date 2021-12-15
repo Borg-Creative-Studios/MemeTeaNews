b8537469d6c9cb7e3dd4eb52880057713fa0a9a3
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-15 10:51:22 -0700

    Updated the archives and latest

--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,28 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'Biden agrees with the Uyghur Forced Labor Prevention Act. The admin has also imposed,'
-    document.getElementById("content1").innerHTML = '<p>-Financial sanctions</p><p>-Visa restrictions</p><p>-Import & export restrictions</p>'
-    document.getElementById("content2").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'WH RELEASE'
-    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/14/statement-by-press-secretary-jen-psaki-on-the-uyghur-forced-labor-prevention-act/'
-    document.getElementById("time1").innerHTML = '12/14/21 7:54pm'
+    document.getElementById("title1").innerHTML = 'NYPD has released the November crime statistics.'
+    document.getElementById("content1").innerHTML = '<p>-Nov 2021 crime was up 21.3% from Nov 2020.</p><p>zcrime in NYC is up 3.4% from 2020.</p><p>-Crime in NYC is down 11% from 2013.</p><p>-Crime in NYC is down 46% from 2000.</p>'
+    document.getElementById("content2").innerHTML = '<p>The NYPD released this information on the 8th of December.</p>'
+    document.getElementById("link1").innerHTML = ''
+    document.getElementById("link1").href = ''
+    document.getElementById("time1").innerHTML = '12/15/21 10:50am'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Today Governor Ron DeSantis said:'
-    document.getElementById("content3").innerHTML = '<p>“The devastation left in the wake of the tornado outbreak over the weekend is an unthinkable tragedy, and Casey and I are keeping the victims and all impacted individuals in our prayers as initial recovery efforts begin. The State of Florida has been communicating with our partners in the impacted states since early Saturday morning and we stand ready to provide any assistance needed during this time. We’ll continue to work with them to ensure they have the resources they need to respond.”</p>'
-    document.getElementById("content4").innerHTML = '<p>#kentuckytornado #news #openteanews </p>'
-    document.getElementById("link2").innerHTML = ''
-    document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '12/14/21 6:13pm'
+    document.getElementById("title2").innerHTML = 'Biden agrees with the Uyghur Forced Labor Prevention Act. The admin has also imposed,'
+    document.getElementById("content3").innerHTML = '<p>-Financial sanctions</p><p>-Visa restrictions</p><p>-Import & export restrictions</p>'
+    document.getElementById("content4").innerHTML = ''
+    document.getElementById("link2").innerHTML = 'WH RELEASE'
+    document.getElementById("link2").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/14/statement-by-press-secretary-jen-psaki-on-the-uyghur-forced-labor-prevention-act/'
+    document.getElementById("time2").innerHTML = '12/14/21 7:54pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'Biden has announced a “historic release” from the Strategic Petroleum Reserve in an effort to reduce gas prices.'
-    document.getElementById("content5").innerHTML = '<p>-Prices are down $0.10 from last months peak.</p><p>-Prices are down $0.30 from this year’s peak.</p>'
-    document.getElementById("content6").innerHTML = '<p>-As of yesterday, the average price per gallon of fuel in the US was up $1.184 from 2020.</p><p>#gasprices #economy #news #openteanews</p>'
+    document.getElementById("title3").innerHTML = 'Today Governor Ron DeSantis said:'
+    document.getElementById("content5").innerHTML = '<p>“The devastation left in the wake of the tornado outbreak over the weekend is an unthinkable tragedy, and Casey and I are keeping the victims and all impacted individuals in our prayers as initial recovery efforts begin. The State of Florida has been communicating with our partners in the impacted states since early Saturday morning and we stand ready to provide any assistance needed during this time. We’ll continue to work with them to ensure they have the resources they need to respond.”</p>'
+    document.getElementById("content6").innerHTML = '<p>#kentuckytornado #news #openteanews </p>'
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/14/21 5:23pm'
-
+    document.getElementById("time3").innerHTML = '12/14/21 6:13pm'
 
 }
