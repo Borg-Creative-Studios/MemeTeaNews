1893cf6fd99d5599a70538686c498ca94987ab48
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-14 23:52:17 -0700

    update

--- tags/meme.js
+++ tags/meme.js
@@ -1,18 +1,18 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("mtitle1").innerHTML = 'Biden agrees with the Uyghur Forced Labor Prevention Act. The admin has also imposed,'
-    document.getElementById("mcontent1").innerHTML = '<p>-Financial sanctions</p><p>-Visa restrictions</p><p>-Import & export restrictions</p>'
+    document.getElementById("mtitle1").innerHTML = ''
+    document.getElementById("mcontent1").src = 'https://tea.borgcreative.com/memes/gasprices1.png'
     document.getElementById("mcontent2").innerHTML = ''
-    document.getElementById("mlink1").innerHTML = 'WH RELEASE'
-    document.getElementById("mlink1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/14/statement-by-press-secretary-jen-psaki-on-the-uyghur-forced-labor-prevention-act/'
-    document.getElementById("mtime1").innerHTML = '12/14/21 7:54pm'
+    document.getElementById("mlink1").innerHTML = ''
+    document.getElementById("mlink1").href = ''
+    document.getElementById("mtime1").innerHTML = '12/14/21 6:26pm'
 
     /* Recent 2 */
 
-    document.getElementById("mtitle2").innerHTML = 'Today Governor Ron DeSantis said:'
-    document.getElementById("mcontent3").innerHTML = '<p>“The devastation left in the wake of the tornado outbreak over the weekend is an unthinkable tragedy, and Casey and I are keeping the victims and all impacted individuals in our prayers as initial recovery efforts begin. The State of Florida has been communicating with our partners in the impacted states since early Saturday morning and we stand ready to provide any assistance needed during this time. We’ll continue to work with them to ensure they have the resources they need to respond.”</p>'
-    document.getElementById("mcontent4").innerHTML = '<p>#kentuckytornado #news #openteanews </p>'
+    document.getElementById("mtitle2").innerHTML = ''
+    document.getElementById("mcontent3").src = 'https://tea.borgcreative.com/memes/intro.png'
+    document.getElementById("mcontent4").innerHTML = ''
     document.getElementById("mlink2").innerHTML = ''
     document.getElementById("mlink2").href = ''
-    document.getElementById("mtime2").innerHTML = '12/14/21 6:13pm'
+    document.getElementById("mtime2").innerHTML = '12/12/21 3:52pm'
 }
