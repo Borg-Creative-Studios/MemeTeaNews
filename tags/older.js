3d54cf09e73fb4209b3634d4e13500d9eeee67b0
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-14 17:32:35 -0700

    Create older.js

---
+++ tags/older.js
@@ -0,0 +1,19 @@
+function older(){
+    /* older 1 */
+    document.getElementById("otitle1").innerHTML = 'Biden has announced a “historic release” from the Strategic Petroleum Reserve in an effort to reduce gas prices.'
+    document.getElementById("ocontent1").innerHTML = '<p>-Prices are down $0.10 from last months peak.</p><p>-Prices are down $0.30 from this year’s peak.</p>'
+    document.getElementById("ocontent2").innerHTML = '<p>-As of yesterday, the average price per gallon of fuel in the US was up $1.184 from 2020.</p><p>#gasprices #economy #news #openteanews</p>'
+    document.getElementById("olink1").innerHTML = ''
+    document.getElementById("olink1").href = ''
+    document.getElementById("otime1").innerHTML = '12/14/21 5:23pm'
+
+    /* older 2 */
+
+    document.getElementById("otitle2").innerHTML = 'KY Gov. Beshear gave an update regarding the Dec 11th tornadoes.'
+    document.getElementById("ocontent3").innerHTML = '<p>-64 confirmed lives lost.</p><p>-18 of those lost have yet to be identified.</p><p>-105 individuals are still missing.</p>'
+    document.getElementById("ocontent4").innerHTML = '<p>-Approximately 1000 homes have been destroyed or damaged.</p><p>-Flags in KY will be flown half mast Tuesday through Monday.</p><p>-The KY relief fund has raised $4,009,817.71</p>'
+    document.getElementById("olink2").innerHTML = 'FDonate to the relief fund.'
+    document.getElementById("olink2").href = 'https://secure.kentucky.gov/formservices/Finance/WKYRelief'
+    document.getElementById("otime2").innerHTML = '12/13/21 3:03pm'
+
+}
