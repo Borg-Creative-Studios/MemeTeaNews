68351bc312f325fccbc21f9f32be0f64bb9bc9f4
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-16 15:37:10 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,31 +1,31 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = '3 suspects have been arrested on suspicion of involvement in the deaths of 2 LA women who were left at separate hospitals.'
-    document.getElementById("content1").innerHTML = '<p>-Nov. 3rd, 2021: Cristy Giles and Hilda Marcela Cabrales Arzola were left at separate hospitals. Both victims were unconscious. Giles was declared deceased.</p><p>-Nov. 24th, 2021: Arzola passed away.</p><p>-Investigators believe both victims overdosed.</p>'
-    document.getElementById("content2").innerHTML = '<p>#news #la #openteanews #losangeles #crime </p>'
+    document.getElementById("title1").innerHTML = 'Gov. Beshear has given an update on the situation in KY.'
+    document.getElementById("content1").innerHTML = '<p>-Confirmed deaths: 74</p><p>-138 people were treated in ERs</p><p>The feds will cover %100 of the clean up costs for the first 30 days</p><p>-400+ FEMA personal on responding</p><p>-There are 16 individuals still missing</p><p>-3280 still do not have power</p><p>-The recovery fund balance: $14,800,437.39</p>'
+    document.getElementById("content2").innerHTML = '<p>#news #openteanews #kentucky #kentuckytornado </p>'
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '12/16/21 11:55am'
+    document.getElementById("time1").innerHTML = '12/16/21 3:37pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Californias newest statewide mask mandate goes into effect today.'
-    document.getElementById("content3").innerHTML = '<p>-Mandated by CDPH (California Department of Public Health)</p><p>-Mandates that masks be worn in all indoor, public spaces</p><p>-Mandate ends January 15th, 2022</p>'
-    document.getElementById("content4").innerHTML = ''
+    document.getElementById("title2").innerHTML = '3 suspects have been arrested on suspicion of involvement in the deaths of 2 LA women who were left at separate hospitals.'
+    document.getElementById("content3").innerHTML = '<p>-Nov. 3rd, 2021: Cristy Giles and Hilda Marcela Cabrales Arzola were left at separate hospitals. Both victims were unconscious. Giles was declared deceased.</p><p>-Nov. 24th, 2021: Arzola passed away.</p><p>-Investigators believe both victims overdosed.</p>'
+    document.getElementById("content4").innerHTML = '<p>#news #la #openteanews #losangeles #crime </p>'
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '12/15/21 11:10am'
+    document.getElementById("time2").innerHTML = '12/16/21 11:55am'
 
     /* Recent 3 */
 
-
-    document.getElementById("title3").innerHTML = 'NYPD has released the November crime statistics.'
-    document.getElementById("content5").innerHTML = '<p>-Nov 2021 crime was up 21.3% from Nov 2020.</p><p>-Crime in NYC is up 3.4% from 2020.</p><p>-Crime in NYC is down 11% from 2013.</p><p>-Crime in NYC is down 46% from 2000.</p>'
-    document.getElementById("content6").innerHTML = '<p>The NYPD released this information on the 8th of December.</p>'
+    document.getElementById("title3").innerHTML = 'Californias newest statewide mask mandate goes into effect today.'
+    document.getElementById("content5").innerHTML = '<p>-Mandated by CDPH (California Department of Public Health)</p><p>-Mandates that masks be worn in all indoor, public spaces</p><p>-Mandate ends January 15th, 2022</p>'
+    document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/15/21 10:50am'
+    document.getElementById("time3").innerHTML = '12/15/21 11:10am'
+
 
 
 }
