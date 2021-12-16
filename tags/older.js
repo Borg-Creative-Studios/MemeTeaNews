804f2875aca80eb5b1b842a7110f4052b64ebeae
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-16 12:03:06 -0700

    Updates

--- tags/older.js
+++ tags/older.js
@@ -1,21 +1,20 @@
 function older(){
     /* older 1 */
-
-    document.getElementById("otitle1").innerHTML = 'Today Governor Ron DeSantis said:'
-    document.getElementById("ocontent1").innerHTML = '<p>“The devastation left in the wake of the tornado outbreak over the weekend is an unthinkable tragedy, and Casey and I are keeping the victims and all impacted individuals in our prayers as initial recovery efforts begin. The State of Florida has been communicating with our partners in the impacted states since early Saturday morning and we stand ready to provide any assistance needed during this time. We’ll continue to work with them to ensure they have the resources they need to respond.”</p>'
-    document.getElementById("ocontent2").innerHTML = '<p>#kentuckytornado #news #openteanews </p>'
-    document.getElementById("olink1").innerHTML = ''
-    document.getElementById("olink1").href = ''
-    document.getElementById("otime1").innerHTML = '12/14/21 6:13pm'
+    document.getElementById("otitle1").innerHTML = 'Biden agrees with the Uyghur Forced Labor Prevention Act. The admin has also imposed,'
+    document.getElementById("ocontent1").innerHTML = '<p>-Financial sanctions</p><p>-Visa restrictions</p><p>-Import & export restrictions</p>'
+    document.getElementById("ocontent2").innerHTML = ''
+    document.getElementById("olink1").innerHTML = 'WH RELEASE'
+    document.getElementById("olink1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/14/statement-by-press-secretary-jen-psaki-on-the-uyghur-forced-labor-prevention-act/'
+    document.getElementById("otime1").innerHTML = '12/14/21 7:54pm'
 
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'KY Gov. Beshear gave an update regarding the Dec 11th tornadoes.'
-    document.getElementById("ocontent3").innerHTML = '<p>-64 confirmed lives lost.</p><p>-18 of those lost have yet to be identified.</p><p>-105 individuals are still missing.</p>'
-    document.getElementById("ocontent4").innerHTML = '<p>-Approximately 1000 homes have been destroyed or damaged.</p><p>-Flags in KY will be flown half mast Tuesday through Monday.</p><p>-The KY relief fund has raised $4,009,817.71</p>'
-    document.getElementById("olink2").innerHTML = 'Donate to the relief fund.'
-    document.getElementById("olink2").href = 'https://secure.kentucky.gov/formservices/Finance/WKYRelief'
-    document.getElementById("otime2").innerHTML = '12/13/21 3:03pm'
+    document.getElementById("otitle2").innerHTML = 'Today Governor Ron DeSantis said:'
+    document.getElementById("ocontent3").innerHTML = '<p>“The devastation left in the wake of the tornado outbreak over the weekend is an unthinkable tragedy, and Casey and I are keeping the victims and all impacted individuals in our prayers as initial recovery efforts begin. The State of Florida has been communicating with our partners in the impacted states since early Saturday morning and we stand ready to provide any assistance needed during this time. We’ll continue to work with them to ensure they have the resources they need to respond.”</p>'
+    document.getElementById("ocontent4").innerHTML = '<p>#kentuckytornado #news #openteanews </p>'
+    document.getElementById("olink2").innerHTML = ''
+    document.getElementById("olink2").href = ''
+    document.getElementById("otime2").innerHTML = '12/14/21 6:13pm'
 
 
 }
