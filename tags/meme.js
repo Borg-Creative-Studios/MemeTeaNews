afd6751ecf34ad6fc33178c74d5e33e9839c5b5b
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-16 16:31:47 -0700

    pondering meme

--- tags/meme.js
+++ tags/meme.js
@@ -1,18 +1,18 @@
 function memes(){
     /* Top most entry */
     document.getElementById("mtitle1").innerHTML = ''
-    document.getElementById("mcontent1").src = 'https://tea.borgcreative.com/memes/gasprices1.png'
+    document.getElementById("mcontent1").src = 'https://tea.borgcreative.com/memes/pondering1.png'
     document.getElementById("mcontent2").innerHTML = ''
     document.getElementById("mlink1").innerHTML = ''
     document.getElementById("mlink1").href = ''
-    document.getElementById("mtime1").innerHTML = '12/14/21 6:26pm'
+    document.getElementById("mtime1").innerHTML = '12/16/21 4:31pm'
 
     /* Recent 2 */
 
     document.getElementById("mtitle2").innerHTML = ''
-    document.getElementById("mcontent3").src = 'https://tea.borgcreative.com/memes/intro.jpeg'
+    document.getElementById("mcontent3").src = 'https://tea.borgcreative.com/memes/gasprices1.png'
     document.getElementById("mcontent4").innerHTML = ''
     document.getElementById("mlink2").innerHTML = ''
     document.getElementById("mlink2").href = ''
-    document.getElementById("mtime2").innerHTML = '12/12/21 3:52pm'
+    document.getElementById("mtime2").innerHTML = '12/14/21 6:26pm'
 }
