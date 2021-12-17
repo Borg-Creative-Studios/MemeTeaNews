30141fd3b5104de7bdd09a892c1b1c42b5de040c
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-17 10:27:54 -0700

    updates

--- tags/older.js
+++ tags/older.js
@@ -1,20 +1,21 @@
 function older(){
     /* older 1 */
-    document.getElementById("otitle1").innerHTML = 'Biden agrees with the Uyghur Forced Labor Prevention Act. The admin has also imposed,'
-    document.getElementById("ocontent1").innerHTML = '<p>-Financial sanctions</p><p>-Visa restrictions</p><p>-Import & export restrictions</p>'
+
+    document.getElementById("otitle1").innerHTML = 'Californias newest statewide mask mandate goes into effect today.'
+    document.getElementById("ocontent1").innerHTML = '<p>-Mandated by CDPH (California Department of Public Health)</p><p>-Mandates that masks be worn in all indoor, public spaces</p><p>-Mandate ends January 15th, 2022</p>'
     document.getElementById("ocontent2").innerHTML = ''
-    document.getElementById("olink1").innerHTML = 'WH RELEASE'
-    document.getElementById("olink1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/14/statement-by-press-secretary-jen-psaki-on-the-uyghur-forced-labor-prevention-act/'
-    document.getElementById("otime1").innerHTML = '12/14/21 7:54pm'
+    document.getElementById("olink1").innerHTML = ''
+    document.getElementById("olink1").href = ''
+    document.getElementById("otime1").innerHTML = '12/15/21 11:10am'
 
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'Today Governor Ron DeSantis said:'
-    document.getElementById("ocontent3").innerHTML = '<p>“The devastation left in the wake of the tornado outbreak over the weekend is an unthinkable tragedy, and Casey and I are keeping the victims and all impacted individuals in our prayers as initial recovery efforts begin. The State of Florida has been communicating with our partners in the impacted states since early Saturday morning and we stand ready to provide any assistance needed during this time. We’ll continue to work with them to ensure they have the resources they need to respond.”</p>'
-    document.getElementById("ocontent4").innerHTML = '<p>#kentuckytornado #news #openteanews </p>'
-    document.getElementById("olink2").innerHTML = ''
-    document.getElementById("olink2").href = ''
-    document.getElementById("otime2").innerHTML = '12/14/21 6:13pm'
+    document.getElementById("otitle2").innerHTML = 'Biden agrees with the Uyghur Forced Labor Prevention Act. The admin has also imposed,'
+    document.getElementById("ocontent3").innerHTML = '<p>-Financial sanctions</p><p>-Visa restrictions</p><p>-Import & export restrictions</p>'
+    document.getElementById("ocontent4").innerHTML = ''
+    document.getElementById("olink2").innerHTML = 'WH RELEASE'
+    document.getElementById("olink2").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/14/statement-by-press-secretary-jen-psaki-on-the-uyghur-forced-labor-prevention-act/'
+    document.getElementById("otime2").innerHTML = '12/14/21 7:54pm'
 
 
 }
