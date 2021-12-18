e033ef64bb76b186b79e9f8df2f4fd1d76949e0e
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-17 19:17:34 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,29 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Operation Allies Welcome'
-    document.getElementById("content1").innerHTML = '<p>-The WH confirmed this morning that ~43,000 Afghans have resettled in the US</p>'
-    document.getElementById("content2").innerHTML = '<p>-The individuals have been resettled in 46 states</p>'
+    document.getElementById("title1").innerHTML = '2.9% of SARS-COV-19 cases in the USA are of the Omnicron Variant.'
+    document.getElementById("content1").innerHTML = ''
+    document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '12/17/21 10:27am'
+    document.getElementById("time1").innerHTML = '12/17/21 6:04pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Gov. Beshear has given an update on the situation in KY.'
-    document.getElementById("content3").innerHTML = '<p>-Confirmed deaths: 74</p><p>-138 people were treated in ERs</p><p>-The feds will cover %100 of the clean up costs for the first 30 days</p><p>-400+ FEMA personal are responding</p><p>-There are 16 individuals still missing</p><p>-3280 still do not have power</p><p>-The recovery fund balance: $14,800,437.39</p>'
-    document.getElementById("content4").innerHTML = '<p>#news #openteanews #kentucky #kentuckytornado </p>'
+    document.getElementById("title2").innerHTML = 'Department of Homeland Security has joined several agencies, including FEMA, in aiding KY post the tornadoes.'
+    document.getElementById("content3").innerHTML = ''
+    document.getElementById("content4").innerHTML = ''
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '12/16/21 3:37pm'
+    document.getElementById("time2").innerHTML = '12/17/21 5:48pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = '3 suspects have been arrested on suspicion of involvement in the deaths of 2 LA women who were left at separate hospitals.'
-    document.getElementById("content5").innerHTML = '<p>-Nov. 3rd, 2021: Cristy Giles and Hilda Marcela Cabrales Arzola were left at separate hospitals. Both victims were unconscious. Giles was declared deceased.</p><p>-Nov. 24th, 2021: Arzola passed away.</p><p>-Investigators believe both victims overdosed.</p>'
-    document.getElementById("content6").innerHTML = '<p>#news #la #openteanews #losangeles #crime </p>'
+    document.getElementById("title3").innerHTML = 'Operation Allies Welcome'
+    document.getElementById("content5").innerHTML = '<p>-The WH confirmed this morning that ~43,000 Afghans have resettled in the US</p>'
+    document.getElementById("content6").innerHTML = '<p>-The individuals have been resettled in 46 states</p>'
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/16/21 11:55'
+    document.getElementById("time3").innerHTML = '12/17/21 10:27am'
 
 }
