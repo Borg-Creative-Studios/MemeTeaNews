5e57b74eed72f809815c0fff17f672415489c82c
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-20 13:02:14 -0700

    update

--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,29 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = '2.9% of SARS-COV-19 cases in the USA are of the Omnicron Variant.'
-    document.getElementById("content1").innerHTML = ''
+    document.getElementById("title1").innerHTML = 'National Security Advisor Jake Sullivan had a call with the Foreign Policy Advisor to the president of Russia, Yuriy Ushakov.'
+    document.getElementById("content1").innerHTML = '-Sullivan indicated that the US is prepared for diplomacy through several channels'
     document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '12/17/21 6:04pm'
+    document.getElementById("time1").innerHTML = '12/20/21 12:53pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Department of Homeland Security has joined several agencies, including FEMA, in aiding KY post the tornadoes.'
+    document.getElementById("title2").innerHTML = '2.9% of SARS-COV-19 cases in the USA are of the Omnicron Variant.'
     document.getElementById("content3").innerHTML = ''
     document.getElementById("content4").innerHTML = ''
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '12/17/21 5:48pm'
+    document.getElementById("time2").innerHTML = '12/17/21 6:04pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'Operation Allies Welcome'
-    document.getElementById("content5").innerHTML = '<p>-The WH confirmed this morning that ~43,000 Afghans have resettled in the US</p>'
-    document.getElementById("content6").innerHTML = '<p>-The individuals have been resettled in 46 states</p>'
+    document.getElementById("title3").innerHTML = 'Department of Homeland Security has joined several agencies, including FEMA, in aiding KY post the tornadoes.'
+    document.getElementById("content5").innerHTML = ''
+    document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/17/21 10:27am'
+    document.getElementById("time3").innerHTML = '12/17/21 5:48pm'
 
 }
