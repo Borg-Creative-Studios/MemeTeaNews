057145b5e8cfc700c40c06beeca4dab20a86e9ed
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-21 17:39:38 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,29 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'National Security Advisor Jake Sullivan had a call with the Foreign Policy Advisor to the president of Russia, Yuriy Ushakov.'
-    document.getElementById("content1").innerHTML = '-Sullivan indicated that the US is prepared for diplomacy through several channels'
-    document.getElementById("content2").innerHTML = ''
-    document.getElementById("link1").innerHTML = ''
-    document.getElementById("link1").href = ''
+    document.getElementById("title1").innerHTML = 'POTUS has approved Alabama disaster declaration.'
+    document.getElementById("content1").innerHTML = '<p>-Oct. 6 & Oct. 7 experienced severe storms</p><p>-This declaration makes federal aid available to impacted people in Jefferson and Shelby counties</p>'
+    document.getElementById("content2").innerHTML = '<p>-The aid can be used towards housing, uninsured property loss, & and more</p><p>#news #openteanews #alabama #potus </p>'
+    document.getElementById("link1").innerHTML = 'You can find the available resources here'
+    document.getElementById("link1").href = 'https://www.disasterassistance.gov/'
     document.getElementById("time1").innerHTML = '12/20/21 12:53pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = '2.9% of SARS-COV-19 cases in the USA are of the Omnicron Variant.'
-    document.getElementById("content3").innerHTML = ''
+    document.getElementById("title2").innerHTML = 'National Security Advisor Jake Sullivan had a call with the Foreign Policy Advisor to the president of Russia, Yuriy Ushakov.'
+    document.getElementById("content3").innerHTML = '-Sullivan indicated that the US is prepared for diplomacy through several channels'
     document.getElementById("content4").innerHTML = ''
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '12/17/21 6:04pm'
+    document.getElementById("time2").innerHTML = '12/20/21 12:53pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'Department of Homeland Security has joined several agencies, including FEMA, in aiding KY post the tornadoes.'
+    document.getElementById("title3").innerHTML = '2.9% of SARS-COV-19 cases in the USA are of the Omnicron Variant.'
     document.getElementById("content5").innerHTML = ''
     document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/17/21 5:48pm'
+    document.getElementById("time3").innerHTML = '12/17/21 6:04pm'
 
 }
