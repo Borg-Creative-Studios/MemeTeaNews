18537fd4d857e6c8629986f13ed5945d890309d8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-22 11:58:25 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,30 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'KY AG challenges the Head Start Vaccine Mandate'
-    document.getElementById("content1").innerHTML = '<p>-The lawsuit was filed with 23 other states</p><p>-The challenge is that the mandate is unconstitutional & unlawful</p><p>-The mandate is aimed towards staff and volunteers of federal Head Start programs</p>'
-    document.getElementById("content2").innerHTML = '<p>-The KY Head Start programs serve 17,000+ children</p><p>- ~17% of staff have not complied with the mandate</p>'
+    document.getElementById("title1").innerHTML = 'Tomorrow marks the 50th anniversary of the National Cancer act of 1971.'
+    document.getElementById("content1").innerHTML = ''
+    document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '12/21/21 5:51pm'
+    document.getElementById("time1").innerHTML = '12/22/21 11:58am'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'POTUS has approved Alabama disaster declaration.'
-    document.getElementById("content3").innerHTML = '<p>-Oct. 6 & Oct. 7 experienced severe storms</p><p>-This declaration makes federal aid available to impacted people in Jefferson and Shelby counties</p>'
-    document.getElementById("content4").innerHTML = '<p>-The aid can be used towards housing, uninsured property loss, & and more</p><p>#news #openteanews #alabama #potus </p>'
-    document.getElementById("link2").innerHTML = 'You can find the available resources here'
-    document.getElementById("link2").href = 'https://www.disasterassistance.gov/'
-    document.getElementById("time2").innerHTML = '12/21/21 5:36pm'
+    document.getElementById("title2").innerHTML = 'KY AG challenges the Head Start Vaccine Mandate'
+    document.getElementById("content3").innerHTML = '<p>-The lawsuit was filed with 23 other states</p><p>-The challenge is that the mandate is unconstitutional & unlawful</p><p>-The mandate is aimed towards staff and volunteers of federal Head Start programs</p>'
+    document.getElementById("content4").innerHTML = '<p>-The KY Head Start programs serve 17,000+ children</p><p>- ~17% of staff have not complied with the mandate</p>'
+    document.getElementById("link2").innerHTML = ''
+    document.getElementById("link2").href = ''
+    document.getElementById("time2").innerHTML = '12/21/21 5:51pm'
+
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'National Security Advisor Jake Sullivan had a call with the Foreign Policy Advisor to the president of Russia, Yuriy Ushakov.'
-    document.getElementById("content5").innerHTML = '-Sullivan indicated that the US is prepared for diplomacy through several channels'
-    document.getElementById("content6").innerHTML = ''
-    document.getElementById("link3").innerHTML = ''
-    document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/20/21 12:53pm'
+    document.getElementById("title3").innerHTML = 'POTUS has approved Alabama disaster declaration.'
+    document.getElementById("content5").innerHTML = '<p>-Oct. 6 & Oct. 7 experienced severe storms</p><p>-This declaration makes federal aid available to impacted people in Jefferson and Shelby counties</p>'
+    document.getElementById("content6").innerHTML = '<p>-The aid can be used towards housing, uninsured property loss, & and more</p><p>#news #openteanews #alabama #potus </p>'
+    document.getElementById("link3").innerHTML = 'You can find the available resources here'
+    document.getElementById("link3").href = 'https://www.disasterassistance.gov/'
+    document.getElementById("time3").innerHTML = '12/21/21 5:36pm'
 
 }
