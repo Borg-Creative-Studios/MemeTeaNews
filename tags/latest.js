66fc698ce5d2fd3da6407c98e1f46361f2f04038
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-25 17:58:35 -0800

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,30 +1,30 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Tomorrow marks the 50th anniversary of the National Cancer act of 1971.'
-    document.getElementById("content1").innerHTML = ''
+    document.getElementById("title1").innerHTML = 'Excerpt from POTUS’ Christmas statement:'
+    document.getElementById("content1").innerHTML = '“On behalf of our family, we wish a merry Christmas to everyone in the United States and around the world who is celebrating the birth of Jesus Christ and the promise of hope and renewal.”'
     document.getElementById("content2").innerHTML = ''
-    document.getElementById("link1").innerHTML = ''
-    document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '12/22/21 11:58am'
+    document.getElementById("link1").innerHTML = 'WH Statement'
+    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/25/statement-by-president-joe-biden-and-first-lady-jill-biden-on-christmas/'
+    document.getElementById("time1").innerHTML = '12/25/21 5:11pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'KY AG challenges the Head Start Vaccine Mandate'
-    document.getElementById("content3").innerHTML = '<p>-The lawsuit was filed with 23 other states</p><p>-The challenge is that the mandate is unconstitutional & unlawful</p><p>-The mandate is aimed towards staff and volunteers of federal Head Start programs</p>'
-    document.getElementById("content4").innerHTML = '<p>-The KY Head Start programs serve 17,000+ children</p><p>- ~17% of staff have not complied with the mandate</p>'
+    document.getElementById("title2").innerHTML = 'Merry Christmas!'
+    document.getElementById("content3").innerHTML = ''
+    document.getElementById("content4").innerHTML = ''
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '12/21/21 5:51pm'
+    document.getElementById("time2").innerHTML = '12/25/21 5:06'
 
 
     /* Recent 3 */
-
-    document.getElementById("title3").innerHTML = 'POTUS has approved Alabama disaster declaration.'
-    document.getElementById("content5").innerHTML = '<p>-Oct. 6 & Oct. 7 experienced severe storms</p><p>-This declaration makes federal aid available to impacted people in Jefferson and Shelby counties</p>'
-    document.getElementById("content6").innerHTML = '<p>-The aid can be used towards housing, uninsured property loss, & and more</p><p>#news #openteanews #alabama #potus </p>'
-    document.getElementById("link3").innerHTML = 'You can find the available resources here'
-    document.getElementById("link3").href = 'https://www.disasterassistance.gov/'
-    document.getElementById("time3").innerHTML = '12/21/21 5:36pm'
+    
+    document.getElementById("title3").innerHTML = 'Tomorrow marks the 50th anniversary of the National Cancer act of 1971.'
+    document.getElementById("content5").innerHTML = ''
+    document.getElementById("content6").innerHTML = ''
+    document.getElementById("link3").innerHTML = ''
+    document.getElementById("link3").href = ''
+    document.getElementById("time3").innerHTML = '12/22/21 11:58am'
 
 }
