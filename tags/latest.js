4b75d7e24de6492cca06629e6e7171ed12a8f86f
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-26 14:26:23 -0800

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,30 +1,30 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Excerpt from POTUS’ Christmas statement:'
-    document.getElementById("content1").innerHTML = '“On behalf of our family, we wish a merry Christmas to everyone in the United States and around the world who is celebrating the birth of Jesus Christ and the promise of hope and renewal.”'
+    document.getElementById("title1").innerHTML = 'Teams from KYTC are being dispatched to Hopkins County, KY to aid tornado victims in replacing IDs and drivers licenses.'
+    document.getElementById("content1").innerHTML = '#news #openteanews #kentuckytornado #kentucky'
     document.getElementById("content2").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'WH Statement'
-    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/25/statement-by-president-joe-biden-and-first-lady-jill-biden-on-christmas/'
-    document.getElementById("time1").innerHTML = '12/25/21 5:11pm'
+    document.getElementById("link1").innerHTML = ''
+    document.getElementById("link1").href = ''
+    document.getElementById("time1").innerHTML = '12/26/21 2:21pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Merry Christmas!'
-    document.getElementById("content3").innerHTML = ''
+    document.getElementById("title2").innerHTML = 'Excerpt from POTUS’ Christmas statement:'
+    document.getElementById("content3").innerHTML = '“On behalf of our family, we wish a merry Christmas to everyone in the United States and around the world who is celebrating the birth of Jesus Christ and the promise of hope and renewal.”'
     document.getElementById("content4").innerHTML = ''
-    document.getElementById("link2").innerHTML = ''
-    document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '12/25/21 5:06'
+    document.getElementById("link2").innerHTML = 'WH Statement'
+    document.getElementById("link2").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/25/statement-by-president-joe-biden-and-first-lady-jill-biden-on-christmas/'
+    document.getElementById("time2").innerHTML = '12/25/21 5:11pm'
 
 
     /* Recent 3 */
     
-    document.getElementById("title3").innerHTML = 'Tomorrow marks the 50th anniversary of the National Cancer act of 1971.'
+    document.getElementById("title3").innerHTML = 'Merry Christmas!'
     document.getElementById("content5").innerHTML = ''
     document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/22/21 11:58am'
+    document.getElementById("time3").innerHTML = '12/25/21 5:06'
 
 }
