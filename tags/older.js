0fc93effbc8de401b3fae7c1d184bb72e6c00f67
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-26 14:27:25 -0800

    Update older.js

--- tags/older.js
+++ tags/older.js
@@ -12,12 +12,12 @@ function older(){
 
     /* older 2 */
 
-    document.getElementById("otitle1").innerHTML = 'Operation Allies Welcome'
-    document.getElementById("ocontent1").innerHTML = '<p>-The WH confirmed this morning that ~43,000 Afghans have resettled in the US</p>'
-    document.getElementById("ocontent2").innerHTML = '<p>-The individuals have been resettled in 46 states</p>'
-    document.getElementById("olink1").innerHTML = ''
-    document.getElementById("olink1").href = ''
-    document.getElementById("otime1").innerHTML = '12/17/21 10:27am'
+    document.getElementById("otitle2").innerHTML = 'Operation Allies Welcome'
+    document.getElementById("ocontent3").innerHTML = '<p>-The WH confirmed this morning that ~43,000 Afghans have resettled in the US</p>'
+    document.getElementById("ocontent4").innerHTML = '<p>-The individuals have been resettled in 46 states</p>'
+    document.getElementById("olink2").innerHTML = ''
+    document.getElementById("olink2").href = ''
+    document.getElementById("otime2").innerHTML = '12/17/21 10:27am'
 
 
 }
