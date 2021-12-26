566cf390ae15b0ccba7e7722fa3494c7b5c9d677
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-26 14:23:23 -0800

    Update older.js

--- tags/older.js
+++ tags/older.js
@@ -1,21 +1,23 @@
 function older(){
     /* older 1 */
 
+    document.getElementById("otitle1").innerHTML = 'Tomorrow marks the 50th anniversary of the National Cancer act of 1971.'
+    document.getElementById("ocontent1").innerHTML = ''
+    document.getElementById("ocontent2").innerHTML = ''
+    document.getElementById("olink1").innerHTML = ''
+    document.getElementById("olink1").href = ''
+    document.getElementById("otime1").innerHTML = '12/22/21 11:58am'
+    
+    
+
+    /* older 2 */
+
     document.getElementById("otitle1").innerHTML = 'Operation Allies Welcome'
     document.getElementById("ocontent1").innerHTML = '<p>-The WH confirmed this morning that ~43,000 Afghans have resettled in the US</p>'
     document.getElementById("ocontent2").innerHTML = '<p>-The individuals have been resettled in 46 states</p>'
     document.getElementById("olink1").innerHTML = ''
     document.getElementById("olink1").href = ''
     document.getElementById("otime1").innerHTML = '12/17/21 10:27am'
-
-    /* older 2 */
-
-    document.getElementById("otitle2").innerHTML = 'Californias newest statewide mask mandate goes into effect today.'
-    document.getElementById("ocontent3").innerHTML = '<p>-Mandated by CDPH (California Department of Public Health)</p><p>-Mandates that masks be worn in all indoor, public spaces</p><p>-Mandate ends January 15th, 2022</p>'
-    document.getElementById("ocontent4").innerHTML = ''
-    document.getElementById("olink2").innerHTML = ''
-    document.getElementById("olink2").href = ''
-    document.getElementById("otime2").innerHTML = '12/15/21 11:10am'
 
 
 }
