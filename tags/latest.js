cbc06fce7e63eafbe373478aa81e0b7fea9e54a0
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-31 15:15:29 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,8 +1,8 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Betty White has passed away at the age of 99.'
-    document.getElementById("content1").innerHTML = ''
+    document.getElementById("title1").innerHTML = 'Biden calls Gov Jared Polis'
+    document.getElementById("content1").innerHTML = '<p>-The two discussed the recent wild fires in CO</p><p>-FEMA is on the ground assisting</p>'
     document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
@@ -10,22 +10,21 @@ function latest(){
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Seacrest Studios at the National Childrens Hospital.'
-    document.getElementById("content3").innerHTML = '<p>As seen in popular social posts and in several broadcasts, “Seacrest Studios” is visible in a shot while POTUS was visiting National Childrens Hospital. Seacrest Studios is part of the Ryan Seacrest Foundation, a nonprofit, and builds broadcast studios in pediatric centers to teach kids about radio, film and other forms of broadcasting while they are receiving treatment.</p>'
+    document.getElementById("title2").innerHTML = 'Betty White has passed away at the age of 99.'
+    document.getElementById("content3").innerHTML = ''
     document.getElementById("content4").innerHTML = ''
-    document.getElementById("link2").innerHTML = 'Seacrest Foundation'
-    document.getElementById("link2").href = 'https://www.ryanseacrestfoundation.org/'
-    document.getElementById("time2").innerHTML = '12/25/21 5:11pm'
-
+    document.getElementById("link2").innerHTML = ''
+    document.getElementById("link2").href = ''
+    document.getElementById("time2").innerHTML = '12/31/21 11:53am'
 
     /* Recent 3 */
     
-    document.getElementById("title3").innerHTML = 'Teams from KYTC are being dispatched to Hopkins County, KY to aid tornado victims in replacing IDs and drivers licenses.'
-    document.getElementById("content5").innerHTML = ''
+    document.getElementById("title3").innerHTML = 'Seacrest Studios at the National Childrens Hospital.'
+    document.getElementById("content5").innerHTML = '<p>As seen in popular social posts and in several broadcasts, “Seacrest Studios” is visible in a shot while POTUS was visiting National Childrens Hospital. Seacrest Studios is part of the Ryan Seacrest Foundation, a nonprofit, and builds broadcast studios in pediatric centers to teach kids about radio, film and other forms of broadcasting while they are receiving treatment.</p>'
     document.getElementById("content6").innerHTML = ''
-    document.getElementById("link3").innerHTML = ''
-    document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/26/21 3:21pm'
+    document.getElementById("link3").innerHTML = 'Seacrest Foundation'
+    document.getElementById("link3").href = 'https://www.ryanseacrestfoundation.org/'
+    document.getElementById("time3").innerHTML = '12/25/21 5:11pm'
 
 }
 
