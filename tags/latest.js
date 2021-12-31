30f140889b59a1abd4e7b43a3b3bc1bb18d11614
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-12-31 14:27:01 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,31 +1,31 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Teams from KYTC are being dispatched to Hopkins County, KY to aid tornado victims in replacing IDs and drivers licenses.'
-    document.getElementById("content1").innerHTML = '#news #openteanews #kentuckytornado #kentucky'
+    document.getElementById("title1").innerHTML = 'Betty White has passed away at the age of 99.'
+    document.getElementById("content1").innerHTML = ''
     document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '12/26/21 2:21pm'
+    document.getElementById("time1").innerHTML = '12/31/21 11:53am'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Excerpt from POTUS’ Christmas statement:'
-    document.getElementById("content3").innerHTML = '“On behalf of our family, we wish a merry Christmas to everyone in the United States and around the world who is celebrating the birth of Jesus Christ and the promise of hope and renewal.”'
+    document.getElementById("title2").innerHTML = 'Seacrest Studios at the National Childrens Hospital.'
+    document.getElementById("content3").innerHTML = '<p>As seen in popular social posts and in several broadcasts, “Seacrest Studios” is visible in a shot while POTUS was visiting National Childrens Hospital. Seacrest Studios is part of the Ryan Seacrest Foundation, a nonprofit, and builds broadcast studios in pediatric centers to teach kids about radio, film and other forms of broadcasting while they are receiving treatment.</p>'
     document.getElementById("content4").innerHTML = ''
-    document.getElementById("link2").innerHTML = 'WH Statement'
-    document.getElementById("link2").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2021/12/25/statement-by-president-joe-biden-and-first-lady-jill-biden-on-christmas/'
+    document.getElementById("link2").innerHTML = 'Seacrest Foundation'
+    document.getElementById("link2").href = 'https://www.ryanseacrestfoundation.org/'
     document.getElementById("time2").innerHTML = '12/25/21 5:11pm'
 
 
     /* Recent 3 */
     
-    document.getElementById("title3").innerHTML = 'Merry Christmas!'
+    document.getElementById("title3").innerHTML = 'Teams from KYTC are being dispatched to Hopkins County, KY to aid tornado victims in replacing IDs and drivers licenses.'
     document.getElementById("content5").innerHTML = ''
     document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/25/21 5:06'
+    document.getElementById("time3").innerHTML = '12/26/21 3:21pm'
 
 }
 
