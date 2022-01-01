b5dbea3a6d11c4fb2553edff9a5a589585fe28ca
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-01 12:12:34 -0700

    update

--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,29 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'POTUS approves CO disaster decalaration.'
-    document.getElementById("content1").innerHTML = '<p>-This will allow additional federal aid to support those impacted by the wild fires</p>'
+    document.getElementById("title1").innerHTML = 'DuckDuckGo has announced that it is developing a desktop web browser.'
+    document.getElementById("content1").innerHTML = ''
     document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '12/31/21 2:15pm'
+    document.getElementById("time1").innerHTML = '1/1/22 12:10pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Biden calls Gov Jared Polis'
-    document.getElementById("content3").innerHTML = '<p>-The two discussed the recent wild fires in CO</p><p>-FEMA is on the ground assisting</p>'
+    document.getElementById("title2").innerHTML = 'POTUS approves CO disaster decalaration.'
+    document.getElementById("content3").innerHTML = '<p>-This will allow additional federal aid to support those impacted by the wild fires</p>'
     document.getElementById("content4").innerHTML = ''
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '12/31/21 2:15pm'
+    document.getElementById("time2").innerHTML = '1/1/22 11:12am'
 
     /* Recent 3 */
-    
-    document.getElementById("title3").innerHTML = 'Betty White has passed away at the age of 99.'
-    document.getElementById("content5").innerHTML = ''
+    document.getElementById("title3").innerHTML = 'Biden calls Gov Jared Polis'
+    document.getElementById("content5").innerHTML = '<p>-The two discussed the recent wild fires in CO</p><p>-FEMA is on the ground assisting</p>'
     document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '12/31/21 11:53am'
+    document.getElementById("time3").innerHTML = '12/31/21 2:15pm'
+
 }
 
--- tags/older.js
+++ tags/older.js
@@ -1,22 +1,20 @@
 function older(){
     /* older 1 */
 
-    document.getElementById("otitle1").innerHTML = 'Seacrest Studios at the National Childrens Hospital.'
-    document.getElementById("ocontent1").innerHTML = '<p>As seen in popular social posts and in several broadcasts, “Seacrest Studios” is visible in a shot while POTUS was visiting National Childrens Hospital. Seacrest Studios is part of the Ryan Seacrest Foundation, a nonprofit, and builds broadcast studios in pediatric centers to teach kids about radio, film and other forms of broadcasting while they are receiving treatment.</p>'
+    document.getElementById("otitle1").innerHTML = 'Betty White has passed away at the age of 99.'
+    document.getElementById("ocontent1").innerHTML = ''
     document.getElementById("ocontent2").innerHTML = ''
-    document.getElementById("olink1").innerHTML = 'Seacrest Foundation'
-    document.getElementById("olink1").href = 'https://www.ryanseacrestfoundation.org/'
-    document.getElementById("otime1").innerHTML = '12/25/21 5:11pm'
-
+    document.getElementById("olink1").innerHTML = ''
+    document.getElementById("olink1").href = ''
+    document.getElementById("otime1").innerHTML = '12/31/21 11:53am'
 
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'Operation Allies Welcome'
-    document.getElementById("ocontent3").innerHTML = '<p>-The WH confirmed this morning that ~43,000 Afghans have resettled in the US</p>'
-    document.getElementById("ocontent4").innerHTML = '<p>-The individuals have been resettled in 46 states</p>'
-    document.getElementById("olink2").innerHTML = ''
-    document.getElementById("olink2").href = ''
-    document.getElementById("otime2").innerHTML = '12/17/21 10:27am'
-
+    document.getElementById("otitle2").innerHTML = 'Seacrest Studios at the National Childrens Hospital.'
+    document.getElementById("ocontent3").innerHTML = '<p>As seen in popular social posts and in several broadcasts, “Seacrest Studios” is visible in a shot while POTUS was visiting National Childrens Hospital. Seacrest Studios is part of the Ryan Seacrest Foundation, a nonprofit, and builds broadcast studios in pediatric centers to teach kids about radio, film and other forms of broadcasting while they are receiving treatment.</p>'
+    document.getElementById("ocontent4").innerHTML = ''
+    document.getElementById("olink2").innerHTML = 'Seacrest Foundation'
+    document.getElementById("olink2").href = 'https://www.ryanseacrestfoundation.org/'
+    document.getElementById("otime2").innerHTML = '12/25/21 5:11pm'
 
 }
