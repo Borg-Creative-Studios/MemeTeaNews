087423a30bf9c0c202899a4e79763a32ed83efc9
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-01 11:39:18 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'FEMA disaster relief resources can be found'
-    document.getElementById("alink").innerHTML = ' here.'
-    document.getElementById("alink").href = 'https://www.fema.gov/assistance/individual'
+    document.getElementById("acontent1").innerHTML = 'Happy new year! :D'
+    document.getElementById("alink").innerHTML = ''
+    document.getElementById("alink").href = ''
 }
