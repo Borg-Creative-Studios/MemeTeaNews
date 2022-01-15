257bec6bfd32ef153d4cb22b4bab74bda7028da7
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-15 15:35:42 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,29 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Iran imposed sanctions against 51 Americans.'
-    document.getElementById("content1").innerHTML = '<p>-National Security Advisor stated: “We will work with our allies and partners to deter and respond to any attacks carried out by Iran. Should Iran attack any of our nationals, including any of the 51 people named yesterday, it will face severe consequences.”</p>'
-    document.getElementById("content2").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'Read the WH report.'
-    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2022/01/09/statement-by-national-security-advisor-jake-sullivan-on-iranian-threats-and-provocations-against-american-citizens/'
-    document.getElementById("time1").innerHTML = '1/9/22 12:38pm'
+    document.getElementById("title1").innerHTML = 'Hostage situation in Colleyville Texas.'
+    document.getElementById("content1").innerHTML = '<p>-SWAT is responding</p>'
+    document.getElementById("content2").innerHTML = '<p>-This is an active situation</p>'
+    document.getElementById("link1").innerHTML = ''
+    document.getElementById("link1").href = ''
+    document.getElementById("time1").innerHTML = '1/15/22 3:32pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'DuckDuckGo has announced that it is developing a desktop web browser.'
-    document.getElementById("content3").innerHTML = ''
+    document.getElementById("title2").innerHTML = 'SCOTUS blocks OSHA #vaccinemandate '
+    document.getElementById("content3").innerHTML = '<p>#news #openteanews</p>'
     document.getElementById("content4").innerHTML = ''
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '1/1/22 12:10pm'
+    document.getElementById("time2").innerHTML = '1/13/22 12:56pm'
 
     /* Recent 3 */
-    document.getElementById("title3").innerHTML = 'POTUS approves CO disaster decalaration.'
-    document.getElementById("content5").innerHTML = '<p>-This will allow additional federal aid to support those impacted by the wild fires</p>'
+    document.getElementById("title3").innerHTML = 'Bob Saget has passed away at the age of 65'
+    document.getElementById("content5").innerHTML = ''
     document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '1/1/22 11:12am'
+    document.getElementById("time3").innerHTML = '1/9/22 6:22pm'
 
 }
 
