6bed98855e08de02f938b3cc66b049e457c6e395
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-16 11:46:58 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = ''
+    document.getElementById("acontent1").innerHTML = 'Storm system bringing snow & ice to Eastern USA.'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
