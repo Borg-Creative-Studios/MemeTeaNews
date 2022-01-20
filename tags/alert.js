e920c49d4b44c3766b46c40bf5b1ed71b9f8437a
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-20 11:59:14 -0700

    update

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Storm system bringing snow & ice to Eastern USA.'
+    document.getElementById("acontent1").innerHTML = 'Happy Thursday! :D'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
--- tags/latest.js
+++ tags/latest.js
@@ -1,29 +1,31 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Hostage situation in Colleyville Texas.'
-    document.getElementById("content1").innerHTML = '<p>-SWAT is responding</p> <p>-This is an active situation</p>'
-    document.getElementById("content2").innerHTML = '<p>-There were 4 hostages</p> <p>-The situation was reported at 10:41 am</p><p>-The 4 hostages have been freed, unharmed</p><p>-The suspect has been killed by law enforcement</p> <p>-POTUS remarked: "This was an act of terror"</p>'
+    document.getElementById("title1").innerHTML = 'Biden has signed H.R. 1192 into law.'
+    document.getElementById("content1").innerHTML = '<p>-H.R. 1192 is named, “Puerto Rico Recovery Accuracy in Disclosures Act of 2021”</p>'
+    document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '1/15/22 3:32pm | Updated 1/15/22 5:23pm | Updated 1/15/22 6:25pm | Updated 1/16/22 11:28am'
+    document.getElementById("time1").innerHTML = '1/20/22 11:56am'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'SCOTUS blocks OSHA #vaccinemandate '
-    document.getElementById("content3").innerHTML = '<p>#news #openteanews</p>'
-    document.getElementById("content4").innerHTML = ''
+    document.getElementById("title2").innerHTML = 'Hostage situation in Colleyville Texas.'
+    document.getElementById("content3").innerHTML = '<p>-SWAT is responding</p> <p>-This is an active situation</p>'
+    document.getElementById("content4").innerHTML = '<p>-There were 4 hostages</p> <p>-The situation was reported at 10:41 am</p><p>-The 4 hostages have been freed, unharmed</p><p>-The suspect has been killed by law enforcement</p> <p>-POTUS remarked: "This was an act of terror"</p>'
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '1/13/22 12:56pm'
+    document.getElementById("time2").innerHTML = '1/15/22 3:32pm | Updated 1/15/22 5:23pm | Updated 1/15/22 6:25pm | Updated 1/16/22 11:28am'
+
 
     /* Recent 3 */
-    document.getElementById("title3").innerHTML = 'Bob Saget has passed away at the age of 65'
-    document.getElementById("content5").innerHTML = ''
+
+    document.getElementById("title3").innerHTML = 'SCOTUS blocks OSHA #vaccinemandate '
+    document.getElementById("content5").innerHTML = '<p>#news #openteanews</p>'
     document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '1/9/22 6:22pm'
+    document.getElementById("time3").innerHTML = '1/13/22 12:56pm'
 
 }
 
