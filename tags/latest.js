bbf4682ef57a5da493fb42072e280f413c30dc1d
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-24 12:55:48 -0700

    update

--- tags/latest.js
+++ tags/latest.js
@@ -1,31 +1,30 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Biden has signed H.R. 1192 into law.'
-    document.getElementById("content1").innerHTML = '<p>-H.R. 1192 is named, “Puerto Rico Recovery Accuracy in Disclosures Act of 2021”</p>'
-    document.getElementById("content2").innerHTML = ''
+    document.getElementById("title1").innerHTML = 'Ukranian official says,'
+    document.getElementById("content1").innerHTML = '<p>"Americans are safer in Kyiv than they are in LA."</p>'
+    document.getElementById("content2").innerHTML = '<p>-Concerns of a possible Russian invasion of Ukraine has increased in recent weeks.</p><p>-LA Crime Index: 337.2</p><p>-Kyiv Crime Index: 45.45</p>'
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
     document.getElementById("time1").innerHTML = '1/20/22 11:56am'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Hostage situation in Colleyville Texas.'
-    document.getElementById("content3").innerHTML = '<p>-SWAT is responding</p> <p>-This is an active situation</p>'
-    document.getElementById("content4").innerHTML = '<p>-There were 4 hostages</p> <p>-The situation was reported at 10:41 am</p><p>-The 4 hostages have been freed, unharmed</p><p>-The suspect has been killed by law enforcement</p> <p>-POTUS remarked: "This was an act of terror"</p>'
+    document.getElementById("title2").innerHTML = 'Biden has signed H.R. 1192 into law.'
+    document.getElementById("content3").innerHTML = '<p>-H.R. 1192 is named, “Puerto Rico Recovery Accuracy in Disclosures Act of 2021”</p>'
+    document.getElementById("content4").innerHTML = ''
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '1/15/22 3:32pm | Updated 1/15/22 5:23pm | Updated 1/15/22 6:25pm | Updated 1/16/22 11:28am'
-
+    document.getElementById("time2").innerHTML = '1/20/22 11:56am'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'SCOTUS blocks OSHA #vaccinemandate '
-    document.getElementById("content5").innerHTML = '<p>#news #openteanews</p>'
-    document.getElementById("content6").innerHTML = ''
+    document.getElementById("title3").innerHTML = 'Hostage situation in Colleyville Texas.'
+    document.getElementById("content5").innerHTML = '<p>-SWAT is responding</p> <p>-This is an active situation</p>'
+    document.getElementById("content6").innerHTML = '<p>-There were 4 hostages</p> <p>-The situation was reported at 10:41 am</p><p>-The 4 hostages have been freed, unharmed</p><p>-The suspect has been killed by law enforcement</p> <p>-POTUS remarked: "This was an act of terror"</p>'
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '1/13/22 12:56pm'
+    document.getElementById("time3").innerHTML = '1/15/22 3:32pm | Updated 1/15/22 5:23pm | Updated 1/15/22 6:25pm | Updated 1/16/22 11:28am'
 
 }
 
--- tags/older.js
+++ tags/older.js
@@ -1,20 +1,19 @@
 function older(){
     /* older 1 */
-
-    document.getElementById("otitle1").innerHTML = 'Betty White has passed away at the age of 99.'
-    document.getElementById("ocontent1").innerHTML = ''
+    document.getElementById("otitle1").innerHTML = 'SCOTUS blocks OSHA #vaccinemandate '
+    document.getElementById("ocontent1").innerHTML = '<p>#news #openteanews</p>'
     document.getElementById("ocontent2").innerHTML = ''
     document.getElementById("olink1").innerHTML = ''
     document.getElementById("olink1").href = ''
-    document.getElementById("otime1").innerHTML = '12/31/21 11:53am'
+    document.getElementById("otime1").innerHTML = '1/13/22 12:56pm'
 
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'Seacrest Studios at the National Childrens Hospital.'
-    document.getElementById("ocontent3").innerHTML = '<p>As seen in popular social posts and in several broadcasts, “Seacrest Studios” is visible in a shot while POTUS was visiting National Childrens Hospital. Seacrest Studios is part of the Ryan Seacrest Foundation, a nonprofit, and builds broadcast studios in pediatric centers to teach kids about radio, film and other forms of broadcasting while they are receiving treatment.</p>'
+    document.getElementById("otitle2").innerHTML = 'Betty White has passed away at the age of 99.'
+    document.getElementById("ocontent3").innerHTML = ''
     document.getElementById("ocontent4").innerHTML = ''
-    document.getElementById("olink2").innerHTML = 'Seacrest Foundation'
-    document.getElementById("olink2").href = 'https://www.ryanseacrestfoundation.org/'
-    document.getElementById("otime2").innerHTML = '12/25/21 5:11pm'
+    document.getElementById("olink2").innerHTML = ''
+    document.getElementById("olink2").href = ''
+    document.getElementById("otime2").innerHTML = '12/31/21 11:53am'
 
 }
