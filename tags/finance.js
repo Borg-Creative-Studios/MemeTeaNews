d8925369a31f8105e993ebdbbf59a14af34bdbf3
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-27 11:56:29 -0700

    Began building finance section

---
+++ tags/finance.js
@@ -0,0 +1,18 @@
+function finance(){
+    /* finance 1 */
+    document.getElementById("ftitle1").innerHTML = ''
+    document.getElementById("fcontent1").innerHTML = ''
+    document.getElementById("flink1").innerHTML = ''
+    document.getElementById("flink1").href = ''
+    document.getElementById("ftime1").innerHTML = '0/0/0 0:0apm'
+
+    /* finance 2 */
+
+    document.getElementById("ftitle2").innerHTML = ''
+    document.getElementById("fcontent3").innerHTML = ''
+    document.getElementById("fcontent4").innerHTML = ''
+    document.getElementById("flink2").innerHTML = ''
+    document.getElementById("flink2").href = ''
+    document.getElementById("ftime2").innerHTML = '0/0/0 0:0apm'
+
+}
