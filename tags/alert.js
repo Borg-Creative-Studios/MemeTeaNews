4c41e816aff1a16acf2b5a86b745de0262e87dd1
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-27 11:43:07 -0700

    updates

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Thousands of US troops have been put on “heightened preparedness” as tensions between Russia and Ukraine rise.'
+    document.getElementById("acontent1").innerHTML = 'Remember the Holocaust.'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
