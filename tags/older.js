503f1d4519f4db2698c6087b455a0eb92888058c
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-27 11:51:31 -0700

    update

--- tags/older.js
+++ tags/older.js
@@ -1,20 +1,19 @@
 function older(){
     /* older 1 */
-
-    document.getElementById("otitle1").innerHTML = 'Hostage situation in Colleyville Texas.'
-    document.getElementById("ocontent1").innerHTML = '<p>-SWAT is responding</p> <p>-This is an active situation</p>'
-    document.getElementById("ocontent2").innerHTML = '<p>-There were 4 hostages</p> <p>-The situation was reported at 10:41 am</p><p>-The 4 hostages have been freed, unharmed</p><p>-The suspect has been killed by law enforcement</p> <p>-POTUS remarked: "This was an act of terror"</p>'
+    document.getElementById("otitle1").innerHTML = 'Ukranian official says,'
+    document.getElementById("ocontent1").innerHTML = '<p>"Americans are safer in Kyiv than they are in LA."</p>'
+    document.getElementById("ocontent2").innerHTML = '<p>-Concerns of a possible Russian invasion of Ukraine has increased in recent weeks.</p><p>-LA Crime Index: 337.2</p><p>-Kyiv Crime Index: 45.45</p>'
     document.getElementById("olink1").innerHTML = ''
     document.getElementById("olink1").href = ''
-    document.getElementById("otime1").innerHTML = '1/15/22 3:32pm | Updated 1/15/22 5:23pm | Updated 1/15/22 6:25pm | Updated 1/16/22 11:28am'
+    document.getElementById("otime1").innerHTML = '1/24/22 12:55pm'
 
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'SCOTUS blocks OSHA #vaccinemandate '
-    document.getElementById("ocontent3").innerHTML = '<p>#news #openteanews</p>'
-    document.getElementById("ocontent4").innerHTML = ''
+    document.getElementById("otitle2").innerHTML = 'Hostage situation in Colleyville Texas.'
+    document.getElementById("ocontent3").innerHTML = '<p>-SWAT is responding</p> <p>-This is an active situation</p>'
+    document.getElementById("ocontent4").innerHTML = '<p>-There were 4 hostages</p> <p>-The situation was reported at 10:41 am</p><p>-The 4 hostages have been freed, unharmed</p><p>-The suspect has been killed by law enforcement</p> <p>-POTUS remarked: "This was an act of terror"</p>'
     document.getElementById("olink2").innerHTML = ''
     document.getElementById("olink2").href = ''
-    document.getElementById("otime2").innerHTML = '1/13/22 12:56pm'
+    document.getElementById("otime2").innerHTML = '1/15/22 3:32pm | Updated 1/15/22 5:23pm | Updated 1/15/22 6:25pm | Updated 1/16/22 11:28am'
 
 }
