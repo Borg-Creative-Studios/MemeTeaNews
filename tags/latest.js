82b1aaf2d15a55efd45716dc22a2d4a465c00627
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-27 17:07:25 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,38 +1,47 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'THREE HOUSTON PD OFFICERS HAVE BEEN SHOT. ALL THREE ARE IN STABLE CONDITION.'
-    document.getElementById("content1").innerHTML = ''
+    document.getElementById("title1").innerHTML = '"#ImpeachBiden" trends in Twitter politics'
+    document.getElementById("content1").innerHTML = '<p>-7,409 Tweets as of Jan 27 5:06pm</p>'
     document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '1/27/22 3:45pm'
+    document.getElementById("time1").innerHTML = '1/27/22 5:07pm'
 
     /* Recent 2 */
-    document.getElementById("title2").innerHTML = 'Philadelphia CPB intercepts two drug shipments.'
-    document.getElementById("content3").innerHTML = '<p>-The 1st shipment was intercepted on Jan. 18th</p>'
-    document.getElementById("content4").innerHTML = '<p>-The 2nd shipment was intercepted on Jan. 20th</p>'
+    document.getElementById("title2").innerHTML = 'THREE HOUSTON PD OFFICERS HAVE BEEN SHOT. ALL THREE ARE IN STABLE CONDITION.'
+    document.getElementById("content3").innerHTML = ''
+    document.getElementById("content4").innerHTML = ''
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '1/27/22 12:28pm'
+    document.getElementById("time2").innerHTML = '1/27/22 3:45pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'Thousands of US troops have been put on “heightened preparedness” as tensions in Eastern Europe between Russia and Ukraine rise.'
-    document.getElementById("content5").innerHTML = ''
-    document.getElementById("content6").innerHTML = ''
+    document.getElementById("title3").innerHTML = 'Philadelphia CPB intercepts two drug shipments.'
+    document.getElementById("content5").innerHTML = '<p>-The 1st shipment was intercepted on Jan. 18th</p>'
+    document.getElementById("content6").innerHTML = '<p>-The 2nd shipment was intercepted on Jan. 20th</p>'
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '1/24/22 1:33pm'
+    document.getElementById("time3").innerHTML = '1/27/22 12:28pm'
 
      /* Recent 4 */
-/*
-    document.getElementById("title4").innerHTML = 'The national gas price average is up $0.932 from one year ago.'
-    document.getElementById("content7").innerHTML = '<p>-CA price average is the highest at $4.645 per gallon</p>'
-    document.getElementById("content8").innerHTML = '<p>-KY price average is the lowest at $2.972 per gallon</p>'
+
+     document.getElementById("title4").innerHTML = 'Thousands of US troops have been put on “heightened preparedness” as tensions in Eastern Europe between Russia and Ukraine rise.'
+    document.getElementById("content7").innerHTML = ''
+    document.getElementById("content8").innerHTML = ''
     document.getElementById("link4").innerHTML = ''
     document.getElementById("link4").href = ''
-    document.getElementById("time4").innerHTML = '1/24/22 1:19pm'
-*/
+    document.getElementById("time4").innerHTML = '1/24/22 1:33pm'
+
+     /* Recent 5 */
+
+     document.getElementById("title5").innerHTML = 'The national gas price average is up $0.932 from one year ago.'
+     document.getElementById("content9").innerHTML = '<p>-CA price average is the highest at $4.645 per gallon</p>'
+     document.getElementById("content10").innerHTML = '<p>-KY price average is the lowest at $2.972 per gallon</p>'
+     document.getElementById("link5").innerHTML = ''
+     document.getElementById("link5").href = ''
+     document.getElementById("time5").innerHTML = '1/24/22 1:19pm'
+
 }
 
