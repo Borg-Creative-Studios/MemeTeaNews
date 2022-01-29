df5b5ef9e7a51afe1655b3d8c71cd14d2e7a7c6b
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-01-28 17:28:45 -0700

    updates

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Remember the Holocaust.'
+    document.getElementById("acontent1").innerHTML = 'Nor easter storm to hit New England and East Coast USA.'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
--- tags/latest.js
+++ tags/latest.js
@@ -1,48 +1,49 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'White House confirms it believes that a Russian invasion of Ukraine could happen at any time.'
-    document.getElementById("content1").innerHTML = '<p>Q: "Has the White House changed its assessment — has the Biden administration changed its assessment that a Russian invasion is imminent?"</p>'
-    document.getElementById("content2").innerHTML = '<p>PSAKI: "Well, I would say that we have said since last week that we have seen preparations and buildup at the border and that an invasion could come at any time.  Our assessment has not changed since that point."</p>'
+    document.getElementById("title1").innerHTML = 'An update on USA troop movement.'
+    document.getElementById("content1").innerHTML = '<p>Q: "Have you decided how soon you would be moving U.S. troops to Eastern Europe?"</p>'
+    document.getElementById("content2").innerHTML = '<p>POTUS: "I’ll be moving U.S. troops to Eastern Europe and the NATO countries in the near term — not a lot."</p>'
     document.getElementById("link1").innerHTML = 'WH Release'
-    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/press-briefings/2022/01/27/press-briefing-by-press-secretary-jen-psaki-january-27-2022/'
-    document.getElementById("time1").innerHTML = '1/27/22 5:30pm'
+    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/speeches-remarks/2022/01/28/remarks-by-president-biden-after-air-force-one-arrival-2/'
+    document.getElementById("time1").innerHTML = '1/28/22 5:25pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = '"#ImpeachBiden" trends in Twitter politics'
-    document.getElementById("content3").innerHTML = '<p>-7,409 Tweets as of Jan 27 5:06pm</p>'
-    document.getElementById("content4").innerHTML = ''
-    document.getElementById("link2").innerHTML = ''
-    document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '1/27/22 5:07pm'
+    document.getElementById("title2").innerHTML = 'White House confirms it believes that a Russian invasion of Ukraine could happen at any time.'
+    document.getElementById("content3").innerHTML = '<p>Q: "Has the White House changed its assessment — has the Biden administration changed its assessment that a Russian invasion is imminent?"</p>'
+    document.getElementById("content4").innerHTML = '<p>PSAKI: "Well, I would say that we have said since last week that we have seen preparations and buildup at the border and that an invasion could come at any time.  Our assessment has not changed since that point."</p>'
+    document.getElementById("link2").innerHTML = 'WH Release'
+    document.getElementById("link2").href = 'https://www.whitehouse.gov/briefing-room/press-briefings/2022/01/27/press-briefing-by-press-secretary-jen-psaki-january-27-2022/'
+    document.getElementById("time2").innerHTML = '1/27/22 5:30pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'THREE HOUSTON PD OFFICERS HAVE BEEN SHOT. ALL THREE ARE IN STABLE CONDITION.'
-    document.getElementById("content5").innerHTML = ''
+    document.getElementById("title3").innerHTML = '"#ImpeachBiden" trends in Twitter politics'
+    document.getElementById("content5").innerHTML = '<p>-7,409 Tweets as of Jan 27 5:06pm</p>'
     document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '1/27/22 3:45pm'
+    document.getElementById("time3").innerHTML = '1/27/22 5:07pm'
 
      /* Recent 4 */
 
-     document.getElementById("title4").innerHTML = 'Philadelphia CPB intercepts two drug shipments.'
-     document.getElementById("content7").innerHTML = '<p>-The 1st shipment was intercepted on Jan. 18th</p>'
-     document.getElementById("content8").innerHTML = '<p>-The 2nd shipment was intercepted on Jan. 20th</p>'
+     document.getElementById("title4").innerHTML = 'THREE HOUSTON PD OFFICERS HAVE BEEN SHOT. ALL THREE ARE IN STABLE CONDITION.'
+     document.getElementById("content7").innerHTML = ''
+     document.getElementById("content8").innerHTML = ''
      document.getElementById("link4").innerHTML = ''
      document.getElementById("link4").href = ''
-     document.getElementById("time4").innerHTML = '1/27/22 12:28pm'
+     document.getElementById("time4").innerHTML = '1/27/22 3:45pm'
 
      /* Recent 5 */
 
-    document.getElementById("title5").innerHTML = 'Thousands of US troops have been put on “heightened preparedness” as tensions in Eastern Europe between Russia and Ukraine rise.'
-    document.getElementById("content9").innerHTML = ''
-    document.getElementById("content10").innerHTML = ''
-    document.getElementById("link5").innerHTML = ''
-    document.getElementById("link5").href = ''
-    document.getElementById("time5").innerHTML = '1/24/22 1:33pm'
+     document.getElementById("title5").innerHTML = 'Philadelphia CPB intercepts two drug shipments.'
+     document.getElementById("content9").innerHTML = '<p>-The 1st shipment was intercepted on Jan. 18th</p>'
+     document.getElementById("content10").innerHTML = '<p>-The 2nd shipment was intercepted on Jan. 20th</p>'
+     document.getElementById("link5").innerHTML = ''
+     document.getElementById("link5").href = ''
+     document.getElementById("time5").innerHTML = '1/27/22 12:28pm'
+
 
 }
 
