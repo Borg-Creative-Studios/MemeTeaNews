e37adeb4ac8a415933546240ef279946f20894e1
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-06 11:35:41 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -10,40 +10,39 @@ function latest(){
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'White House publishes Oct. 2021 visitor records.'
-    document.getElementById("content3").innerHTML = '<p>-This was a voluntary disclosure</p>'
-    document.getElementById("content4").innerHTML = '<p>-2858 records</p>'
-    document.getElementById("link2").innerHTML = 'WH Disclosures'
-    document.getElementById("link2").href = 'https://www.whitehouse.gov/disclosures/visitor-logs/'
-    document.getElementById("time2").innerHTML = '2/5/22 8:35pm'
+    document.getElementById("title2").innerHTML = 'US Central Command releases report on the Augest Abbey Gate attack.'
+    document.getElementById("content3").innerHTML = '<p>-Happened on Augest 26, 2021</p>'
+    document.getElementById("content4").innerHTML = '<p>-Killed 13 US services members & 170 Afghan civilians</p>'
+    document.getElementById("link2").innerHTML = 'DOD Release'
+    document.getElementById("link2").href = 'https://www.defense.gov/News/News-Stories/Article/Article/2924398/us-central-command-releases-report-on-august-abbey-gate-attack/'
+    document.getElementById("time2").innerHTML = '2/5/22 8:30pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'An update on possible USA troop movement.'
-    document.getElementById("content5").innerHTML = '<p>Q: "Have you decided how soon you would be moving U.S. troops to Eastern Europe?"</p>'
-    document.getElementById("content6").innerHTML = '<p>POTUS: "I’ll be moving U.S. troops to Eastern Europe and the NATO countries in the near term — not a lot."</p>'
-    document.getElementById("link3").innerHTML = 'WH Release'
-    document.getElementById("link3").href = 'https://www.whitehouse.gov/briefing-room/speeches-remarks/2022/01/28/remarks-by-president-biden-after-air-force-one-arrival-2/'
-    document.getElementById("time3").innerHTML = '1/28/22 5:25pm'
+    document.getElementById("title3").innerHTML = 'White House publishes Oct. 2021 visitor records.'
+    document.getElementById("content5").innerHTML = '<p>-This was a voluntary disclosure</p>'
+    document.getElementById("content6").innerHTML = '<p>-2858 records</p>'
+    document.getElementById("link3").innerHTML = 'WH Disclosures'
+    document.getElementById("link3").href = 'https://www.whitehouse.gov/disclosures/visitor-logs/'
+    document.getElementById("time3").innerHTML = '2/5/22 8:35pm'
 
      /* Recent 4 */
 
-     document.getElementById("title4").innerHTML = 'White House confirms it believes that a Russian invasion of Ukraine could happen at any time.'
-     document.getElementById("content7").innerHTML = '<p>Q: "Has the White House changed its assessment — has the Biden administration changed its assessment that a Russian invasion is imminent?"</p>'
-     document.getElementById("content8").innerHTML = '<p>PSAKI: "Well, I would say that we have said since last week that we have seen preparations and buildup at the border and that an invasion could come at any time.  Our assessment has not changed since that point."</p>'
+     document.getElementById("title4").innerHTML = 'An update on possible USA troop movement.'
+     document.getElementById("content7").innerHTML = '<p>Q: "Have you decided how soon you would be moving U.S. troops to Eastern Europe?"</p>'
+     document.getElementById("content8").innerHTML = '<p>POTUS: "I’ll be moving U.S. troops to Eastern Europe and the NATO countries in the near term — not a lot."</p>'
      document.getElementById("link4").innerHTML = 'WH Release'
-     document.getElementById("link4").href = 'https://www.whitehouse.gov/briefing-room/press-briefings/2022/01/27/press-briefing-by-press-secretary-jen-psaki-january-27-2022/'
-     document.getElementById("time4").innerHTML = '1/27/22 5:30pm'
+     document.getElementById("link4").href = 'https://www.whitehouse.gov/briefing-room/speeches-remarks/2022/01/28/remarks-by-president-biden-after-air-force-one-arrival-2/'
+     document.getElementById("time4").innerHTML = '1/28/22 5:25pm'
  
-
      /* Recent 5 */
 
-     document.getElementById("title5").innerHTML = '"#ImpeachBiden" trends in Twitter politics'
-     document.getElementById("content9").innerHTML = '<p>-7,409 Tweets as of Jan 27 5:06pm</p>'
-     document.getElementById("content10").innerHTML = ''
-     document.getElementById("link5").innerHTML = ''
-     document.getElementById("link5").href = ''
-     document.getElementById("time5").innerHTML = '1/27/22 5:07pm'
+     document.getElementById("title5").innerHTML = 'White House confirms it believes that a Russian invasion of Ukraine could happen at any time.'
+     document.getElementById("content9").innerHTML = '<p>Q: "Has the White House changed its assessment — has the Biden administration changed its assessment that a Russian invasion is imminent?"</p>'
+     document.getElementById("content10").innerHTML = '<p>PSAKI: "Well, I would say that we have said since last week that we have seen preparations and buildup at the border and that an invasion could come at any time.  Our assessment has not changed since that point."</p>'
+     document.getElementById("link5").innerHTML = 'WH Release'
+     document.getElementById("link5").href = 'https://www.whitehouse.gov/briefing-room/press-briefings/2022/01/27/press-briefing-by-press-secretary-jen-psaki-january-27-2022/'
+     document.getElementById("time5").innerHTML = '1/27/22 5:30pm'
  
 }
 
