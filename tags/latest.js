c9b5b4cb907391fa9dbcc0e7a82c3f94b875f2ee
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-10 23:12:46 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,48 +1,48 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'LA County FD is deploying support to Orange County to assist in the Emerald Fire.'
-    document.getElementById("content1").innerHTML = '<p>-Deployed two water-dropping helicopters</p>'
-    document.getElementById("content2").innerHTML = '<p>-Fire was reported at 0409</p><p>-Evacuations have been ordered for Irvine Cove & Emerald Bay</p><p>-Evacuation warning for North Laguna</p>'
+    document.getElementById("title1").innerHTML = 'LA County FD has announced that two homes have been destroyed and a third damaged in the Sycamore Fire.'
+    document.getElementById("content1").innerHTML = ''
+    document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '2/10/22 11:45am'
+    document.getElementById("time1").innerHTML = '2/10/22 11:06pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'California legislature votes to keep the COVID state of emergency in effect.'
-    document.getElementById("content3").innerHTML = '<p>-The vote was on whether or not to suspend the state of emergency.</p>'
-    document.getElementById("content4").innerHTML = '<p>-Ayes: 15</p><p>-No: 48</p>'
+    document.getElementById("title2").innerHTML = 'Week 23 of operation Hammer Strike:'
+    document.getElementById("content3").innerHTML = '<p>-31 suspects arrested</p>'
+    document.getElementById("content4").innerHTML = '<p>-423 greenhouses eradicated</p>'
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '2/10/22 11:37am'
+    document.getElementById("time2").innerHTML = '2/10/22 11:04pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'National gas price average is up $0.977.'
-    document.getElementById("content5").innerHTML = '<p>-Current average: $3.439</p>'
-    document.getElementById("content6").innerHTML = '<p>-One year ago average: $2.462</p>'
+    document.getElementById("title3").innerHTML = 'California legislature votes to keep the COVID state of emergency in effect.'
+    document.getElementById("content5").innerHTML = '<p>-The vote was on whether or not to suspend the state of emergency.</p>'
+    document.getElementById("content6").innerHTML = '<p>-Ayes: 15</p><p>-No: 48</p>'
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '2/6/22 11:33am'
+    document.getElementById("time3").innerHTML = '2/10/22 11:37am'
 
      /* Recent 4 */
 
-     document.getElementById("title4").innerHTML = 'US Central Command releases report on the Augest Abbey Gate attack.'
-     document.getElementById("content7").innerHTML = '<p>-Happened on Augest 26, 2021</p>'
-     document.getElementById("content8").innerHTML = '<p>-Killed 13 US services members & 170 Afghan civilians</p>'
-     document.getElementById("link4").innerHTML = 'DOD Release'
-     document.getElementById("link4").href = 'https://www.defense.gov/News/News-Stories/Article/Article/2924398/us-central-command-releases-report-on-august-abbey-gate-attack/'
-     document.getElementById("time4").innerHTML = '2/5/22 8:30pm'
+     document.getElementById("title4").innerHTML = 'National gas price average is up $0.977.'
+     document.getElementById("content7").innerHTML = '<p>-Current average: $3.439</p>'
+     document.getElementById("content8").innerHTML = '<p>-One year ago average: $2.462</p>'
+     document.getElementById("link4").innerHTML = ''
+     document.getElementById("link4").href = ''
+     document.getElementById("time4").innerHTML = '2/6/22 11:33am'
  
      /* Recent 5 */
 
-     document.getElementById("title5").innerHTML = 'White House publishes Oct. 2021 visitor records.'
-     document.getElementById("content9").innerHTML = '<p>-This was a voluntary disclosure</p>'
-     document.getElementById("content10").innerHTML = '<p>-2858 records</p>'
-     document.getElementById("link5").innerHTML = 'WH Disclosures'
-     document.getElementById("link5").href = 'https://www.whitehouse.gov/disclosures/visitor-logs/'
-     document.getElementById("time5").innerHTML = '2/5/22 8:35pm'
+     document.getElementById("title5").innerHTML = 'National gas price average is up $0.977.'
+     document.getElementById("content9").innerHTML = '<p>-Current average: $3.439</p>'
+     document.getElementById("content10").innerHTML = '<p>-One year ago average: $2.462</p>'
+     document.getElementById("link5").innerHTML = ''
+     document.getElementById("link5").href = ''
+     document.getElementById("time5").innerHTML = '2/6/22 11:33am'
  
 }
 
