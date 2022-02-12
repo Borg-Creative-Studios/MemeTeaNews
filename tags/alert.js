bf94d40f0729bd70ba20fd2f76eb8039252d1bf8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-12 15:47:55 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'California legislature introduces bill to force businesses to make their employees get vaccinated.'
+    document.getElementById("acontent1").innerHTML = 'Emerald Fire update: 154 acres; 60% contained'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
