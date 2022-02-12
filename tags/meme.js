d6e3134823d4934100726abd3d51d0c58c42068f
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-11 22:36:51 -0700

    updated memes

--- tags/meme.js
+++ tags/meme.js
@@ -1,19 +1,19 @@
 function memes(){
     /* Top most entry */
     document.getElementById("mtitle1").innerHTML = ''
-    document.getElementById("mcontent1").src = 'https://tea.borgcreative.com/memes/ismsm.png'
+    document.getElementById("mcontent1").src = 'https://tea.borgcreative.com/memes/facepalmmsm.png'
     document.getElementById("mcontent2").innerHTML = ''
     document.getElementById("mlink1").innerHTML = ''
     document.getElementById("mlink1").href = ''
-    document.getElementById("mtime1").innerHTML = '2/11/22 12:12am'
+    document.getElementById("mtime1").innerHTML = '2/11/22 10:36pm'
 
     /* Recent 2 */
 
     document.getElementById("mtitle2").innerHTML = ''
-    document.getElementById("mcontent3").src = 'https://tea.borgcreative.com/memes/promo1.png'
+    document.getElementById("mcontent3").src = 'https://tea.borgcreative.com/memes/ismsm.png'
     document.getElementById("mcontent4").innerHTML = ''
     document.getElementById("mlink2").innerHTML = ''
     document.getElementById("mlink2").href = ''
-    document.getElementById("mtime2").innerHTML = '12/16/21 4:38pm'
+    document.getElementById("mtime2").innerHTML = '2/11/22 12:12am'
 
 }
