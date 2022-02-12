09b621e5737194015339b6d000a7c62eaa42dfd5
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-12 15:46:17 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,49 +1,49 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'PHX PD release statement on the officer involved shooting that occurred at 2am Friday morning (2/11/22)'
-    document.getElementById("content1").innerHTML = '<p>-9 officers injured</p><p>-2 other individuals killed</p>'
-    document.getElementById("content2").innerHTML = '<p>-Investigation is on going</p>'
-    document.getElementById("link1").innerHTML = 'PHX PD RELEASE'
-    document.getElementById("link1").href = 'https://www.phoenix.gov/newsroom/police/2238'
-    document.getElementById("time1").innerHTML = '2/11/22 10:20pm'
+    document.getElementById("title1").innerHTML = 'POTUS speaks with Rssiuan president Putin.'
+    document.getElementById("content1").innerHTML = '<p>-The discussion was centered around the Ukraine situation</p>'
+    document.getElementById("content2").innerHTML = '<p>-POTUS informed that the US and its allies would "impose swift and severe costs on Russia" if the country invades Ukraine</p>'
+    document.getElementById("link1").innerHTML = 'WH RELEASE'
+    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2022/02/12/readout-of-president-bidens-call-with-president-vladimir-putin-of-russia/'
+    document.getElementById("time1").innerHTML = '2/12/22 3:45pm'
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Roseville Joint Union High School District passes resolution making masking optional for students.'
-    document.getElementById("content3").innerHTML = '<p>-Passed unanimously </p>'
-    document.getElementById("content4").innerHTML = '<p>-Applies to all students</p>'
-    document.getElementById("link2").innerHTML = ''
-    document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '2/11/22 10:11pm'
+    document.getElementById("title2").innerHTML = 'PHX PD release statement on the officer involved shooting that occurred at 2am Friday morning (2/11/22)'
+    document.getElementById("content3").innerHTML = '<p>-9 officers injured</p><p>-2 other individuals killed</p>'
+    document.getElementById("content4").innerHTML = '<p>-Investigation is on going</p>'
+    document.getElementById("link2").innerHTML = 'PHX PD RELEASE'
+    document.getElementById("link2").href = 'https://www.phoenix.gov/newsroom/police/2238'
+    document.getElementById("time2").innerHTML = '2/11/22 10:20pm'
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'LA County FD has announced that two homes have been destroyed and a third damaged in the Sycamore Fire.'
-    document.getElementById("content5").innerHTML = ''
-    document.getElementById("content6").innerHTML = ''
+    document.getElementById("title3").innerHTML = 'Roseville Joint Union High School District passes resolution making masking optional for students.'
+    document.getElementById("content5").innerHTML = '<p>-Passed unanimously </p>'
+    document.getElementById("content6").innerHTML = '<p>-Applies to all students</p>'
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '2/10/22 11:06pm'
+    document.getElementById("time3").innerHTML = '2/11/22 10:11pm'
 
      /* Recent 4 */
 
-     document.getElementById("title4").innerHTML = 'Week 23 of operation Hammer Strike:'
-     document.getElementById("content7").innerHTML = '<p>-31 suspects arrested</p>'
-     document.getElementById("content8").innerHTML = '<p>-423 greenhouses eradicated</p>'
+     document.getElementById("title4").innerHTML = 'LA County FD has announced that two homes have been destroyed and a third damaged in the Sycamore Fire.'
+     document.getElementById("content7").innerHTML = ''
+     document.getElementById("content8").innerHTML = ''
      document.getElementById("link4").innerHTML = ''
      document.getElementById("link4").href = ''
-     document.getElementById("time4").innerHTML = '2/10/22 11:04pm'
+     document.getElementById("time4").innerHTML = '2/10/22 11:06pm'
  
 
      /* Recent 5 */
 
-     document.getElementById("title5").innerHTML = 'California legislature votes to keep the COVID state of emergency in effect.'
-     document.getElementById("content9").innerHTML = '<p>-The vote was on whether or not to suspend the state of emergency.</p>'
-     document.getElementById("content10").innerHTML = '<p>-Ayes: 15</p><p>-No: 48</p>'
+     document.getElementById("title5").innerHTML = 'Week 23 of operation Hammer Strike:'
+     document.getElementById("content9").innerHTML = '<p>-31 suspects arrested</p>'
+     document.getElementById("content10").innerHTML = '<p>-423 greenhouses eradicated</p>'
      document.getElementById("link5").innerHTML = ''
      document.getElementById("link5").href = ''
-     document.getElementById("time5").innerHTML = '2/10/22 11:37am'
+     document.getElementById("time5").innerHTML = '2/10/22 11:04pm'
  
 }
 
