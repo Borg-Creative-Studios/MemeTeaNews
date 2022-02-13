c30963181a61d6256f1cd065009f404313172bfb
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-12 22:14:41 -0700

    update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Emerald Fire update: 154 acres; 60% contained'
+    document.getElementById("acontent1").innerHTML = 'Emerald Fire update: 154 acres; 90% contained'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
