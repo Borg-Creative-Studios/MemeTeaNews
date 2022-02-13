a54ae88d08d7dd5f2c0c34482f2b4623d01b3824
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-13 14:23:06 -0700

    update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Emerald Fire update: 154 acres; 90% contained'
+    document.getElementById("acontent1").innerHTML = 'Super Bowl: Today at 6:30 ET'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
