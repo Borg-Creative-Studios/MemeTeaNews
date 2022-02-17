f8f51a77332b1e87461885c161c9fa36a04b7d7e
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-17 16:26:04 -0700

    updates

--- tags/funfacts.js
+++ tags/funfacts.js
@@ -1,20 +1,20 @@
 function funfacts(){
     /* older 1 */
 
-    document.getElementById("otitle1").innerHTML = 'Moles and Chemistry'
-    document.getElementById("ocontent1").innerHTML = '<p>Moles are a measurement unit used in chemistry.</p>'
-    document.getElementById("ocontent2").innerHTML = '<p>1 mole is equal to 6.02x10^23 and is the number of atoms in an elements atomic weight.</p><p>EXAMPLE: 1.008g of Hydrogen has 1 mole of atoms.</p>'
-    document.getElementById("olink1").innerHTML = ''
-    document.getElementById("olink1").href = ''
-    document.getElementById("otime1").innerHTML = ''
+    document.getElementById("fftitle1").innerHTML = 'Moles and Chemistry'
+    document.getElementById("ffcontent1").innerHTML = '<p>Moles are a measurement unit used in chemistry.</p>'
+    document.getElementById("ffcontent2").innerHTML = '<p>1 mole is equal to 6.02x10^23 and is the number of atoms in an elements atomic weight.</p><p>EXAMPLE: 1.008g of Hydrogen has 1 mole of atoms.</p>'
+    document.getElementById("fflink1").innerHTML = ''
+    document.getElementById("fflink1").href = ''
+    document.getElementById("fftime1").innerHTML = ''
 
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'The Mona Lisa'
-    document.getElementById("ocontent3").innerHTML = '<p>The Mona Lisa was painted by Leonardo Da Vinci between 1503 and 1506.</p>'
-    document.getElementById("ocontent4").innerHTML = '<p>The Mona Lisa is a painting of noblewoman Lisa Gherardini.</p><p>The painting is on display in the Louvre in Paris France.</p>'
-    document.getElementById("olink2").innerHTML = ''
-    document.getElementById("olink2").href = ''
-    document.getElementById("otime2").innerHTML = ''
+    document.getElementById("fftitle2").innerHTML = 'The Mona Lisa'
+    document.getElementById("ffcontent3").innerHTML = '<p>The Mona Lisa was painted by Leonardo Da Vinci between 1503 and 1506.</p>'
+    document.getElementById("ffcontent4").innerHTML = '<p>The Mona Lisa is a painting of noblewoman Lisa Gherardini.</p><p>The painting is on display in the Louvre in Paris France.</p>'
+    document.getElementById("fflink2").innerHTML = ''
+    document.getElementById("fflink2").href = ''
+    document.getElementById("fftime2").innerHTML = ''
 
 }
