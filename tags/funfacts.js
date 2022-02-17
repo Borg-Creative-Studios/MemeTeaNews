36dd813a953961a1d8bc0c6bb04263d18f29f093
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-17 16:21:55 -0700

    updates

---
+++ tags/funfacts.js
@@ -0,0 +1,20 @@
+function funfacts(){
+    /* older 1 */
+
+    document.getElementById("otitle1").innerHTML = 'Moles and Chemistry'
+    document.getElementById("ocontent1").innerHTML = '<p>Moles are a measurement unit used in chemistry.</p>'
+    document.getElementById("ocontent2").innerHTML = '<p>1 mole is equal to 6.02x10^23 and is the number of atoms in an elements atomic weight.</p><p>EXAMPLE: 1.008g of Hydrogen has 1 mole of atoms.</p>'
+    document.getElementById("olink1").innerHTML = ''
+    document.getElementById("olink1").href = ''
+    document.getElementById("otime1").innerHTML = ''
+
+    /* older 2 */
+
+    document.getElementById("otitle2").innerHTML = ''
+    document.getElementById("ocontent3").innerHTML = '<p></p>'
+    document.getElementById("ocontent4").innerHTML = '<p></p>'
+    document.getElementById("olink2").innerHTML = ''
+    document.getElementById("olink2").href = ''
+    document.getElementById("otime2").innerHTML = ''
+
+}
