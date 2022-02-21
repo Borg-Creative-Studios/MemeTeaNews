461d3837198eb8fd319d329cfcebbb57aabde1a8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-21 13:15:27 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,56 +1,58 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Angeles National Forest is conducting 2 controlled burns.'
-    document.getElementById("content1").innerHTML = '<p>-The burns are near the Chilao campground & Barret Stoddard</p>'
-    document.getElementById("content2").innerHTML = '<p>-Forest service advises that light smoke may be visible</p>'
+    document.getElementById("title1").innerHTML = 'Russia is to recognize the independence of two breakaway regions of Ukraine.'
+    document.getElementById("content1").innerHTML = ''
+    document.getElementById("content2").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '2/21/22 1:08pm'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/16'
+    document.getElementById("time1").innerHTML = '2/21/22 1:14pm'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/17'
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
-    document.getElementById("content3").innerHTML = ''
-    document.getElementById("content4").innerHTML = ''
+    document.getElementById("title2").innerHTML = 'Angeles National Forest is conducting 2 controlled burns.'
+    document.getElementById("content3").innerHTML = '<p>-The burns are near the Chilao campground & Barret Stoddard</p>'
+    document.getElementById("content4").innerHTML = '<p>-Forest service advises that light smoke may be visible</p>'
     document.getElementById("link2").innerHTML = ''
     document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '2/17/22 3:32pm'
-    document.getElementById("telegram2").href = 'https://t.me/openteanews/15'
+    document.getElementById("time2").innerHTML = '2/21/22 1:08pm'
+    document.getElementById("telegram2").href = 'https://t.me/openteanews/16'
     
+
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = '3 San Francisco area school board members are recalled.'
+    document.getElementById("title3").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
     document.getElementById("content5").innerHTML = ''
     document.getElementById("content6").innerHTML = ''
     document.getElementById("link3").innerHTML = ''
     document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '2/17/22 11:12am'
-    document.getElementById("telegram3").href = 'https://t.me/openteanews/14'
-
+    document.getElementById("time3").innerHTML = '2/17/22 3:32pm'
+    document.getElementById("telegram3").href = 'https://t.me/openteanews/15'
+    
      /* Recent 4 */
 
-     document.getElementById("title4").innerHTML = 'Gov. Newsom announces an exension of Californias school mask mandate.'
+     document.getElementById("title4").innerHTML = '3 San Francisco area school board members are recalled.'
      document.getElementById("content7").innerHTML = ''
      document.getElementById("content8").innerHTML = ''
      document.getElementById("link4").innerHTML = ''
      document.getElementById("link4").href = ''
-     document.getElementById("time4").innerHTML = '2/14/22 8:07pm'
-     document.getElementById("telegram4").href = 'https://t.me/openteanews'
-    
+     document.getElementById("time4").innerHTML = '2/17/22 11:12am'
+     document.getElementById("telegram4").href = 'https://t.me/openteanews/14'
+ 
      /* Recent 5 */
 
-     document.getElementById("title5").innerHTML = 'POTUS speaks with Russian president Putin.'
-     document.getElementById("content9").innerHTML = '<p>-The discussion was centered around the Ukraine situation</p>'
-     document.getElementById("content10").innerHTML = '<p>-POTUS informed that the US and its allies would "impose swift and severe costs on Russia" if the country invades Ukraine</p>'
-     document.getElementById("link5").innerHTML = 'WH RELEASE'
-     document.getElementById("link5").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2022/02/12/readout-of-president-bidens-call-with-president-vladimir-putin-of-russia/'
-     document.getElementById("time5").innerHTML = '2/12/22 3:45pm'
+     document.getElementById("title5").innerHTML = 'Gov. Newsom announces an exension of Californias school mask mandate.'
+     document.getElementById("content9").innerHTML = ''
+     document.getElementById("content10").innerHTML = ''
+     document.getElementById("link5").innerHTML = ''
+     document.getElementById("link5").href = ''
+     document.getElementById("time5").innerHTML = '2/14/22 8:07pm'
      document.getElementById("telegram5").href = 'https://t.me/openteanews'
- 
+    
+
 }
 
