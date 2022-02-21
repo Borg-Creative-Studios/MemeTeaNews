bae66be57ef09ebd8a9442cd3109007f93b21912
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-21 13:20:14 -0700

    Backend update
    Changed IDs to make updating faster and simpler

--- tags/latest.js
+++ tags/latest.js
@@ -3,7 +3,7 @@ function latest(){
 
     document.getElementById("title1").innerHTML = 'Russia is to recognize the independence of two breakaway regions of Ukraine.'
     document.getElementById("content1").innerHTML = ''
-    document.getElementById("content2").innerHTML = ''
+    document.getElementById("content01").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
     document.getElementById("time1").innerHTML = '2/21/22 1:14pm'
@@ -14,44 +14,44 @@ function latest(){
 
     /* Recent 2 */
 
-    document.getElementById("title2").innerHTML = 'Angeles National Forest is conducting 2 controlled burns.'
-    document.getElementById("content3").innerHTML = '<p>-The burns are near the Chilao campground & Barret Stoddard</p>'
-    document.getElementById("content4").innerHTML = '<p>-Forest service advises that light smoke may be visible</p>'
-    document.getElementById("link2").innerHTML = ''
-    document.getElementById("link2").href = ''
-    document.getElementById("time2").innerHTML = '2/21/22 1:08pm'
-    document.getElementById("telegram2").href = 'https://t.me/openteanews/16'
+    document.getElementById("title11").innerHTML = 'Angeles National Forest is conducting 2 controlled burns.'
+    document.getElementById("content11").innerHTML = '<p>-The burns are near the Chilao campground & Barret Stoddard</p>'
+    document.getElementById("content011").innerHTML = '<p>-Forest service advises that light smoke may be visible</p>'
+    document.getElementById("link11").innerHTML = ''
+    document.getElementById("link11").href = ''
+    document.getElementById("time11").innerHTML = '2/21/22 1:08pm'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/16'
     
 
     /* Recent 3 */
 
-    document.getElementById("title3").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
-    document.getElementById("content5").innerHTML = ''
-    document.getElementById("content6").innerHTML = ''
-    document.getElementById("link3").innerHTML = ''
-    document.getElementById("link3").href = ''
-    document.getElementById("time3").innerHTML = '2/17/22 3:32pm'
-    document.getElementById("telegram3").href = 'https://t.me/openteanews/15'
+    document.getElementById("title111").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
+    document.getElementById("content111").innerHTML = ''
+    document.getElementById("content0111").innerHTML = ''
+    document.getElementById("link111").innerHTML = ''
+    document.getElementById("link111").href = ''
+    document.getElementById("time111").innerHTML = '2/17/22 3:32pm'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/15'
     
      /* Recent 4 */
 
-     document.getElementById("title4").innerHTML = '3 San Francisco area school board members are recalled.'
-     document.getElementById("content7").innerHTML = ''
-     document.getElementById("content8").innerHTML = ''
-     document.getElementById("link4").innerHTML = ''
-     document.getElementById("link4").href = ''
-     document.getElementById("time4").innerHTML = '2/17/22 11:12am'
-     document.getElementById("telegram4").href = 'https://t.me/openteanews/14'
+     document.getElementById("title1111").innerHTML = '3 San Francisco area school board members are recalled.'
+     document.getElementById("content1111").innerHTML = ''
+     document.getElementById("content01111").innerHTML = ''
+     document.getElementById("link1111").innerHTML = ''
+     document.getElementById("link1111").href = ''
+     document.getElementById("time1111").innerHTML = '2/17/22 11:12am'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/14'
  
      /* Recent 5 */
 
-     document.getElementById("title5").innerHTML = 'Gov. Newsom announces an exension of Californias school mask mandate.'
-     document.getElementById("content9").innerHTML = ''
-     document.getElementById("content10").innerHTML = ''
-     document.getElementById("link5").innerHTML = ''
-     document.getElementById("link5").href = ''
-     document.getElementById("time5").innerHTML = '2/14/22 8:07pm'
-     document.getElementById("telegram5").href = 'https://t.me/openteanews'
+     document.getElementById("title11111").innerHTML = 'Gov. Newsom announces an exension of Californias school mask mandate.'
+     document.getElementById("content11111").innerHTML = ''
+     document.getElementById("content011111").innerHTML = ''
+     document.getElementById("link11111").innerHTML = ''
+     document.getElementById("link11111").href = ''
+     document.getElementById("time11111").innerHTML = '2/14/22 8:07pm'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews'
     
 
 }
