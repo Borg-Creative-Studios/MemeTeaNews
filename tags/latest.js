970473c1f6664d59533240c4556f0fea6c4347ff
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-26 11:17:34 -0700

    updates

--- tags/latest.js
+++ tags/latest.js
@@ -3,10 +3,10 @@ function latest(){
 
     document.getElementById("title1").innerHTML = 'War in Ukraine:'
     document.getElementById("content1").innerHTML = '<p>-Russia began its invasion of Ukraine at 5:00am, Feb 24th</p><p>-Biden has imposed new sanctions against Russia</p><p>-Zelenskyy cuts diplomatic ties with Russia.</p>'
-    document.getElementById("content01").innerHTML = '<p>-Anonymous says that it has leaked a database from the RMD website</p><p>-(Feb 26) Kyiv makes it through the night</p>  <p>This is a developing situation and encourage that you exercise caution with the sources you get info from.</p><p><a href="https://twitter.com/StateDept" target="_blank">@StateDept (Twitter)</a></p><p><a href="https://twitter.com/NATOinUkraine" target="_blank">@NATOinUkraine (Twitter)</a></p>'
+    document.getElementById("content01").innerHTML = '<p>-Anonymous says that it has leaked a database from the RMD website</p><p>-(Feb 26) Kyiv makes it through the night</p> <p>-(Feb 26) U.S. Pentagon: "we stand ready if called upon by NATO"</p> <p>-(Feb 26) Anonymous intercepts Russian military communications</p><p>-(Feb 26) Anonymous has operation to keep .ru gov sites down</p>  <p>This is a developing situation and encourage that you exercise caution with the sources you get info from.</p><p><a href="https://twitter.com/StateDept" target="_blank">@StateDept (Twitter)</a></p><p><a href="https://twitter.com/NATOinUkraine" target="_blank">@NATOinUkraine (Twitter)</a></p>'
     document.getElementById("link1").innerHTML = '@Ukraine (Twitter)'
     document.getElementById("link1").href = 'https://twitter.com/Ukraine'
-    document.getElementById("time1").innerHTML = 'Updated 2/26/22 12:32am | 2/24/22 2:22pm'
+    document.getElementById("time1").innerHTML = 'Updated 2/26/22 11:17am | Updated 2/26/22 12:32am | 2/24/22 2:22pm'
     document.getElementById("telegram1").href = 'https://t.me/openteanews/18'
     
     
@@ -14,29 +14,37 @@ function latest(){
 
     /* Recent 2 */
 
-    document.getElementById("title11").innerHTML = 'Ukrainian Minister of Foreign Affairs, Dmytro Kuleba:'
-    document.getElementById("content11").innerHTML = '<p>“Kyiv, our splendid, peaceful city, survived another night under attacks by Russian ground forces, missiles. One of them has hit a residential apartment in Kyiv. I demand the world: fully isolate Russia, expel ambassadors, oil embargo, ruin its economy. Stop Russian war criminals!”</p>'
+    document.getElementById("title11").innerHTML = 'There are reports of fraudulant donation requests in the name of Ukraine.'
+    document.getElementById("content11").innerHTML = '<p>Before donating to any fund, we recommend that you reswearch who created it and who shared it and to be cautious prior to donating.</p>'
     document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'See the Tweet'
-    document.getElementById("link11").href = 'https://twitter.com/DmytroKuleba/status/1497463617767227395'
-    document.getElementById("time11").innerHTML = '2/26/22 12:49am'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/18'
+    document.getElementById("link11").innerHTML = ''
+    document.getElementById("link11").href = ''
+    document.getElementById("time11").innerHTML = '2/26/22 11:17am'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/19'
     
 
     
 
     /* Recent 3 */
 
-    document.getElementById("title111").innerHTML = 'Russia is to recognize the independence of two breakaway regions of Ukraine.'
-    document.getElementById("content111").innerHTML = ''
+    document.getElementById("title111").innerHTML = 'Ukrainian Minister of Foreign Affairs, Dmytro Kuleba:'
+    document.getElementById("content111").innerHTML = '<p>“Kyiv, our splendid, peaceful city, survived another night under attacks by Russian ground forces, missiles. One of them has hit a residential apartment in Kyiv. I demand the world: fully isolate Russia, expel ambassadors, oil embargo, ruin its economy. Stop Russian war criminals!”</p>'
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = ''
-    document.getElementById("link111").href = ''
-    document.getElementById("time111").innerHTML = '2/21/22 1:14pm'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/17'
+    document.getElementById("link111").innerHTML = 'See the Tweet'
+    document.getElementById("link111").href = 'https://twitter.com/DmytroKuleba/status/1497463617767227395'
+    document.getElementById("time111").innerHTML = '2/26/22 12:49am'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/18'
     
      /* Recent 4 */
 
+     document.getElementById("title1111").innerHTML = 'Russia is to recognize the independence of two breakaway regions of Ukraine.'
+     document.getElementById("content1111").innerHTML = ''
+     document.getElementById("content01111").innerHTML = ''
+     document.getElementById("link1111").innerHTML = ''
+     document.getElementById("link1111").href = ''
+     document.getElementById("time1111").innerHTML = '2/21/22 1:14pm'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/17'
+     
      document.getElementById("title1111").innerHTML = 'Angeles National Forest is conducting 2 controlled burns.'
      document.getElementById("content1111").innerHTML = '<p>-The burns are near the Chilao campground & Barret Stoddard</p>'
      document.getElementById("content01111").innerHTML = '<p>-Forest service advises that light smoke may be visible</p>'
@@ -48,14 +56,14 @@ function latest(){
 
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
-     document.getElementById("content11111").innerHTML = ''
-     document.getElementById("content011111").innerHTML = ''
+     document.getElementById("title11111").innerHTML = 'Angeles National Forest is conducting 2 controlled burns.'
+     document.getElementById("content11111").innerHTML = '<p>-The burns are near the Chilao campground & Barret Stoddard</p>'
+     document.getElementById("content011111").innerHTML = '<p>-Forest service advises that light smoke may be visible</p>'
      document.getElementById("link11111").innerHTML = ''
      document.getElementById("link11111").href = ''
-     document.getElementById("time11111").innerHTML = '2/17/22 3:32pm'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/15'
-     
+     document.getElementById("time11111").innerHTML = '2/21/22 1:08pm'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/16'
+    
 
 }
 
--- tags/older.js
+++ tags/older.js
@@ -1,20 +1,20 @@
 function older(){
     /* older 1 */
-
-    document.getElementById("otitle1").innerHTML = 'Week 23 of operation Hammer Strike:'
-    document.getElementById("ocontent1").innerHTML = '<p>-31 suspects arrested</p>'
-    document.getElementById("ocontent2").innerHTML = '<p>-423 greenhouses eradicated</p>'
+    document.getElementById("otitle1").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
+    document.getElementById("ocontent1").innerHTML = ''
+    document.getElementById("ocontent2").innerHTML = ''
     document.getElementById("olink1").innerHTML = ''
     document.getElementById("olink1").href = ''
-    document.getElementById("otime1").innerHTML = '2/10/22 11:04pm'
+    document.getElementById("otime1").innerHTML = '2/17/22 3:32pm'
 
+    
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'An update on possible USA troop movement.'
-    document.getElementById("ocontent3").innerHTML = '<p>Q: "Have you decided how soon you would be moving U.S. troops to Eastern Europe?"</p>'
-    document.getElementById("ocontent4").innerHTML = '<p>POTUS: "I’ll be moving U.S. troops to Eastern Europe and the NATO countries in the near term — not a lot."</p>'
-    document.getElementById("olink2").innerHTML = 'WH Release'
-    document.getElementById("olink2").href = 'https://www.whitehouse.gov/briefing-room/speeches-remarks/2022/01/28/remarks-by-president-biden-after-air-force-one-arrival-2/'
-    document.getElementById("otime2").innerHTML = '1/28/22 5:25pm'
+    document.getElementById("otitle2").innerHTML = 'Week 23 of operation Hammer Strike:'
+    document.getElementById("ocontent3").innerHTML = '<p>-31 suspects arrested</p>'
+    document.getElementById("ocontent4").innerHTML = '<p>-423 greenhouses eradicated</p>'
+    document.getElementById("olink2").innerHTML = ''
+    document.getElementById("olink2").href = ''
+    document.getElementById("otime2").innerHTML = '2/10/22 11:04pm'
 
 }
