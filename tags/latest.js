ee2441e1190d135830bb96533685a3c93c823d02
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-26 00:50:33 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -14,48 +14,48 @@ function latest(){
 
     /* Recent 2 */
 
-    document.getElementById("title11").innerHTML = 'Russia is to recognize the independence of two breakaway regions of Ukraine.'
-    document.getElementById("content11").innerHTML = ''
+    document.getElementById("title11").innerHTML = 'Ukrainian Minister of Foreign Affairs, Dmytro Kuleba:'
+    document.getElementById("content11").innerHTML = '<p>“Kyiv, our splendid, peaceful city, survived another night under attacks by Russian ground forces, missiles. One of them has hit a residential apartment in Kyiv. I demand the world: fully isolate Russia, expel ambassadors, oil embargo, ruin its economy. Stop Russian war criminals!”</p>'
     document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = ''
-    document.getElementById("link11").href = ''
-    document.getElementById("time11").innerHTML = '2/21/22 1:14pm'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/17'
+    document.getElementById("link11").innerHTML = 'See the Tweet'
+    document.getElementById("link11").href = 'https://twitter.com/DmytroKuleba/status/1497463617767227395'
+    document.getElementById("time11").innerHTML = '2/26/22 12:49am'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/18'
     
 
     
 
     /* Recent 3 */
 
-    document.getElementById("title111").innerHTML = 'Angeles National Forest is conducting 2 controlled burns.'
-    document.getElementById("content111").innerHTML = '<p>-The burns are near the Chilao campground & Barret Stoddard</p>'
-    document.getElementById("content0111").innerHTML = '<p>-Forest service advises that light smoke may be visible</p>'
+    document.getElementById("title111").innerHTML = 'Russia is to recognize the independence of two breakaway regions of Ukraine.'
+    document.getElementById("content111").innerHTML = ''
+    document.getElementById("content0111").innerHTML = ''
     document.getElementById("link111").innerHTML = ''
     document.getElementById("link111").href = ''
-    document.getElementById("time111").innerHTML = '2/21/22 1:08pm'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/16'
+    document.getElementById("time111").innerHTML = '2/21/22 1:14pm'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/17'
     
      /* Recent 4 */
 
-     document.getElementById("title1111").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
-     document.getElementById("content1111").innerHTML = ''
-     document.getElementById("content01111").innerHTML = ''
+     document.getElementById("title1111").innerHTML = 'Angeles National Forest is conducting 2 controlled burns.'
+     document.getElementById("content1111").innerHTML = '<p>-The burns are near the Chilao campground & Barret Stoddard</p>'
+     document.getElementById("content01111").innerHTML = '<p>-Forest service advises that light smoke may be visible</p>'
      document.getElementById("link1111").innerHTML = ''
      document.getElementById("link1111").href = ''
-     document.getElementById("time1111").innerHTML = '2/17/22 3:32pm'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/15'
-     
+     document.getElementById("time1111").innerHTML = '2/21/22 1:08pm'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/16'
+    
 
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = '3 San Francisco area school board members are recalled.'
+     document.getElementById("title11111").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
      document.getElementById("content11111").innerHTML = ''
      document.getElementById("content011111").innerHTML = ''
      document.getElementById("link11111").innerHTML = ''
      document.getElementById("link11111").href = ''
-     document.getElementById("time11111").innerHTML = '2/17/22 11:12am'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/14'
- 
+     document.getElementById("time11111").innerHTML = '2/17/22 3:32pm'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/15'
+     
 
 }
 
