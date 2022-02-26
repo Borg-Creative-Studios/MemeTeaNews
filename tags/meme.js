4a187304b1f7f47b2b7a0aef41a32de7a6284358
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-26 14:34:10 -0700

    Update meme.js
    i fixed the meme that i broke

--- tags/meme.js
+++ tags/meme.js
@@ -9,12 +9,12 @@ function memes(){
 
     /* Recent 2 */
 
-    document.getElementById("mtitle1").innerHTML = ''
-    document.getElementById("mcontent1").src = 'https://tea.borgcreative.com/memes/facepalmmsm.png'
-    document.getElementById("mcontent2").innerHTML = ''
-    document.getElementById("mlink1").innerHTML = ''
-    document.getElementById("mlink1").href = ''
-    document.getElementById("mtime1").innerHTML = '2/11/22 10:36pm'
+    document.getElementById("mtitle2").innerHTML = ''
+    document.getElementById("mcontent3").src = 'https://tea.borgcreative.com/memes/facepalmmsm.png'
+    document.getElementById("mcontent4").innerHTML = ''
+    document.getElementById("mlink2").innerHTML = ''
+    document.getElementById("mlink2").href = ''
+    document.getElementById("mtime2").innerHTML = '2/11/22 10:36pm'
 
 
 }
