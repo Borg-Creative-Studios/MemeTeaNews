48b6cd4492af9bec1e0d2d3ca7e2ba32c49ce006
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-26 11:32:49 -0700

    +Research Center tag script

---
+++ tags/research.js
@@ -0,0 +1,20 @@
+function research(){
+    /* older 1 */
+
+    document.getElementById("rtitle1").innerHTML = 'Moles and Chemistry'
+    document.getElementById("rcontent1").innerHTML = '<p>Moles are a measurement unit used in chemistry.</p>'
+    document.getElementById("rcontent2").innerHTML = '<p>1 mole is equal to 6.02x10^23 and is the number of atoms in an elements atomic weight.</p><p>EXAMPLE: 1.008g of Hydrogen has 1 mole of atoms.</p>'
+    document.getElementById("rlink1").innerHTML = ''
+    document.getElementById("rlink1").href = ''
+    document.getElementById("rtime1").innerHTML = ''
+
+    /* older 2 */
+
+    document.getElementById("rtitle2").innerHTML = 'The Mona Lisa'
+    document.getElementById("rcontent3").innerHTML = '<p>The Mona Lisa was painted by Leonardo Da Vinci between 1503 and 1506.</p>'
+    document.getElementById("rcontent4").innerHTML = '<p>The Mona Lisa is a painting of noblewoman Lisa Gherardini.</p><p>The painting is on display in the Louvre in Paris France.</p>'
+    document.getElementById("rlink2").innerHTML = ''
+    document.getElementById("rlink2").href = ''
+    document.getElementById("rtime2").innerHTML = ''
+
+}
