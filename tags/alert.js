9244453a7b5255261aaa5917c7bd97b67c58a51b
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-02-26 18:53:50 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Russia invades Ukraine.'
+    document.getElementById("acontent1").innerHTML = 'Russia has invaded Ukraine.'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
