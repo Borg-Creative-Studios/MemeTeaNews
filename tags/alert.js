decf4ba6043236bd12cb1e355895247b66a2e597
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-03-09 22:48:55 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,6 +1,6 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Ukrainian officials report a change in radiation levels at Chernobyl.'
-    document.getElementById("alink").innerHTML = ''
-    document.getElementById("alink").href = ''
+    document.getElementById("acontent1").innerHTML = 'Elon Musk says Europe should restart its deactivated nuclear power plants.'
+    document.getElementById("alink").innerHTML = 'Read the Tweet'
+    document.getElementById("alink").href = 'https://twitter.com/elonmusk/status/1500613952031444995'
 }
 
