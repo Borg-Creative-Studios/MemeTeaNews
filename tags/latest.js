484428c99cadf5bf4c12ad8a1a39bad4ade69b8d
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-03-29 08:05:54 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,65 +1,64 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'House approves financial aid for Ukraine.'
-    document.getElementById("content1").innerHTML = '<p>-The bill provides $13.6B to Ukraine and its allies.</p>'
+    document.getElementById("title1").innerHTML = 'POTUS has ammended the 2019 Alaska Disaster Declaration.'
+    document.getElementById("content1").innerHTML = '<p>-This ammendment increases the amount of funds available to those impacted by the November 30th (2018) earthquake. </p>'
     document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = ''
-    document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '3/9/22 10:46pm MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/22'
+    document.getElementById("link1").innerHTML = 'WH RELEASE'
+    document.getElementById("link1").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2022/03/29/president-joseph-r-biden-jr-amends-alaska-disaster-declaration/'
+    document.getElementById("time1").innerHTML = '3/29/22 8:05am MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/23'
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
 
-    document.getElementById("title11").innerHTML = 'War in Ukraine:'
-    document.getElementById("content11").innerHTML = '<p>-Russia began its invasion of Ukraine at 5:00am, Feb 24th</p><p>-Biden has imposed new sanctions against Russia</p><p>-Zelenskyy cuts diplomatic ties with Russia.</p>'
-    document.getElementById("content011").innerHTML = '<p>-Anonymous says that it has leaked a database from the RMD website</p><p>-(Feb 26) Kyiv makes it through the night</p> <p>-(Feb 26) U.S. Pentagon: "we stand ready if called upon by NATO"</p> <p>-(Feb 26) Anonymous intercepts Russian military communications</p><p>-(Feb 26) Anonymous has operation to keep .ru gov sites down</p><p>-(Feb 27) Peace talks between Ukraine and Russia today</p><p>-(Feb 27) EU sanctions Belarus</p><p>-(Feb 27) Canada has closed its airspace to all Russian aircraft</p><p>-(Feb 27) @MFA_Ukraine reports that "Mriya" (Dream), the largest plane in the world, has been destroyed by Russian forces</p>  <p>This is a developing situation and encourage that you exercise caution with the sources you get info from.</p><p style="padding: 15px;"></p><p><a href="https://twitter.com/StateDept" target="_blank">@StateDept (Twitter)</a></p><p><a href="https://twitter.com/NATOinUkraine" target="_blank">@NATOinUkraine (Twitter)</a></p>'
-    document.getElementById("link11").innerHTML = '@Ukraine (Twitter)'
-    document.getElementById("link11").href = 'https://twitter.com/Ukraine'
-    document.getElementById("time11").innerHTML = 'Updated 2/27/22 12:48pm MST | Updated 2/27/22 12:45pm MST | Updated 2/27/22 12:14pm MST | Updated 2/26/22 11:17am MST | Updated 2/26/22 12:32am MST | 2/24/22 2:22pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/18'
-    
+
+    document.getElementById("title11").innerHTML = 'House approves financial aid for Ukraine.'
+    document.getElementById("content11").innerHTML = '<p>-The bill provides $13.6B to Ukraine and its allies.</p>'
+    document.getElementById("content011").innerHTML = ''
+    document.getElementById("link11").innerHTML = ''
+    document.getElementById("link11").href = ''
+    document.getElementById("time11").innerHTML = '3/9/22 10:46pm MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/22'
+
 
     
 
     /* Recent 3 */
-
-    document.getElementById("title111").innerHTML = 'Ukrainian MFA reports on Russian losses.'
-    document.getElementById("content111").innerHTML = '<p>-4500 wounded or killed</p><p>-200+ taken prisoner</p><p>-46 aircraft</p><p>-26 helicopters</p><p>-146 tanks</p>'
-    document.getElementById("content0111").innerHTML = '<p>-706 armored vehicles</p><p>-1 anti-aircraft system BUK</p><p>-20 automotive equipment</p><p>-49 artillery pieces</p><p>-4 Grad systems</p><p>-30 vehicles</p><p>-2 vessels</p>'
-    document.getElementById("link111").innerHTML = 'Read the Tweet from @MFA_Ukraine'
-    document.getElementById("link111").href = 'https://twitter.com/MFA_Ukraine/status/1497983902614315008'
-    document.getElementById("time111").innerHTML = '2/27/22 12:18pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/21'
+    document.getElementById("title111").innerHTML = 'War in Ukraine:'
+    document.getElementById("content111").innerHTML = '<p>-Russia began its invasion of Ukraine at 5:00am, Feb 24th</p><p>-Biden has imposed new sanctions against Russia</p><p>-Zelenskyy cuts diplomatic ties with Russia.</p>'
+    document.getElementById("content0111").innerHTML = '<p>-Anonymous says that it has leaked a database from the RMD website</p><p>-(Feb 26) Kyiv makes it through the night</p> <p>-(Feb 26) U.S. Pentagon: "we stand ready if called upon by NATO"</p> <p>-(Feb 26) Anonymous intercepts Russian military communications</p><p>-(Feb 26) Anonymous has operation to keep .ru gov sites down</p><p>-(Feb 27) Peace talks between Ukraine and Russia today</p><p>-(Feb 27) EU sanctions Belarus</p><p>-(Feb 27) Canada has closed its airspace to all Russian aircraft</p><p>-(Feb 27) @MFA_Ukraine reports that "Mriya" (Dream), the largest plane in the world, has been destroyed by Russian forces</p>  <p>This is a developing situation and encourage that you exercise caution with the sources you get info from.</p><p style="padding: 15px;"></p><p><a href="https://twitter.com/StateDept" target="_blank">@StateDept (Twitter)</a></p><p><a href="https://twitter.com/NATOinUkraine" target="_blank">@NATOinUkraine (Twitter)</a></p>'
+    document.getElementById("link111").innerHTML = '@Ukraine (Twitter)'
+    document.getElementById("link111").href = 'https://twitter.com/Ukraine'
+    document.getElementById("time111").innerHTML = 'Updated 2/27/22 12:48pm MST | Updated 2/27/22 12:45pm MST | Updated 2/27/22 12:14pm MST | Updated 2/26/22 11:17am MST | Updated 2/26/22 12:32am MST | 2/24/22 2:22pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/18'
     
 
 
 
      /* Recent 4 */
-
-     document.getElementById("title1111").innerHTML = 'Elon Musks Starlink is now active in Ukraine with additional units on their way.'
-     document.getElementById("content1111").innerHTML = '<p>-5:16 am Feb 26, 2022: Mykhailo Fedorov, Minister of Digital Transformation of Ukraine, Tweeted a public plea to Elon Musk for assistance</p>'
-     document.getElementById("content01111").innerHTML = '<p>-3:33 pm Feb 26, 2022: Elon Musk replies to Fedorovs Tweet'
-     document.getElementById("link1111").innerHTML = 'Read the Tweets'
-     document.getElementById("link1111").href = 'https://twitter.com/FedorovMykhailo/status/1497543633293266944'
-     document.getElementById("time1111").innerHTML = '2/26/22 8:10pm MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/20'
+     document.getElementById("title1111").innerHTML = 'Ukrainian MFA reports on Russian losses.'
+     document.getElementById("content1111").innerHTML = '<p>-4500 wounded or killed</p><p>-200+ taken prisoner</p><p>-46 aircraft</p><p>-26 helicopters</p><p>-146 tanks</p>'
+     document.getElementById("content01111").innerHTML = '<p>-706 armored vehicles</p><p>-1 anti-aircraft system BUK</p><p>-20 automotive equipment</p><p>-49 artillery pieces</p><p>-4 Grad systems</p><p>-30 vehicles</p><p>-2 vessels</p>'
+     document.getElementById("link1111").innerHTML = 'Read the Tweet from @MFA_Ukraine'
+     document.getElementById("link1111").href = 'https://twitter.com/MFA_Ukraine/status/1497983902614315008'
+     document.getElementById("time1111").innerHTML = '2/27/22 12:18pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/21'
      
 
 
 
-     /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'There are reports of fraudulent donation requests in the name of Ukraine.'
-     document.getElementById("content11111").innerHTML = '<p>Before donating to any fund, we recommend that you research who created it and who shared it and to be cautious prior to donating.</p>'
-     document.getElementById("content011111").innerHTML = ''
-     document.getElementById("link11111").innerHTML = ''
-     document.getElementById("link11111").href = ''
-     document.getElementById("time11111").innerHTML = '2/26/22 11:17am MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/19'
+     /* Recent 5 */
+     document.getElementById("title11111").innerHTML = 'Elon Musks Starlink is now active in Ukraine with additional units on their way.'
+     document.getElementById("content11111").innerHTML = '<p>-5:16 am Feb 26, 2022: Mykhailo Fedorov, Minister of Digital Transformation of Ukraine, Tweeted a public plea to Elon Musk for assistance</p>'
+     document.getElementById("content011111").innerHTML = '<p>-3:33 pm Feb 26, 2022: Elon Musk replies to Fedorovs Tweet'
+     document.getElementById("link11111").innerHTML = 'Read the Tweets'
+     document.getElementById("link11111").href = 'https://twitter.com/FedorovMykhailo/status/1497543633293266944'
+     document.getElementById("time11111").innerHTML = '2/26/22 8:10pm MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/20'
      
 
 
