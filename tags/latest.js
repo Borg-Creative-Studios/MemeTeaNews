4df0fe2f5ce8a8dcbbef45079ef0e90c68a872b4
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-04 10:32:11 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,67 +1,73 @@
 function latest(){
     /* Top most entry */
 
-    document.getElementById("title1").innerHTML = 'Anonymous releases 140,000 emails from Russian corporation "MashOil."'
-    document.getElementById("content1").innerHTML = '<p>-The corporation develops oil drilling and fracking equipment</p> <p>-Anonymous says it will continue such attacks until the Russian aggression subsides.</p>'
+    document.getElementById("title1").innerHTML = 'Elon Musk is now the largest shareholder of Twitter stock.'
+    document.getElementById("content1").innerHTML = ''
     document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'Read the Tweet'
-    document.getElementById("link1").href = 'https://twitter.com/YourAnonNews/status/1508487379950186499'
-    document.getElementById("time1").innerHTML = '3/29/22 8:32am MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/24'
+    document.getElementById("link1").innerHTML = ''
+    document.getElementById("link1").href = ''
+    document.getElementById("time1").innerHTML = '4/4/22 10:31am MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/25'
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
 
-        document.getElementById("title11").innerHTML = 'POTUS has ammended the 2019 Alaska Disaster Declaration.'
-    document.getElementById("content11").innerHTML = '<p>-This ammendment increases the amount of funds available to those impacted by the November 30th (2018) earthquake. </p>'
+    document.getElementById("title11").innerHTML = 'Anonymous releases 140,000 emails from Russian corporation "MashOil."'
+    document.getElementById("content11").innerHTML = '<p>-The corporation develops oil drilling and fracking equipment</p> <p>-Anonymous says it will continue such attacks until the Russian aggression subsides.</p>'
     document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'WH RELEASE'
-    document.getElementById("link11").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2022/03/29/president-joseph-r-biden-jr-amends-alaska-disaster-declaration/'
-    document.getElementById("time11").innerHTML = '3/29/22 8:05am MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/23'
+    document.getElementById("link11").innerHTML = 'Read the Tweet'
+    document.getElementById("link11").href = 'https://twitter.com/YourAnonNews/status/1508487379950186499'
+    document.getElementById("time11").innerHTML = '3/29/22 8:32am MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/24'
     
+
 
 
 
     
 
     /* Recent 3 */
-    document.getElementById("title111").innerHTML = 'House approves financial aid for Ukraine.'
-    document.getElementById("content111").innerHTML = '<p>-The bill provides $13.6B to Ukraine and its allies.</p>'
+
+    document.getElementById("title111").innerHTML = 'POTUS has ammended the 2019 Alaska Disaster Declaration.'
+    document.getElementById("content111").innerHTML = '<p>-This ammendment increases the amount of funds available to those impacted by the November 30th (2018) earthquake. </p>'
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = ''
-    document.getElementById("link111").href = ''
-    document.getElementById("time111").innerHTML = '3/9/22 10:46pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/22'
+    document.getElementById("link111").innerHTML = 'WH RELEASE'
+    document.getElementById("link111").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2022/03/29/president-joseph-r-biden-jr-amends-alaska-disaster-declaration/'
+    document.getElementById("time111").innerHTML = '3/29/22 8:05am MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/23'
+    
 
 
 
 
 
      /* Recent 4 */
-     document.getElementById("title1111").innerHTML = 'War in Ukraine:'
-     document.getElementById("content1111").innerHTML = '<p>-Russia began its invasion of Ukraine at 5:00am, Feb 24th</p><p>-Biden has imposed new sanctions against Russia</p><p>-Zelenskyy cuts diplomatic ties with Russia.</p>'
-     document.getElementById("content01111").innerHTML = '<p>-Anonymous says that it has leaked a database from the RMD website</p><p>-(Feb 26) Kyiv makes it through the night</p> <p>-(Feb 26) U.S. Pentagon: "we stand ready if called upon by NATO"</p> <p>-(Feb 26) Anonymous intercepts Russian military communications</p><p>-(Feb 26) Anonymous has operation to keep .ru gov sites down</p><p>-(Feb 27) Peace talks between Ukraine and Russia today</p><p>-(Feb 27) EU sanctions Belarus</p><p>-(Feb 27) Canada has closed its airspace to all Russian aircraft</p><p>-(Feb 27) @MFA_Ukraine reports that "Mriya" (Dream), the largest plane in the world, has been destroyed by Russian forces</p>  <p>This is a developing situation and encourage that you exercise caution with the sources you get info from.</p><p style="padding: 15px;"></p><p><a href="https://twitter.com/StateDept" target="_blank">@StateDept (Twitter)</a></p><p><a href="https://twitter.com/NATOinUkraine" target="_blank">@NATOinUkraine (Twitter)</a></p>'
-     document.getElementById("link1111").innerHTML = '@Ukraine (Twitter)'
-     document.getElementById("link1111").href = 'https://twitter.com/Ukraine'
-     document.getElementById("time1111").innerHTML = 'Updated 2/27/22 12:48pm MST | Updated 2/27/22 12:45pm MST | Updated 2/27/22 12:14pm MST | Updated 2/26/22 11:17am MST | Updated 2/26/22 12:32am MST | 2/24/22 2:22pm MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/18'
-     
+
+     document.getElementById("title1111").innerHTML = 'House approves financial aid for Ukraine.'
+     document.getElementById("content1111").innerHTML = '<p>-The bill provides $13.6B to Ukraine and its allies.</p>'
+     document.getElementById("content01111").innerHTML = ''
+     document.getElementById("link1111").innerHTML = ''
+     document.getElementById("link1111").href = ''
+     document.getElementById("time1111").innerHTML = '3/9/22 10:46pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/22'
+ 
+
 
 
 
 
      /* Recent 5 */
-     document.getElementById("title11111").innerHTML = 'Ukrainian MFA reports on Russian losses.'
-     document.getElementById("content11111").innerHTML = '<p>-4500 wounded or killed</p><p>-200+ taken prisoner</p><p>-46 aircraft</p><p>-26 helicopters</p><p>-146 tanks</p>'
-     document.getElementById("content011111").innerHTML = '<p>-706 armored vehicles</p><p>-1 anti-aircraft system BUK</p><p>-20 automotive equipment</p><p>-49 artillery pieces</p><p>-4 Grad systems</p><p>-30 vehicles</p><p>-2 vessels</p>'
-     document.getElementById("link11111").innerHTML = 'Read the Tweet from @MFA_Ukraine'
-     document.getElementById("link11111").href = 'https://twitter.com/MFA_Ukraine/status/1497983902614315008'
-     document.getElementById("time11111").innerHTML = '2/27/22 12:18pm MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/21'
+     document.getElementById("title11111").innerHTML = 'War in Ukraine:'
+     document.getElementById("content11111").innerHTML = '<p>-Russia began its invasion of Ukraine at 5:00am, Feb 24th</p><p>-Biden has imposed new sanctions against Russia</p><p>-Zelenskyy cuts diplomatic ties with Russia.</p>'
+     document.getElementById("content011111").innerHTML = '<p>-Anonymous says that it has leaked a database from the RMD website</p><p>-(Feb 26) Kyiv makes it through the night</p> <p>-(Feb 26) U.S. Pentagon: "we stand ready if called upon by NATO"</p> <p>-(Feb 26) Anonymous intercepts Russian military communications</p><p>-(Feb 26) Anonymous has operation to keep .ru gov sites down</p><p>-(Feb 27) Peace talks between Ukraine and Russia today</p><p>-(Feb 27) EU sanctions Belarus</p><p>-(Feb 27) Canada has closed its airspace to all Russian aircraft</p><p>-(Feb 27) @MFA_Ukraine reports that "Mriya" (Dream), the largest plane in the world, has been destroyed by Russian forces</p>  <p>This is a developing situation and encourage that you exercise caution with the sources you get info from.</p><p style="padding: 15px;"></p><p><a href="https://twitter.com/StateDept" target="_blank">@StateDept (Twitter)</a></p><p><a href="https://twitter.com/NATOinUkraine" target="_blank">@NATOinUkraine (Twitter)</a></p>'
+     document.getElementById("link11111").innerHTML = '@Ukraine (Twitter)'
+     document.getElementById("link11111").href = 'https://twitter.com/Ukraine'
+     document.getElementById("time11111").innerHTML = 'Updated 2/27/22 12:48pm MST | Updated 2/27/22 12:45pm MST | Updated 2/27/22 12:14pm MST | Updated 2/26/22 11:17am MST | Updated 2/26/22 12:32am MST | 2/24/22 2:22pm MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/18'
      
+
 
 
 
