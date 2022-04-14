d0d51cb73fdba2a72dfffd37baf3a0baee728c52
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-14 16:57:37 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,6 +1,6 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Privacy-freindly ads are now available on OTN! '
-    document.getElementById("alink").innerHTML = 'Purchase an ad'
-    document.getElementById("alink").href = 'https://www.borgcreative.com/page12.html'
+    document.getElementById("acontent1").innerHTML = 'Manhunt underway in Phoenix for suspect. PHX PD: "Phoenix PD is still searching for a white male; Nicholas Cowan, 215 pounds, 6 0 tall, hazel eyes/brown hair. Last seen wearing a black hat, black t-shirt, black shorts. Has tattoos on neck, arms, chest and back. Armed and dangerous. If sighted call 911."'
+    document.getElementById("alink").innerHTML = 'Read PHX PDs Tweet'
+    document.getElementById("alink").href = 'https://twitter.com/PhoenixPolice/status/1514747707923087365'
 }
 
