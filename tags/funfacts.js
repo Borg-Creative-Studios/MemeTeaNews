5239bf2087473168b40eb0c23df258f6bcca5241
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-14 17:08:59 -0700

    Update funfacts.js

--- tags/funfacts.js
+++ tags/funfacts.js
@@ -1,18 +1,18 @@
 function funfacts(){
     /* older 1 */
 
-    document.getElementById("fftitle1").innerHTML = 'Moles and Chemistry'
-    document.getElementById("ffcontent1").innerHTML = '<p>Moles are a measurement unit used in chemistry.</p>'
-    document.getElementById("ffcontent2").innerHTML = '<p>1 mole is equal to 6.02x10^23 and is the number of atoms in an elements atomic weight.</p><p>EXAMPLE: 1.008g of Hydrogen has 1 mole of atoms.</p>'
+    document.getElementById("fftitle1").innerHTML = 'The New York Stock Exchange'
+    document.getElementById("ffcontent1").innerHTML = '<p>The exchange was started in 1792.</p>'
+    document.getElementById("ffcontent2").innerHTML = '<p>The exchange began with just 24 stockbrokers and has evolved into one of the largest marketplaces for securities and investments.</p>'
     document.getElementById("fflink1").innerHTML = ''
     document.getElementById("fflink1").href = ''
     document.getElementById("fftime1").innerHTML = ''
 
     /* older 2 */
 
-    document.getElementById("fftitle2").innerHTML = 'The Mona Lisa'
-    document.getElementById("ffcontent3").innerHTML = '<p>The Mona Lisa was painted by Leonardo Da Vinci between 1503 and 1506.</p>'
-    document.getElementById("ffcontent4").innerHTML = '<p>The Mona Lisa is a painting of noblewoman Lisa Gherardini.</p><p>The painting is on display in the Louvre in Paris France.</p>'
+    document.getElementById("fftitle2").innerHTML = 'Moles and Chemistry'
+    document.getElementById("ffcontent3").innerHTML = '<p>Moles are a measurement unit used in chemistry.</p>'
+    document.getElementById("ffcontent4").innerHTML = '<p>1 mole is equal to 6.02x10^23 and is the number of atoms in an elements atomic weight.</p><p>EXAMPLE: 1.008g of Hydrogen has 1 mole of atoms.</p>'
     document.getElementById("fflink2").innerHTML = ''
     document.getElementById("fflink2").href = ''
     document.getElementById("fftime2").innerHTML = ''
