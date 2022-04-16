f7ce1b33bdfe6713fd4a2addae0e63e9398278b8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-16 16:32:38 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,69 +1,62 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'Elon Musk is attempting a hostile take over of Twitter.'
-    document.getElementById("content1").innerHTML = 'Musk offered to purchase Twitter at a premium to shareholders.'
+    document.getElementById("title1").innerHTML = 'The total # of people employed in Texas has hit an all-time high.'
+    document.getElementById("content1").innerHTML = 'Gov. Greg Abbott Tweeted that the total # of employed in Texas is about 13.2 million.'
     document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = ''
-    document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '4/14/22 5:00pm MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/27'
+    document.getElementById("link1").innerHTML = 'Read the Tweet'
+    document.getElementById("link1").href = 'https://twitter.com/GregAbbott_TX/status/1515410620853723143'
+    document.getElementById("time1").innerHTML = '4/16/22 4:32pm MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/28'
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
 
-    document.getElementById("title11").innerHTML = 'Elon Musk to join Twitters board of executives.'
-    document.getElementById("content11").innerHTML = ''
+    document.getElementById("title11").innerHTML = 'Elon Musk is attempting a hostile take over of Twitter.'
+    document.getElementById("content11").innerHTML = 'Musk offered to purchase Twitter at a premium to shareholders.'
     document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'Read the Tweet'
-    document.getElementById("link11").href = 'https://twitter.com/paraga/status/1511320953598357505'
-    document.getElementById("time11").innerHTML = '4/5/22 10:55am MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/26'
-
+    document.getElementById("link11").innerHTML = ''
+    document.getElementById("link11").href = ''
+    document.getElementById("time11").innerHTML = '4/14/22 5:00pm MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/27'
 
     
 
     /* Recent 3 */
 
-    document.getElementById("title111").innerHTML = 'Elon Musk is now the largest shareholder of Twitter stock.'
+    document.getElementById("title111").innerHTML = 'Elon Musk to join Twitters board of executives.'
     document.getElementById("content111").innerHTML = ''
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = ''
-    document.getElementById("link111").href = ''
-    document.getElementById("time111").innerHTML = '4/4/22 10:31am MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/25'
-
-
-    
-
+    document.getElementById("link111").innerHTML = 'Read the Tweet'
+    document.getElementById("link111").href = 'https://twitter.com/paraga/status/1511320953598357505'
+    document.getElementById("time111").innerHTML = '4/5/22 10:55am MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/26'
 
 
 
 
      /* Recent 4 */
 
-     document.getElementById("title1111").innerHTML = 'Anonymous releases 140,000 emails from Russian corporation "MashOil."'
-     document.getElementById("content1111").innerHTML = '<p>-The corporation develops oil drilling and fracking equipment</p> <p>-Anonymous says it will continue such attacks until the Russian aggression subsides.</p>'
+     document.getElementById("title1111").innerHTML = 'Elon Musk is now the largest shareholder of Twitter stock.'
+     document.getElementById("content1111").innerHTML = ''
      document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = 'Read the Tweet'
-     document.getElementById("link1111").href = 'https://twitter.com/YourAnonNews/status/1508487379950186499'
-     document.getElementById("time1111").innerHTML = '3/29/22 8:32am MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/24'
-
-
-
+     document.getElementById("link1111").innerHTML = ''
+     document.getElementById("link1111").href = ''
+     document.getElementById("time1111").innerHTML = '4/4/22 10:31am MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/25'
+ 
 
 
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'POTUS has ammended the 2019 Alaska Disaster Declaration.'
-     document.getElementById("content11111").innerHTML = '<p>-This ammendment increases the amount of funds available to those impacted by the November 30th (2018) earthquake. </p>'
+     document.getElementById("title11111").innerHTML = 'Anonymous releases 140,000 emails from Russian corporation "MashOil."'
+     document.getElementById("content11111").innerHTML = '<p>-The corporation develops oil drilling and fracking equipment</p> <p>-Anonymous says it will continue such attacks until the Russian aggression subsides.</p>'
      document.getElementById("content011111").innerHTML = ''
-     document.getElementById("link11111").innerHTML = 'WH RELEASE'
-     document.getElementById("link11111").href = 'https://www.whitehouse.gov/briefing-room/statements-releases/2022/03/29/president-joseph-r-biden-jr-amends-alaska-disaster-declaration/'
-     document.getElementById("time11111").innerHTML = '3/29/22 8:05am MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/23'
+     document.getElementById("link11111").innerHTML = 'Read the Tweet'
+     document.getElementById("link11111").href = 'https://twitter.com/YourAnonNews/status/1508487379950186499'
+     document.getElementById("time11111").innerHTML = '3/29/22 8:32am MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/24'
 
 
 
