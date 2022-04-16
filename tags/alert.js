4c299d85386ffa7517e246f10f20605fd72478ab
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-16 16:27:09 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,6 +1,6 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Manhunt underway in Phoenix for suspect. PHX PD: "Phoenix PD is still searching for a white male; Nicholas Cowan, 215 pounds, 6 0 tall, hazel eyes/brown hair. Last seen wearing a black hat, black t-shirt, black shorts. Has tattoos on neck, arms, chest and back. Armed and dangerous. If sighted call 911."'
-    document.getElementById("alink").innerHTML = 'Read PHX PDs Tweet'
-    document.getElementById("alink").href = 'https://twitter.com/PhoenixPolice/status/1514747707923087365'
+    document.getElementById("acontent1").innerHTML = '"The ATF has now matched @SilentwitnessAZ reward of up to $5,000 for a combined total of $10,000 for the arrest of Nicholas Cowan. @ATFHQ @ATFPhoenix" -PHX PD via Twitter'
+    document.getElementById("alink").innerHTML = 'Read the Tweet'
+    document.getElementById("alink").href = 'https://twitter.com/PhoenixPolice/status/1515038781006499841'
 }
 
