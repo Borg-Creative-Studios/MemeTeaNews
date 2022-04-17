266759685c72bc8ad7308b49e7e4f1fac571022c
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-16 20:24:18 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = '"The ATF has now matched @SilentwitnessAZ reward of up to $5,000 for a combined total of $10,000 for the arrest of Nicholas Cowan. @ATFHQ @ATFPhoenix" -PHX PD via Twitter'
+    document.getElementById("acontent1").innerHTML = '"The ATF has now matched @SilentwitnessAZ reward of up to $5,000 for a combined total of $10,000 for the arrest of Nicholas Cowan. @ATFHQ @ATFPhoenix" -PHX PD via Twitter '
     document.getElementById("alink").innerHTML = 'Read the Tweet'
     document.getElementById("alink").href = 'https://twitter.com/PhoenixPolice/status/1515038781006499841'
 }
