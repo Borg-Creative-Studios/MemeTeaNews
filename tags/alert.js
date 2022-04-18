8af06b106aff25a0b7e28a8a83b2e8c2aef58c4b
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-18 10:45:48 -0700

    update

--- tags/alert.js
+++ tags/alert.js
@@ -1,6 +1,6 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = '"The ATF has now matched @SilentwitnessAZ reward of up to $5,000 for a combined total of $10,000 for the arrest of Nicholas Cowan. @ATFHQ @ATFPhoenix" -PHX PD via Twitter '
-    document.getElementById("alink").innerHTML = 'Read the Tweet'
-    document.getElementById("alink").href = 'https://twitter.com/PhoenixPolice/status/1515038781006499841'
+    document.getElementById("acontent1").innerHTML = '"Nicholas Cowan, the suspect wanted for shooting a Phoenix Police Officer, has been arrested." -PHX PD via Twitter '
+    document.getElementById("alink").innerHTML = ''
+    document.getElementById("alink").href = ''
 }
 
