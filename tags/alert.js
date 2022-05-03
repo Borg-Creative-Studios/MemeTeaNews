496c7d0fc8aabdf9372e91fc8c0abd917a5047b9
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-05-03 16:22:37 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = '"Nicholas Cowan, the suspect wanted for shooting a Phoenix Police Officer, has been arrested." -PHX PD via Twitter '
+    document.getElementById("acontent1").innerHTML = 'According to leaked documents and POLITICO, SCOTUS is set to overturn Roe Vs. Wade.'
     document.getElementById("alink").innerHTML = ''
     document.getElementById("alink").href = ''
 }
