847fcace081faade80a249896ad456cbadd5f621
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-05-03 16:50:40 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -14,49 +14,49 @@ function latest(){
     /* Recent 2 */
 
     document.getElementById("title1").innerHTML = 'Tempe FD, PHX FD, and PHX PD rescue injured hiker from Camelback Mountain.'
-    document.getElementById("content1").innerHTML = '"We please ask you to take all safety precautions before, during and after your hike." -@PHXFire'
-    document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'Read the Tweets'
-    document.getElementById("link1").href = 'https://twitter.com/PHXFire/status/1515418491892879360'
-    document.getElementById("time1").innerHTML = '4/16/22 4:42pm MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/29'
+    document.getElementById("content11").innerHTML = '"We please ask you to take all safety precautions before, during and after your hike." -@PHXFire'
+    document.getElementById("content011").innerHTML = ''
+    document.getElementById("link11").innerHTML = 'Read the Tweets'
+    document.getElementById("link11").href = 'https://twitter.com/PHXFire/status/1515418491892879360'
+    document.getElementById("time11").innerHTML = '4/16/22 4:42pm MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/29'
     
 
     
 
     /* Recent 3 */
 
-    document.getElementById("title11").innerHTML = 'The total # of people employed in Texas has hit an all-time high.'
-    document.getElementById("content11").innerHTML = 'Gov. Greg Abbott Tweeted that the total # of employed in Texas is about 13.2 million.'
-    document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'Read the Tweet'
-    document.getElementById("link11").href = 'https://twitter.com/GregAbbott_TX/status/1515410620853723143'
-    document.getElementById("time11").innerHTML = '4/16/22 4:32pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/28'
+    document.getElementById("title111").innerHTML = 'The total # of people employed in Texas has hit an all-time high.'
+    document.getElementById("content111").innerHTML = 'Gov. Greg Abbott Tweeted that the total # of employed in Texas is about 13.2 million.'
+    document.getElementById("content0111").innerHTML = ''
+    document.getElementById("link111").innerHTML = 'Read the Tweet'
+    document.getElementById("link111").href = 'https://twitter.com/GregAbbott_TX/status/1515410620853723143'
+    document.getElementById("time111").innerHTML = '4/16/22 4:32pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/28'
     
 
 
      /* Recent 4 */
 
-     document.getElementById("title111").innerHTML = 'Elon Musk is attempting a hostile take over of Twitter.'
-     document.getElementById("content111").innerHTML = 'Musk offered to purchase Twitter at a premium to shareholders.'
-     document.getElementById("content0111").innerHTML = ''
-     document.getElementById("link111").innerHTML = ''
-     document.getElementById("link111").href = ''
-     document.getElementById("time111").innerHTML = '4/14/22 5:00pm MST'
-     document.getElementById("telegram111").href = 'https://t.me/openteanews/27'
+     document.getElementById("title1111").innerHTML = 'Elon Musk is attempting a hostile take over of Twitter.'
+     document.getElementById("content1111").innerHTML = 'Musk offered to purchase Twitter at a premium to shareholders.'
+     document.getElementById("content01111").innerHTML = ''
+     document.getElementById("link1111").innerHTML = ''
+     document.getElementById("link1111").href = ''
+     document.getElementById("time1111").innerHTML = '4/14/22 5:00pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/27'
  
  
 
      /* Recent 5 */
 
-     document.getElementById("title1111").innerHTML = 'Elon Musk to join Twitters board of executives.'
-     document.getElementById("content1111").innerHTML = ''
-     document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = 'Read the Tweet'
-     document.getElementById("link1111").href = 'https://twitter.com/paraga/status/1511320953598357505'
-     document.getElementById("time1111").innerHTML = '4/5/22 10:55am MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/26'
+     document.getElementById("title11111").innerHTML = 'Elon Musk to join Twitters board of executives.'
+     document.getElementById("content11111").innerHTML = ''
+     document.getElementById("content011111").innerHTML = ''
+     document.getElementById("link11111").innerHTML = 'Read the Tweet'
+     document.getElementById("link11111").href = 'https://twitter.com/paraga/status/1511320953598357505'
+     document.getElementById("time11111").innerHTML = '4/5/22 10:55am MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/26'
  
 
 
