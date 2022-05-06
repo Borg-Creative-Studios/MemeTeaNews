d30ae117eee37282c7c00b2f983739ac75cbf621
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-05-05 18:24:04 -0700

    updates

--- tags/latest.js
+++ tags/latest.js
@@ -1,62 +1,63 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'CA State Sen. Melissa Melendez reports that SB 1142 has been introduced by Dems.'
-    document.getElementById("content1").innerHTML = '"...Democrats in CA have introduced SB 1142 which would invite women across the country to come to CA for an abortion, paid for by CA." -@senatormelendez'
-    document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'Read the Tweet'
-    document.getElementById("link1").href = 'https://twitter.com/senatormelendez/status/1521332604548517888'
-    document.getElementById("time1").innerHTML = '5/3/22 4:42pm MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/30'
+    document.getElementById("title1").innerHTML = 'Breaking911 Tweet labeled as "misleading" and its interactions were disabled for a brief time.'
+    document.getElementById("content1").innerHTML = 'Breaking911 <a href="https://twitter.com/Breaking911/status/1522331986454409217" target="_blank">tweeted</a> about the FDA restricting the J&J COVID vaccine due to blood clotting concerns.'
+    document.getElementById("content01").innerHTML = 'Twitter has since re-enabled interactions on the tweet.'
+    document.getElementById("link1").innerHTML = 'Read Breaking911s response'
+    document.getElementById("link1").href = 'https://twitter.com/Breaking911/status/1522345312744095745'
+    document.getElementById("time1").innerHTML = '5/5/22 6:22pm MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/31'
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
 
-    document.getElementById("title11").innerHTML = 'Tempe FD, PHX FD, and PHX PD rescue injured hiker from Camelback Mountain.'
-    document.getElementById("content11").innerHTML = '"We please ask you to take all safety precautions before, during and after your hike." -@PHXFire'
+    document.getElementById("title11").innerHTML = 'CA State Sen. Melissa Melendez reports that SB 1142 has been introduced by Dems.'
+    document.getElementById("content11").innerHTML = '"...Democrats in CA have introduced SB 1142 which would invite women across the country to come to CA for an abortion, paid for by CA." -@senatormelendez'
     document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'Read the Tweets'
-    document.getElementById("link11").href = 'https://twitter.com/PHXFire/status/1515418491892879360'
-    document.getElementById("time11").innerHTML = '4/16/22 4:42pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/29'
+    document.getElementById("link11").innerHTML = 'Read the Tweet'
+    document.getElementById("link11").href = 'https://twitter.com/senatormelendez/status/1521332604548517888'
+    document.getElementById("time11").innerHTML = '5/3/22 4:42pm MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/30'
     
 
     
 
     /* Recent 3 */
 
-    document.getElementById("title111").innerHTML = 'The total # of people employed in Texas has hit an all-time high.'
-    document.getElementById("content111").innerHTML = 'Gov. Greg Abbott Tweeted that the total # of employed in Texas is about 13.2 million.'
+    document.getElementById("title111").innerHTML = 'Tempe FD, PHX FD, and PHX PD rescue injured hiker from Camelback Mountain.'
+    document.getElementById("content111").innerHTML = '"We please ask you to take all safety precautions before, during and after your hike." -@PHXFire'
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = 'Read the Tweet'
-    document.getElementById("link111").href = 'https://twitter.com/GregAbbott_TX/status/1515410620853723143'
-    document.getElementById("time111").innerHTML = '4/16/22 4:32pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/28'
+    document.getElementById("link111").innerHTML = 'Read the Tweets'
+    document.getElementById("link111").href = 'https://twitter.com/PHXFire/status/1515418491892879360'
+    document.getElementById("time111").innerHTML = '4/16/22 4:42pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/29'
     
 
 
      /* Recent 4 */
 
-     document.getElementById("title1111").innerHTML = 'Elon Musk is attempting a hostile take over of Twitter.'
-     document.getElementById("content1111").innerHTML = 'Musk offered to purchase Twitter at a premium to shareholders.'
+     document.getElementById("title1111").innerHTML = 'The total # of people employed in Texas has hit an all-time high.'
+     document.getElementById("content1111").innerHTML = 'Gov. Greg Abbott Tweeted that the total # of employed in Texas is about 13.2 million.'
      document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = ''
-     document.getElementById("link1111").href = ''
-     document.getElementById("time1111").innerHTML = '4/14/22 5:00pm MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/27'
- 
- 
+     document.getElementById("link1111").innerHTML = 'Read the Tweet'
+     document.getElementById("link1111").href = 'https://twitter.com/GregAbbott_TX/status/1515410620853723143'
+     document.getElementById("time1111").innerHTML = '4/16/22 4:32pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/28'
+     
+
 
      /* Recent 5 */
 
-     document.getElementById("title11111").innerHTML = 'Elon Musk to join Twitters board of executives.'
-     document.getElementById("content11111").innerHTML = ''
+     document.getElementById("title11111").innerHTML = 'Elon Musk is attempting a hostile take over of Twitter.'
+     document.getElementById("content11111").innerHTML = 'Musk offered to purchase Twitter at a premium to shareholders.'
      document.getElementById("content011111").innerHTML = ''
-     document.getElementById("link11111").innerHTML = 'Read the Tweet'
-     document.getElementById("link11111").href = 'https://twitter.com/paraga/status/1511320953598357505'
-     document.getElementById("time11111").innerHTML = '4/5/22 10:55am MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/26'
+     document.getElementById("link11111").innerHTML = ''
+     document.getElementById("link11111").href = ''
+     document.getElementById("time11111").innerHTML = '4/14/22 5:00pm MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/27'
+ 
  
 
 
--- tags/older.js
+++ tags/older.js
@@ -1,22 +1,24 @@
 function older(){
     /* older 1 */
-    document.getElementById("otitle1").innerHTML = 'War in Ukraine:'
-    document.getElementById("ocontent1").innerHTML = '<p>-Russia began its invasion of Ukraine at 5:00am, Feb 24th</p><p>-Biden has imposed new sanctions against Russia</p><p>-Zelenskyy cuts diplomatic ties with Russia.</p>'
-    document.getElementById("ocontent2").innerHTML = '<p>-Anonymous says that it has leaked a database from the RMD website</p><p>-(Feb 26) Kyiv makes it through the night</p> <p>-(Feb 26) U.S. Pentagon: "we stand ready if called upon by NATO"</p> <p>-(Feb 26) Anonymous intercepts Russian military communications</p><p>-(Feb 26) Anonymous has operation to keep .ru gov sites down</p><p>-(Feb 27) Peace talks between Ukraine and Russia today</p><p>-(Feb 27) EU sanctions Belarus</p><p>-(Feb 27) Canada has closed its airspace to all Russian aircraft</p><p>-(Feb 27) @MFA_Ukraine reports that "Mriya" (Dream), the largest plane in the world, has been destroyed by Russian forces</p>  <p>This is a developing situation and encourage that you exercise caution with the sources you get info from.</p><p style="padding: 15px;"></p><p><a href="https://twitter.com/StateDept" target="_blank">@StateDept (Twitter)</a></p><p><a href="https://twitter.com/NATOinUkraine" target="_blank">@NATOinUkraine (Twitter)</a></p>'
-    document.getElementById("olink1").innerHTML = '@Ukraine (Twitter)'
-    document.getElementById("olink1").href = 'https://twitter.com/Ukraine'
-    document.getElementById("otime1").innerHTML = 'Updated 2/27/22 12:48pm MST | Updated 2/27/22 12:45pm MST | Updated 2/27/22 12:14pm MST | Updated 2/26/22 11:17am MST | Updated 2/26/22 12:32am MST | 2/24/22 2:22pm MST'
+
+    document.getElementById("otitle1").innerHTML = 'Elon Musk to join Twitters board of executives.'
+    document.getElementById("ocontent1").innerHTML = ''
+    document.getElementById("ocontent2").innerHTML = ''
+    document.getElementById("olink1").innerHTML = 'Read the Tweet'
+    document.getElementById("olink1").href = 'https://twitter.com/paraga/status/1511320953598357505'
+    document.getElementById("otime1").innerHTML = '4/5/22 10:55am MST'
 
 
 
     
     /* older 2 */
 
-    document.getElementById("otitle2").innerHTML = 'All PHX PD officers harmed on Friday have been released from the hospital.'
-    document.getElementById("ocontent3").innerHTML = ''
-    document.getElementById("ocontent4").innerHTML = ''
-    document.getElementById("olink2").innerHTML = ''
-    document.getElementById("olink2").href = ''
-    document.getElementById("otime2").innerHTML = '2/17/22 3:32pm MST'
+    document.getElementById("otitle2").innerHTML = 'War in Ukraine:'
+    document.getElementById("ocontent3").innerHTML = '<p>-Russia began its invasion of Ukraine at 5:00am, Feb 24th</p><p>-Biden has imposed new sanctions against Russia</p><p>-Zelenskyy cuts diplomatic ties with Russia.</p>'
+    document.getElementById("ocontent4").innerHTML = '<p>-Anonymous says that it has leaked a database from the RMD website</p><p>-(Feb 26) Kyiv makes it through the night</p> <p>-(Feb 26) U.S. Pentagon: "we stand ready if called upon by NATO"</p> <p>-(Feb 26) Anonymous intercepts Russian military communications</p><p>-(Feb 26) Anonymous has operation to keep .ru gov sites down</p><p>-(Feb 27) Peace talks between Ukraine and Russia today</p><p>-(Feb 27) EU sanctions Belarus</p><p>-(Feb 27) Canada has closed its airspace to all Russian aircraft</p><p>-(Feb 27) @MFA_Ukraine reports that "Mriya" (Dream), the largest plane in the world, has been destroyed by Russian forces</p>  <p>This is a developing situation and encourage that you exercise caution with the sources you get info from.</p><p style="padding: 15px;"></p><p><a href="https://twitter.com/StateDept" target="_blank">@StateDept (Twitter)</a></p><p><a href="https://twitter.com/NATOinUkraine" target="_blank">@NATOinUkraine (Twitter)</a></p>'
+    document.getElementById("olink2").innerHTML = '@Ukraine (Twitter)'
+    document.getElementById("olink2").href = 'https://twitter.com/Ukraine'
+    document.getElementById("otime2").innerHTML = 'Updated 2/27/22 12:48pm MST | Updated 2/27/22 12:45pm MST | Updated 2/27/22 12:14pm MST | Updated 2/26/22 11:17am MST | Updated 2/26/22 12:32am MST | 2/24/22 2:22pm MST'
+
 
 }
