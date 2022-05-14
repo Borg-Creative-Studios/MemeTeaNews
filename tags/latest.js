1de2ff5ad4d3776e3abcd1d97e2553d164bed5bf
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-05-14 16:37:16 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,17 +1,31 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'Jen Psaki via Twitter:'
-    document.getElementById("content1").innerHTML = '".<a href="https://twitter.com/POTUS" target="_blank">@POTUS</a> strongly believes in the Constitutional right to protest. But that should never include violence, threats, or vandalism. Judges perform an incredibly important function in our society, and they must be able to do their jobs without concern for their personal safety."'
-    document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = 'Read the Tweet'
-    document.getElementById("link1").href = 'https://twitter.com/PressSec/status/1523649143951962115'
-    document.getElementById("time1").innerHTML = '5/9/22 9:45am MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/33'
+    document.getElementById("title1").innerHTML = '10 killed in supermarket shooting; Suspect in custody'
+    document.getElementById("content1").innerHTML = 'Buffalo, NY: A person opened fire in a supermarket prior to exiting and surrendering to authorities.'
+    document.getElementById("content01").innerHTML = '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">BREAKING: BPD on scene of a mass shooting at the Tops in the 1200 block of Jefferson Avenue. Police say multiple people have been struck by gunfire. The shooter is in custody. Motorists and residents are urged to avoid the area.</p>&mdash; Buffalo Police Dept (@BPDAlerts) <a href="https://twitter.com/BPDAlerts/status/1525558280981991428?ref_src=twsrc%5Etfw">May 14, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> '
+    document.getElementById("link1").innerHTML = ''
+    document.getElementById("link1").href = ''
+    document.getElementById("time1").innerHTML = '5/14/22 4:37pm MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/34'
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
+
+        document.getElementById("title1").innerHTML = 'Jen Psaki via Twitter:'
+    document.getElementById("content1").innerHTML = '".<a href="https://twitter.com/POTUS" target="_blank">@POTUS</a> strongly believes in the Constitutional right to protest. But that should never include violence, threats, or vandalism. Judges perform an incredibly important function in our society, and they must be able to do their jobs without concern for their personal safety."'
+    document.getElementById("content01").innerHTML = '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">.<a href="https://twitter.com/POTUS?ref_src=twsrc%5Etfw">@POTUS</a> strongly believes in the Constitutional right to protest. But that should never include violence, threats, or vandalism. Judges perform an incredibly important function in our society, and they must be able to do their jobs without concern for their personal safety.</p>&mdash; Karine Jean-Pierre (@PressSec) <a href="https://twitter.com/PressSec/status/1523649143951962115?ref_src=twsrc%5Etfw">May 9, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> '
+    document.getElementById("link1").innerHTML = 'Read the Tweet'
+    document.getElementById("link1").href = 'https://twitter.com/PressSec/status/1523649143951962115'
+    document.getElementById("time1").innerHTML = '5/9/22 9:45am MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/33'
+
+
+
+    
+
+    /* Recent 3 */
 
     document.getElementById("title11").innerHTML = 'Breaking911 Tweet labeled as "misleading" and its interactions were disabled for a brief time.'
     document.getElementById("content11").innerHTML = 'Breaking911 <a href="https://twitter.com/Breaking911/status/1522331986454409217" target="_blank">tweeted</a> about the FDA restricting the J&J COVID vaccine due to blood clotting concerns.'
@@ -22,21 +36,23 @@ function latest(){
     document.getElementById("telegram11").href = 'https://t.me/openteanews/32'
     
 
-    
-
-    /* Recent 3 */
-
-    document.getElementById("title111").innerHTML = 'CA State Sen. Melissa Melendez reports that SB 1142 has been introduced by Dems.'
-    document.getElementById("content111").innerHTML = '"...Democrats in CA have introduced SB 1142 which would invite women across the country to come to CA for an abortion, paid for by CA." -@senatormelendez'
-    document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = 'Read the Tweet'
-    document.getElementById("link111").href = 'https://twitter.com/senatormelendez/status/1521332604548517888'
-    document.getElementById("time111").innerHTML = '5/3/22 4:42pm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/31'
-    
 
 
      /* Recent 4 */
+
+     document.getElementById("title111").innerHTML = 'CA State Sen. Melissa Melendez reports that SB 1142 has been introduced by Dems.'
+     document.getElementById("content111").innerHTML = '"...Democrats in CA have introduced SB 1142 which would invite women across the country to come to CA for an abortion, paid for by CA." -@senatormelendez'
+     document.getElementById("content0111").innerHTML = ''
+     document.getElementById("link111").innerHTML = 'Read the Tweet'
+     document.getElementById("link111").href = 'https://twitter.com/senatormelendez/status/1521332604548517888'
+     document.getElementById("time111").innerHTML = '5/3/22 4:42pm MST'
+     document.getElementById("telegram111").href = 'https://t.me/openteanews/31'
+     
+
+
+
+
+     /* Recent 5 */
 
      document.getElementById("title1111").innerHTML = 'Tempe FD, PHX FD, and PHX PD rescue injured hiker from Camelback Mountain.'
      document.getElementById("content1111").innerHTML = '"We please ask you to take all safety precautions before, during and after your hike." -@PHXFire'
@@ -46,19 +62,7 @@ function latest(){
      document.getElementById("time1111").innerHTML = '4/16/22 4:42pm MST'
      document.getElementById("telegram1111").href = 'https://t.me/openteanews/29'
      
-
-
-
-     /* Recent 5 */
-
-     document.getElementById("title11111").innerHTML = 'The total # of people employed in Texas has hit an all-time high.'
-     document.getElementById("content11111").innerHTML = 'Gov. Greg Abbott Tweeted that the total # of employed in Texas is about 13.2 million.'
-     document.getElementById("content011111").innerHTML = ''
-     document.getElementById("link11111").innerHTML = 'Read the Tweet'
-     document.getElementById("link11111").href = 'https://twitter.com/GregAbbott_TX/status/1515410620853723143'
-     document.getElementById("time11111").innerHTML = '4/16/22 4:32pm MST'
-     document.getElementById("telegram11111").href = 'https://t.me/openteanews/28'
-     
+ 
  
 
 
