d0c139271eb53d6c4da77e1abc750ca2d8cf3bba
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-05-14 16:38:29 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -13,13 +13,13 @@ function latest(){
 
     /* Recent 2 */
 
-        document.getElementById("title1").innerHTML = 'Jen Psaki via Twitter:'
-    document.getElementById("content1").innerHTML = '".<a href="https://twitter.com/POTUS" target="_blank">@POTUS</a> strongly believes in the Constitutional right to protest. But that should never include violence, threats, or vandalism. Judges perform an incredibly important function in our society, and they must be able to do their jobs without concern for their personal safety."'
-    document.getElementById("content01").innerHTML = '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">.<a href="https://twitter.com/POTUS?ref_src=twsrc%5Etfw">@POTUS</a> strongly believes in the Constitutional right to protest. But that should never include violence, threats, or vandalism. Judges perform an incredibly important function in our society, and they must be able to do their jobs without concern for their personal safety.</p>&mdash; Karine Jean-Pierre (@PressSec) <a href="https://twitter.com/PressSec/status/1523649143951962115?ref_src=twsrc%5Etfw">May 9, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> '
-    document.getElementById("link1").innerHTML = 'Read the Tweet'
-    document.getElementById("link1").href = 'https://twitter.com/PressSec/status/1523649143951962115'
-    document.getElementById("time1").innerHTML = '5/9/22 9:45am MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/33'
+        document.getElementById("title11").innerHTML = 'Jen Psaki via Twitter:'
+    document.getElementById("content11").innerHTML = '".<a href="https://twitter.com/POTUS" target="_blank">@POTUS</a> strongly believes in the Constitutional right to protest. But that should never include violence, threats, or vandalism. Judges perform an incredibly important function in our society, and they must be able to do their jobs without concern for their personal safety."'
+    document.getElementById("content011").innerHTML = '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">.<a href="https://twitter.com/POTUS?ref_src=twsrc%5Etfw">@POTUS</a> strongly believes in the Constitutional right to protest. But that should never include violence, threats, or vandalism. Judges perform an incredibly important function in our society, and they must be able to do their jobs without concern for their personal safety.</p>&mdash; Karine Jean-Pierre (@PressSec) <a href="https://twitter.com/PressSec/status/1523649143951962115?ref_src=twsrc%5Etfw">May 9, 2022</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> '
+    document.getElementById("link11").innerHTML = 'Read the Tweet'
+    document.getElementById("link11").href = 'https://twitter.com/PressSec/status/1523649143951962115'
+    document.getElementById("time11").innerHTML = '5/9/22 9:45am MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/33'
 
 
 
@@ -27,26 +27,26 @@ function latest(){
 
     /* Recent 3 */
 
-    document.getElementById("title11").innerHTML = 'Breaking911 Tweet labeled as "misleading" and its interactions were disabled for a brief time.'
-    document.getElementById("content11").innerHTML = 'Breaking911 <a href="https://twitter.com/Breaking911/status/1522331986454409217" target="_blank">tweeted</a> about the FDA restricting the J&J COVID vaccine due to blood clotting concerns.'
-    document.getElementById("content011").innerHTML = 'Twitter has since re-enabled interactions on the tweet and removed the "misleading" label.'
-    document.getElementById("link11").innerHTML = 'Read Breaking911s response'
-    document.getElementById("link11").href = 'https://twitter.com/Breaking911/status/1522345312744095745'
-    document.getElementById("time11").innerHTML = '5/5/22 6:22pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/32'
+    document.getElementById("title111").innerHTML = 'Breaking911 Tweet labeled as "misleading" and its interactions were disabled for a brief time.'
+    document.getElementById("content111").innerHTML = 'Breaking911 <a href="https://twitter.com/Breaking911/status/1522331986454409217" target="_blank">tweeted</a> about the FDA restricting the J&J COVID vaccine due to blood clotting concerns.'
+    document.getElementById("content0111").innerHTML = 'Twitter has since re-enabled interactions on the tweet and removed the "misleading" label.'
+    document.getElementById("link111").innerHTML = 'Read Breaking911s response'
+    document.getElementById("link111").href = 'https://twitter.com/Breaking911/status/1522345312744095745'
+    document.getElementById("time111").innerHTML = '5/5/22 6:22pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/32'
     
 
 
 
      /* Recent 4 */
 
-     document.getElementById("title111").innerHTML = 'CA State Sen. Melissa Melendez reports that SB 1142 has been introduced by Dems.'
-     document.getElementById("content111").innerHTML = '"...Democrats in CA have introduced SB 1142 which would invite women across the country to come to CA for an abortion, paid for by CA." -@senatormelendez'
-     document.getElementById("content0111").innerHTML = ''
-     document.getElementById("link111").innerHTML = 'Read the Tweet'
-     document.getElementById("link111").href = 'https://twitter.com/senatormelendez/status/1521332604548517888'
-     document.getElementById("time111").innerHTML = '5/3/22 4:42pm MST'
-     document.getElementById("telegram111").href = 'https://t.me/openteanews/31'
+     document.getElementById("title1111").innerHTML = 'CA State Sen. Melissa Melendez reports that SB 1142 has been introduced by Dems.'
+     document.getElementById("content1111").innerHTML = '"...Democrats in CA have introduced SB 1142 which would invite women across the country to come to CA for an abortion, paid for by CA." -@senatormelendez'
+     document.getElementById("content01111").innerHTML = ''
+     document.getElementById("link1111").innerHTML = 'Read the Tweet'
+     document.getElementById("link1111").href = 'https://twitter.com/senatormelendez/status/1521332604548517888'
+     document.getElementById("time1111").innerHTML = '5/3/22 4:42pm MST'
+     document.getElementById("telegram1111").href = 'https://t.me/openteanews/31'
      
 
 
@@ -54,13 +54,13 @@ function latest(){
 
      /* Recent 5 */
 
-     document.getElementById("title1111").innerHTML = 'Tempe FD, PHX FD, and PHX PD rescue injured hiker from Camelback Mountain.'
-     document.getElementById("content1111").innerHTML = '"We please ask you to take all safety precautions before, during and after your hike." -@PHXFire'
-     document.getElementById("content01111").innerHTML = ''
-     document.getElementById("link1111").innerHTML = 'Read the Tweets'
-     document.getElementById("link1111").href = 'https://twitter.com/PHXFire/status/1515418491892879360'
-     document.getElementById("time1111").innerHTML = '4/16/22 4:42pm MST'
-     document.getElementById("telegram1111").href = 'https://t.me/openteanews/29'
+     document.getElementById("title11111").innerHTML = 'Tempe FD, PHX FD, and PHX PD rescue injured hiker from Camelback Mountain.'
+     document.getElementById("content11111").innerHTML = '"We please ask you to take all safety precautions before, during and after your hike." -@PHXFire'
+     document.getElementById("content011111").innerHTML = ''
+     document.getElementById("link11111").innerHTML = 'Read the Tweets'
+     document.getElementById("link11111").href = 'https://twitter.com/PHXFire/status/1515418491892879360'
+     document.getElementById("time11111").innerHTML = '4/16/22 4:42pm MST'
+     document.getElementById("telegram11111").href = 'https://t.me/openteanews/29'
      
  
  
