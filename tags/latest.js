7114cda72c45c5b17e59ffc5e8a0fe0f207ccaa2
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-07-16 09:28:22 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -1,5 +1,19 @@
 function latest(){
     /* Top most entry */
+    document.getElementById("title1").innerHTML = 'National gas price average: $4.548'
+    document.getElementById("content1").innerHTML = ''
+    document.getElementById("content01").innerHTML = ''
+    document.getElementById("link1").innerHTML = ''
+    document.getElementById("link1").href = ''
+    document.getElementById("time1").innerHTML = '7/16/22 9:26am MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/37'
+    
+    
+    
+    /*https://tea.borgcreative.com/tags/telegram1.html */
+
+    /* Recent 2 */
+
     document.getElementById("title1").innerHTML = 'US national gas price average is $4.577'
     document.getElementById("content1").innerHTML = ''
     document.getElementById("content01").innerHTML = ''
@@ -8,32 +22,20 @@ function latest(){
     document.getElementById("time1").innerHTML = '7/15/22 10:52pm MST'
     document.getElementById("telegram1").href = 'https://t.me/openteanews/36'
     
-    
-    /*https://tea.borgcreative.com/tags/telegram1.html */
 
-    /* Recent 2 */
-
-    document.getElementById("title11").innerHTML = 'SoCal law enforcement officers arrest two suspects in connection with string of deadly robbies.'
-    document.getElementById("content11").innerHTML = ''
-    document.getElementById("content011").innerHTML = ''
-    document.getElementById("link11").innerHTML = 'Read the Tweet'
-    document.getElementById("link11").href = 'https://twitter.com/LAPDHQ/status/1548142254161768449'
-    document.getElementById("time11").innerHTML = '7/15/22 10:47pm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/35'
-    
 
     
 
     /* Recent 3 */
 
-    document.getElementById("title111").innerHTML = '- - -'
+    document.getElementById("title111").innerHTML = 'SoCal law enforcement officers arrest two suspects in connection with string of deadly robbies.'
     document.getElementById("content111").innerHTML = ''
     document.getElementById("content0111").innerHTML = ''
-    document.getElementById("link111").innerHTML = ''
-    document.getElementById("link111").href = ''
-    document.getElementById("time111").innerHTML = '00/00/22 00:00apm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/'
-
+    document.getElementById("link111").innerHTML = 'Read the Tweet'
+    document.getElementById("link111").href = 'https://twitter.com/LAPDHQ/status/1548142254161768449'
+    document.getElementById("time111").innerHTML = '7/15/22 10:47pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/35'
+    
 
 
 
