45386bb467ab4251b2bdd9f614a022aa25b669e7
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-10-29 16:44:22 -0700

    updates

--- tags/latest.js
+++ tags/latest.js
@@ -1,12 +1,12 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = ''
-    document.getElementById("content1").innerHTML = ''
+    document.getElementById("title1").innerHTML = 'National Gas Price Average - 10/29/22'
+    document.getElementById("content1").innerHTML = '$3.761'
     document.getElementById("content01").innerHTML = ''
     document.getElementById("link1").innerHTML = ''
     document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '00/00/22 00:00apm MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/'
+    document.getElementById("time1").innerHTML = '10/29/22 4:40pm MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/38'
     
     
     
@@ -14,13 +14,13 @@ function latest(){
 
     /* Recent 2 */
 
-    document.getElementById("title11").innerHTML = ''
+    document.getElementById("title11").innerHTML = 'Midterms approach '
     document.getElementById("content11").innerHTML = ''
     document.getElementById("content011").innerHTML = ''
     document.getElementById("link11").innerHTML = ''
     document.getElementById("link11").href = ''
-    document.getElementById("time11").innerHTML = '00/00/22 00:00apm MST'
-    document.getElementById("telegram11").href = 'https://t.me/openteanews/'
+    document.getElementById("time11").innerHTML = '10/29/22 4:40pm MST'
+    document.getElementById("telegram11").href = 'https://t.me/openteanews/38'
     
 
 
