85452b6403bef3678fb213bfae132a99b64f1a8e
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-10-29 16:59:21 -0700

    Update alert.js

--- tags/alert.js
+++ tags/alert.js
@@ -1,5 +1,5 @@
 function alert(){
-    document.getElementById("acontent1").innerHTML = 'Elon Musk has completed his purchase of Twitter.'
+    document.getElementById("acontent1").innerHTML = '"let that sink in!" Elon Musk has completed his purchase of Twitter.'
     document.getElementById("alink").innerHTML = 'Read the Tweet'
     document.getElementById("alink").href = 'https://twitter.com/elonmusk/status/1585341984679469056'
 }
