8cc8ac0daca1054741b9cd9a599255eb11328aba
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-10-29 21:09:24 -0700

    Update latest.js

--- tags/latest.js
+++ tags/latest.js
@@ -6,13 +6,13 @@ function latest(){
     document.getElementById("link1").innerHTML = 'Read the Tweet'
     document.getElementById("link1").href = 'https://twitter.com/FBILasVegas/status/1586426436755267584'
     document.getElementById("time1").innerHTML = '10/29/22 8:39pm MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/'
-    document.getElementById("rlink01").innerHTML = ''
-    document.getElementById("rlink01").href = ''
-
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/39'
 
     document.getElementById("rlink1").innerHTML = 'Common cryptocurrency scams and how to avoid them - Kaspersky'
     document.getElementById("rlink1").href = 'https://www.kaspersky.com/resource-center/definitions/cryptocurrency-scams'
+
+    document.getElementById("rlink01").innerHTML = ''
+    document.getElementById("rlink01").href = ''
 
     
     
