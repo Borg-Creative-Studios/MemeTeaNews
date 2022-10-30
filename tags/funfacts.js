6b6f2af3c7a3d62ddb384adc6e34147649b69072
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-10-29 21:31:11 -0700

    Update funfacts.js

--- tags/funfacts.js
+++ tags/funfacts.js
@@ -1,20 +1,21 @@
 function funfacts(){
     /* older 1 */
 
-    document.getElementById("fftitle1").innerHTML = 'The New York Stock Exchange'
-    document.getElementById("ffcontent1").innerHTML = '<p>The exchange was started in 1792.</p>'
-    document.getElementById("ffcontent2").innerHTML = '<p>The exchange began with just 24 stockbrokers and has evolved into one of the largest marketplaces for securities and investments.</p>'
+    document.getElementById("fftitle1").innerHTML = 'Noah Webster and the Dictionary'
+    document.getElementById("ffcontent1").innerHTML = '<p>Webster wrote various dictionaries of the English language, his most influentialbeing "An American Dictionary of the English Language". </p>'
+    document.getElementById("ffcontent2").innerHTML = '<p> </p>'
     document.getElementById("fflink1").innerHTML = ''
     document.getElementById("fflink1").href = ''
     document.getElementById("fftime1").innerHTML = ''
 
     /* older 2 */
 
-    document.getElementById("fftitle2").innerHTML = 'Moles and Chemistry'
-    document.getElementById("ffcontent3").innerHTML = '<p>Moles are a measurement unit used in chemistry.</p>'
-    document.getElementById("ffcontent4").innerHTML = '<p>1 mole is equal to 6.02x10^23 and is the number of atoms in an elements atomic weight.</p><p>EXAMPLE: 1.008g of Hydrogen has 1 mole of atoms.</p>'
+    document.getElementById("fftitle2").innerHTML = 'The New York Stock Exchange'
+    document.getElementById("ffcontent3").innerHTML = '<p>The exchange was started in 1792.</p>'
+    document.getElementById("ffcontent4").innerHTML = '<p>The exchange began with just 24 stockbrokers and has evolved into one of the largest marketplaces for securities and investments.</p>'
     document.getElementById("fflink2").innerHTML = ''
     document.getElementById("fflink2").href = ''
     document.getElementById("fftime2").innerHTML = ''
+
 
 }
