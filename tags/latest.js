08b6835beb8db3e3d43b04cb79873099ab951bf8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-10-29 20:42:07 -0700

    Updates
    -Added additional sources options to post scripts (Not to be confused with postscript lol)
    -Updated latest posts

--- tags/latest.js
+++ tags/latest.js
@@ -1,43 +1,57 @@
 function latest(){
     /* Top most entry */
-    document.getElementById("title1").innerHTML = 'National Gas Price Average - 10/29/22'
-    document.getElementById("content1").innerHTML = '$3.761'
+    document.getElementById("title1").innerHTML = 'FBI warns of scammers DMing social media users.'
+    document.getElementById("content1").innerHTML = 'Scammers are utilizing social media services to DM (Direct Message) users in an attempt to get them to buy into fake cryptocurrency investments. <p>Ways to protect yourself:</p> <p>Do not accept DM requests from those you do not know</p> <p>Never share your crypto-wallet address with someone you do not know</p>'
     document.getElementById("content01").innerHTML = ''
-    document.getElementById("link1").innerHTML = ''
-    document.getElementById("link1").href = ''
-    document.getElementById("time1").innerHTML = '10/29/22 4:40pm MST'
-    document.getElementById("telegram1").href = 'https://t.me/openteanews/38'
+    document.getElementById("link1").innerHTML = 'Read the Tweet'
+    document.getElementById("link1").href = 'https://twitter.com/FBILasVegas/status/1586426436755267584'
+    document.getElementById("time1").innerHTML = '10/29/22 8:39pm MST'
+    document.getElementById("telegram1").href = 'https://t.me/openteanews/'
     
+    document.getElementById("rlink1").innerHTML = 'Common cryptocurrency scams and how to avoid them - Kaspersky'
+    document.getElementById("rlink1").href = 'https://www.kaspersky.com/resource-center/definitions/cryptocurrency-scams'
+
+    document.getElementById("rlink01").innerHTML = ''
+    document.getElementById("rlink01").href = ''
     
     
     /*https://tea.borgcreative.com/tags/telegram1.html */
 
     /* Recent 2 */
 
-    document.getElementById("title11").innerHTML = 'Midterms approach'
-    document.getElementById("content11").innerHTML = ''
+
+    document.getElementById("title11").innerHTML = 'National Gas Price Average - 10/29/22'
+    document.getElementById("content11").innerHTML = '$3.761'
     document.getElementById("content011").innerHTML = ''
     document.getElementById("link11").innerHTML = ''
     document.getElementById("link11").href = ''
     document.getElementById("time11").innerHTML = '10/29/22 4:40pm MST'
     document.getElementById("telegram11").href = 'https://t.me/openteanews/38'
-    
 
 
+    document.getElementById("rlink11").innerHTML = ''
+    document.getElementById("rlink11").href = ''
+
+    document.getElementById("rlink011").innerHTML = ''
+    document.getElementById("rlink011").href = ''
     
 
     /* Recent 3 */
 
-    document.getElementById("title111").innerHTML = ''
-    document.getElementById("content111").innerHTML = ''
+    document.getElementById("title111").innerHTML = 'Midterms approach'
+    document.getElementById("content111").innerHTML = 'Midterms are November 8th, 2022.'
     document.getElementById("content0111").innerHTML = ''
     document.getElementById("link111").innerHTML = ''
     document.getElementById("link111").href = ''
-    document.getElementById("time111").innerHTML = '00/00/22 00:00apm MST'
-    document.getElementById("telegram111").href = 'https://t.me/openteanews/'
+    document.getElementById("time111").innerHTML = '10/29/22 4:40pm MST'
+    document.getElementById("telegram111").href = 'https://t.me/openteanews/38'
     
 
+    document.getElementById("rlink111").innerHTML = ''
+    document.getElementById("rlink111").href = ''
 
+    document.getElementById("rlink0111").innerHTML = ''
+    document.getElementById("rlink0111").href = ''
 
 
 
@@ -52,7 +66,11 @@ function latest(){
      document.getElementById("time1111").innerHTML = '00/00/22 00:00apm MST'
      document.getElementById("telegram1111").href = 'https://t.me/openteanews/'
  
-
+     document.getElementById("rlink1111").innerHTML = ''
+     document.getElementById("rlink1111").href = ''
+ 
+     document.getElementById("rlink01111").innerHTML = ''
+     document.getElementById("rlink01111").href = ''
 
      /* Recent 5 */
 
@@ -64,7 +82,11 @@ function latest(){
      document.getElementById("time11111").innerHTML = '00/00/22 00:00apm MST'
      document.getElementById("telegram11111").href = 'https://t.me/openteanews/'
      
-
+     document.getElementById("rlink1").innerHTML = ''
+     document.getElementById("rlink1").href = ''
+ 
+     document.getElementById("rlink011111").innerHTML = ''
+     document.getElementById("rlink011111").href = ''
 
  
  
